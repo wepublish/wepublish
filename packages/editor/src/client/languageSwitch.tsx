5721ea3bbe71d852502e1bea98616059ab48fcda
Pascal Witzig <pascal.witzig@gmail.com>
2020-10-06 00:01:37 +0200

    wip(editor): integrating language switch into navbar

--- packages/editor/src/client/languageSwitch.tsx
+++ packages/editor/src/client/languageSwitch.tsx
@@ -1,23 +1,62 @@
 /* eslint-disable i18next/no-literal-string */
 
 import React, {useState} from 'react'
-import {Select, Spacing} from '@karma.run/ui'
+import {elementForTypographyVariant, Select, Spacing} from '@karma.run/ui'
 import {cssRule, useStyle} from '@karma.run/react'
 import {MaterialIconLanguage} from '@karma.run/icons'
+import {Color} from '../style/colors'
+import e from 'express'
 //import {useTranslation} from 'react-i18next'
 
-const languageSwitcher = cssRule({
+export function pxToRem(px: number) {
+  return `${px / 10}rem`
+}
+
+const languageSwitch = cssRule({
   position: 'relative',
-  padding: 20
+  marginBottom: pxToRem(100),
+  paddingLeft: pxToRem(11),
+  paddingBottom: pxToRem(10),
+  paddingTop: pxToRem(10),
+
+  '&:hover': {
+    backgroundColor: 'rgba(0, 0, 0, 0.025)'
+  },
+
+  '& > div ': {padding: 0},
+
+  '& > div > ul': {
+    borderRadius: 0,
+    marginTop: pxToRem(10),
+    marginLeft: pxToRem(-11),
+    width: 'calc(100% + 1.1rem)',
+
+    '& li': {
+      paddingLeft: pxToRem(13)
+    }
+  },
+
+  '& > div > label > button': {
+    paddingLeft: pxToRem(21),
+    border: 'none'
+  },
+
+  '& > div > label > svg': {
+    fontSize: '1.5em'
+  },
+
+  '& > div > label > svg > path': {
+    cursor: 'pointer'
+  }
 })
 
 export function LanguageSwitch() {
   const css = useStyle()
-  const [uiLanguage, setUILanguage] = useState([])
+  const [uiLanguage, setUILanguage] = useState({id: 'en', lang: 'en_US', name: 'English'})
   //const {t} = useTranslation()
 
   return (
-    <div className={css(languageSwitcher)}>
+    <div className={css(languageSwitch)}>
       <Select
         icon={MaterialIconLanguage}
         options={[
@@ -25,9 +64,8 @@ export function LanguageSwitch() {
           {id: 'de', lang: 'de_CH', name: 'Deutsch'},
           {id: 'fr', lang: 'fr_FR', name: 'Français'}
         ]}
-        value={{id: uiLanguage}}
+        value={{id: uiLanguage.id, lang: uiLanguage.lang, name: uiLanguage.name}}
         renderListItem={value => value?.name}
-        marginBottom={Spacing.Small}
         onChange={value => {
           if (value?.name) {
             setUILanguage(value)
