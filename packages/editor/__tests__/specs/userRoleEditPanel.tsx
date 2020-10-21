0c1872e36be0a90feac02fb9c928f150c172a7db
Nicolas Roos <nicolas.k.roos@gmail.com>
2020-10-21 10:37:11 +0200

    test(editor): added utils and little clanup

--- packages/editor/__tests__/specs/userRoleEditPanel.tsx
+++ packages/editor/__tests__/specs/userRoleEditPanel.tsx
@@ -9,28 +9,8 @@ import Adapter from 'enzyme-adapter-react-16'
 Enzyme.configure({adapter: new Adapter()})
 
 import {UIProvider /*, Select*/} from '@karma.run/ui'
-import {act} from 'react-dom/test-utils'
 import * as fela from 'fela'
-
-// https://github.com/wesbos/waait/blob/master/index.js
-export function wait(amount = 0) {
-  return new Promise(resolve => setTimeout(resolve, amount))
-}
-
-// Use this in your test after mounting if you need just need to let the query finish without updating the wrapper
-export async function actWait(amount = 0) {
-  await act(async () => {
-    await wait(amount)
-  })
-}
-
-// Use this in your test after mounting if you want the query to finish and update the wrapper
-export async function updateWrapper(wrapper: Enzyme.ReactWrapper, amount = 0) {
-  await act(async () => {
-    await wait(amount)
-    wrapper.update()
-  })
-}
+import {updateWrapper} from '../utils'
 
 const styleRenderer: fela.IRenderer = {
   renderRule: jest.fn(),
