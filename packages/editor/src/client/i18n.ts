bdd90f65ab6793fbbc5823aba61c393fc00bf9ff
Pascal Witzig <pascal.witzig@gmail.com>
2020-10-21 15:24:30 +0200

    bugfix(editor): update i18n.ts for de and fr

--- packages/editor/src/client/i18n.ts
+++ packages/editor/src/client/i18n.ts
@@ -3,6 +3,8 @@ import {initReactI18next} from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 
 import en from './locales/en.json'
+import fr from './locales/fr.json'
+import de from './locales/de.json'
 
 i18n
   .use(LanguageDetector)
@@ -11,7 +13,9 @@ i18n
     fallbackLng: 'en',
     debug: true,
     resources: {
-      en
+      en,
+      fr,
+      de
     },
 
     interpolation: {
