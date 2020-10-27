766e5f9f10868f36a31b84a5c2a600a528f883c2
faebebin <fabianbndr@gmail.com>
2020-10-27 18:32:22 +0100

    Added sub- und superscript icons form materui-icons.

--- packages/editor/src/client/blocks/richTextBlock.tsx
+++ packages/editor/src/client/blocks/richTextBlock.tsx
@@ -27,9 +27,7 @@ import {
   MaterialIconLink,
   MaterialIconLinkOff,
   MaterialIconClose,
-  MaterialIconCheck,
-  MaterialIconDone,
-  MaterialIconDoneAll
+  MaterialIconCheck
 } from '@karma.run/icons'
 
 import {
@@ -333,8 +331,8 @@ export const RichTextBlock = memo(function RichTextBlock({
 
         <ToolbarDivider />
 
-        <FormatButton icon={MaterialIconDone} format={TextFormat.Superscript} />
-        <FormatButton icon={MaterialIconDoneAll} format={TextFormat.Subscript} />
+        <FormatButton icon={MaterialIconFormatSuperscript} format={TextFormat.Superscript} />
+        <FormatButton icon={MaterialIconFormatSubscript} format={TextFormat.Subscript} />
 
         <ToolbarDivider />
 
@@ -588,4 +586,36 @@ function withRichText<T extends ReactEditor>(editor: T): T {
 
 export function createDefaultValue(): RichTextBlockValue {
   return [{type: BlockFormat.Paragraph, children: [{text: ''}]}]
+}
+
+function MaterialIconFormatSuperscript() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      enableBackground="new 0 0 24 24"
+      height="24"
+      viewBox="0 0 24 24"
+      width="24">
+      <g>
+        <rect fill="none" height="24" width="24" x="0" y="0" />
+        <path d="M22,7h-2v1h3v1h-4V7c0-0.55,0.45-1,1-1h2V5h-3V4h3c0.55,0,1,0.45,1,1v1C23,6.55,22.55,7,22,7z M5.88,20h2.66l3.4-5.42h0.12 l3.4,5.42h2.66l-4.65-7.27L17.81,6h-2.68l-3.07,4.99h-0.12L8.85,6H6.19l4.32,6.73L5.88,20z" />
+      </g>
+    </svg>
+  )
+}
+
+function MaterialIconFormatSubscript() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      enableBackground="new 0 0 24 24"
+      height="24"
+      viewBox="0 0 24 24"
+      width="24">
+      <g>
+        <rect fill="none" height="24" width="24" />
+        <path d="M22,18h-2v1h3v1h-4v-2c0-0.55,0.45-1,1-1h2v-1h-3v-1h3c0.55,0,1,0.45,1,1v1C23,17.55,22.55,18,22,18z M5.88,18h2.66 l3.4-5.42h0.12l3.4,5.42h2.66l-4.65-7.27L17.81,4h-2.68l-3.07,4.99h-0.12L8.85,4H6.19l4.32,6.73L5.88,18z" />
+      </g>
+    </svg>
+  )
 }
