ac3ef51fdcdc6ee8740110451c120edc0f079a8f
Nicolas Roos <nicolas.k.roos@gmail.com>
2020-10-30 13:44:35 +0100

    test(editor): changed tsconfig

--- packages/editor/jest.config.js
+++ packages/editor/jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   globals: {
     'ts-jest': {
-      tsConfig: 'src/client/tsconfig.json'
+      tsconfig: './tsconfig.json'
     }
   },
   testMatch: ['**/__tests__/specs/**/*.+(ts|tsx|js)'],
