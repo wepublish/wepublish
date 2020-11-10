6f2aa6d43012c1be09a2e01e597b8d3de2155cf7
faebebin <fabianbndr@gmail.com>
2020-11-10 21:12:29 +0100

    Comment out for css loader fixing.

--- packages/editor/src/client/blocks/richTextBlock.tsx
+++ packages/editor/src/client/blocks/richTextBlock.tsx
@@ -15,7 +15,7 @@ import {withHistory} from 'slate-history'
 import {jsx} from 'slate-hyperscript'
 
 import 'emoji-mart/css/emoji-mart.css'
-import {Picker, PickerProps} from 'emoji-mart'
+// import {Picker, PickerProps} from 'emoji-mart'
 
 import {BlockProps} from '../atoms/blockList'
 import {Toolbar, ToolbarButtonProps, ToolbarButton, ToolbarDivider} from '../atoms/toolbar'
@@ -309,11 +309,6 @@ export const RichTextBlock = memo(function RichTextBlock({
 
         <LinkFormatButton />
         <RemoveLinkFormatButton />
-
-        <ToolbarDivider />
-
-        <FormatButton icon={MaterialIconFormatSuperscript} format={TextFormat.Superscript} />
-        <FormatButton icon={MaterialIconFormatSubscript} format={TextFormat.Subscript} />
       </Toolbar>
       <Editable
         readOnly={disabled}
@@ -343,6 +338,14 @@ function FormatButton({icon, format}: SlateBlockButtonProps) {
     />
   )
 }
+
+//
+//        <ToolbarDivider />
+//
+//        <FormatButton icon={MaterialIconFormatSuperscript} format={TextFormat.Superscript} />
+//        <FormatButton icon={MaterialIconFormatSubscript} format={TextFormat.Subscript} />
+//
+
 //
 //        <ToolbarDivider />
 //
@@ -572,34 +575,34 @@ export function createDefaultValue(): RichTextBlockValue {
   return [{type: BlockFormat.Paragraph, children: [{text: ''}]}]
 }
 
-function MaterialIconFormatSuperscript() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      enableBackground="new 0 0 24 24"
-      height="24"
-      viewBox="0 0 24 24"
-      width="24">
-      <g>
-        <rect fill="none" height="24" width="24" x="0" y="0" />
-        <path d="M22,7h-2v1h3v1h-4V7c0-0.55,0.45-1,1-1h2V5h-3V4h3c0.55,0,1,0.45,1,1v1C23,6.55,22.55,7,22,7z M5.88,20h2.66l3.4-5.42h0.12 l3.4,5.42h2.66l-4.65-7.27L17.81,6h-2.68l-3.07,4.99h-0.12L8.85,6H6.19l4.32,6.73L5.88,20z" />
-      </g>
-    </svg>
-  )
-}
-
-function MaterialIconFormatSubscript() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      enableBackground="new 0 0 24 24"
-      height="24"
-      viewBox="0 0 24 24"
-      width="24">
-      <g>
-        <rect fill="none" height="24" width="24" />
-        <path d="M22,18h-2v1h3v1h-4v-2c0-0.55,0.45-1,1-1h2v-1h-3v-1h3c0.55,0,1,0.45,1,1v1C23,17.55,22.55,18,22,18z M5.88,18h2.66 l3.4-5.42h0.12l3.4,5.42h2.66l-4.65-7.27L17.81,4h-2.68l-3.07,4.99h-0.12L8.85,4H6.19l4.32,6.73L5.88,18z" />
-      </g>
-    </svg>
-  )
-}
+// function MaterialIconFormatSuperscript() {
+//   return (
+//     <svg
+//       xmlns="http://www.w3.org/2000/svg"
+//       enableBackground="new 0 0 24 24"
+//       height="24"
+//       viewBox="0 0 24 24"
+//       width="24">
+//       <g>
+//         <rect fill="none" height="24" width="24" x="0" y="0" />
+//         <path d="M22,7h-2v1h3v1h-4V7c0-0.55,0.45-1,1-1h2V5h-3V4h3c0.55,0,1,0.45,1,1v1C23,6.55,22.55,7,22,7z M5.88,20h2.66l3.4-5.42h0.12 l3.4,5.42h2.66l-4.65-7.27L17.81,6h-2.68l-3.07,4.99h-0.12L8.85,6H6.19l4.32,6.73L5.88,20z" />
+//       </g>
+//     </svg>
+//   )
+// }
+//
+// function MaterialIconFormatSubscript() {
+//   return (
+//     <svg
+//       xmlns="http://www.w3.org/2000/svg"
+//       enableBackground="new 0 0 24 24"
+//       height="24"
+//       viewBox="0 0 24 24"
+//      width="24">
+//      <g>
+//        <rect fill="none" height="24" width="24" />
+//        <path d="M22,18h-2v1h3v1h-4v-2c0-0.55,0.45-1,1-1h2v-1h-3v-1h3c0.55,0,1,0.45,1,1v1C23,17.55,22.55,18,22,18z M5.88,18h2.66 l3.4-5.42h0.12l3.4,5.42h2.66l-4.65-7.27L17.81,4h-2.68l-3.07,4.99h-0.12L8.85,4H6.19l4.32,6.73L5.88,18z" />
+//      </g>
+//    </svg>
+//  )
+// }
