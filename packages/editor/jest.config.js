5b74e35d5c4b0e98b31da814ba0265cf13cc9e99
Nicolas Roos <nicolas.k.roos@gmail.com>
2020-11-11 17:01:16 +0100

    test(editor): mock css imports

--- packages/editor/jest.config.js
+++ packages/editor/jest.config.js
@@ -13,6 +13,11 @@ module.exports = {
   setupFilesAfterEnv: ['./__tests__/setup.ts'],
   verbose: true,
   testURL: 'http://localhost/',
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': 'identity-obj-proxy'
+  },
   snapshotSerializers: ['enzyme-to-json/serializer'],
   coveragePathIgnorePatterns: ['node_modules', 'verion.ts'],
   coverageDirectory: '__tests__/coverage',
