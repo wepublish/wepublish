424ff7d6bddac8991d9bb8294d5884c39d028b1b
Nicolas Roos <nicolas.k.roos@gmail.com>
2020-11-13 18:58:14 +0100

    refactor(editor): updated extended linkPageBreak block and panel

--- packages/editor/src/client/blocks/linkPageBreakBlock.tsx
+++ packages/editor/src/client/blocks/linkPageBreakBlock.tsx
@@ -1,6 +1,6 @@
 import React, {useRef, useEffect, useState, useCallback} from 'react'
 
-import {TypographicTextArea} from '../atoms/typographicTextArea'
+import {Drawer, IconButton, Icon, Input} from 'rsuite'
 import {BlockProps} from '../atoms/blockList'
 import {LinkPageBreakBlockValue, RichTextBlockValue} from './types'
 import {createDefaultValue, RichTextBlock} from './richTextBlock'
@@ -10,6 +10,7 @@ import {isFunctionalUpdate} from '@karma.run/react'
 
 import {useTranslation} from 'react-i18next'
 import {LinkPageBreakEditPanel} from '../panel/linkPageBreakEditPanel'
+import {ChooseEditImage} from '../atoms/chooseEditImage'
 export type LinkPageBreakBlockProps = BlockProps<LinkPageBreakBlockValue>
 
 export function LinkPageBreakBlock({
@@ -43,94 +44,63 @@ export function LinkPageBreakBlock({
 
   return (
     <>
-      <Box position="relative" width="100%">
-        <Box position="absolute" zIndex={ZIndex.Default} height="100%" right={0}>
+      <div style={{position: 'relative', width: '100%'}}>
+        <div style={{position: 'absolute', zIndex: 1, height: '100%', right: 0}}>
           <IconButton
-            icon={MaterialIconEditOutlined}
+            size={'lg'}
+            icon={<Icon icon="pencil" />}
             onClick={() => setEditPanelOpen(true)}
-            margin={Spacing.ExtraSmall}
           />
-        </Box>
-      </Box>
-      <div style={{display: 'flex', flexFlow: 'row wrap'}}>
-        <div style={{flex: '1 0 25%', alignSelf: 'center', marginBottom: '10px'}}>
-          <Card overflow="hidden" width={'100%'} height={150} padding={0}>
-            <PlaceholderInput onAddClick={() => setChooseModalOpen(true)}>
-              {image && (
-                <Box position="relative" width="100%" height="100%">
-                  <Box position="absolute" zIndex={ZIndex.Default} right={0} top={0}>
-                    <IconButton
-                      icon={MaterialIconImageOutlined}
-                      margin={Spacing.ExtraSmall}
-                      onClick={() => setChooseModalOpen(true)}
-                    />
-                    <IconButton
-                      icon={MaterialIconEditOutlined}
-                      margin={Spacing.ExtraSmall}
-                      onClick={() => setEditModalOpen(true)}
-                    />
-                    <IconButton
-                      icon={MaterialIconClose}
-                      margin={Spacing.ExtraSmall}
-                      onClick={() => onChange(value => ({...value, image: undefined}))}
-                    />
-                  </Box>
-                  {image.previewURL && <Image src={image.previewURL} width="100%" height="100%" />}
-                </Box>
-              )}
-            </PlaceholderInput>
-          </Card>
-        </div>
-        <div style={{flex: '1 0 70%'}}>
-          <Box padding={Spacing.ExtraSmall} marginBottom={Spacing.ExtraSmall}>
-            <Box>
-              <TextInput
-                ref={focusInputRef}
-                placeholder={t('blocks.linkPageBreak.title')}
-                label={t('blocks.linkPageBreak.title')}
-                style={{fontSize: '24px'}}
-                value={text}
-                disabled={disabled}
-                onChange={e => onChange({...value, text: e.target.value})}
-              />
-            </Box>
-            <Box flexGrow={1}>
-              <RichTextBlock
-                value={richText || createDefaultValue()}
-                onChange={handleRichTextChange}
-              />
-            </Box>
-          </Box>
         </div>
       </div>
-      <Drawer open={isChooseModalOpen} width={480}>
-        {() => (
-          <ImageSelectPanel
-            onClose={() => setChooseModalOpen(false)}
-            onSelect={image => {
-              setChooseModalOpen(false)
-              onChange(value => ({...value, image, imageID: image.id}))
-            }}
+      <div style={{display: 'flex', flexFlow: 'row wrap', marginTop: 50}}>
+        <div style={{flex: '1 0 25%', alignSelf: 'center', marginBottom: '10px'}}>
+          <ChooseEditImage
+            header={''}
+            image={image}
+            disabled={false}
+            openChooseModalOpen={() => setChooseModalOpen(true)}
+            openEditModalOpen={() => setEditModalOpen(true)}
+            removeImage={() => onChange(value => ({...value, image: undefined}))}
           />
-        )}
+        </div>
+        <div style={{flex: '1 0 70%'}}>
+          <Input
+            ref={focusInputRef}
+            placeholder={t('blocks.linkPageBreak.title')}
+            style={{fontSize: '24px', marginBottom: 20}}
+            value={text}
+            disabled={disabled}
+            onChange={text => onChange({...value, text})}
+          />
+
+          <RichTextBlock value={richText || createDefaultValue()} onChange={handleRichTextChange} />
+        </div>
+      </div>
+      <Drawer show={isChooseModalOpen} size={'sm'} onHide={() => setChooseModalOpen(false)}>
+        <ImageSelectPanel
+          onClose={() => setChooseModalOpen(false)}
+          onSelect={image => {
+            setChooseModalOpen(false)
+            onChange(value => ({...value, image, imageID: image.id}))
+          }}
+        />
       </Drawer>
-      <Drawer open={isEditModalOpen} width={480}>
-        {() => (
+      {image && (
+        <Drawer show={isEditModalOpen} size={'sm'} onHide={() => setEditModalOpen(false)}>
           <ImagedEditPanel
             id={image!.id}
             onClose={() => setEditModalOpen(false)}
             onSave={() => setEditModalOpen(false)}
           />
-        )}
-      </Drawer>
-      <Drawer open={isEditPanelOpen} width={480}>
-        {() => (
-          <LinkPageBreakEditPanel
-            value={value}
-            onClose={() => setEditPanelOpen(false)}
-            onChange={onChange}
-          />
-        )}
+        </Drawer>
+      )}
+      <Drawer show={isEditPanelOpen} size={'sm'} onHide={() => setEditPanelOpen(false)}>
+        <LinkPageBreakEditPanel
+          value={value}
+          onClose={() => setEditPanelOpen(false)}
+          onChange={onChange}
+        />
       </Drawer>
     </>
   )
