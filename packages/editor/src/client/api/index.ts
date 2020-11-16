4cf4b68ccca7c7d87d98c06fc8b50b5e010a545b
faebebin <fabianbndr@gmail.com>
2020-11-16 09:44:36 +0100

    Autogenerated changes.

--- packages/editor/src/client/api/index.ts
+++ packages/editor/src/client/api/index.ts
@@ -382,14 +382,28 @@ export type InstagramPostBlockInput = {
 export type LinkPageBreakBlock = {
   __typename?: 'LinkPageBreakBlock';
   text?: Maybe<Scalars['String']>;
+  richText: Scalars['RichText'];
   linkURL?: Maybe<Scalars['String']>;
   linkText?: Maybe<Scalars['String']>;
+  linkTarget?: Maybe<Scalars['String']>;
+  hideButton: Scalars['Boolean'];
+  styleOption?: Maybe<Scalars['String']>;
+  layoutOption?: Maybe<Scalars['String']>;
+  templateOption?: Maybe<Scalars['String']>;
+  image?: Maybe<Image>;
 };
 
 export type LinkPageBreakBlockInput = {
   text?: Maybe<Scalars['String']>;
+  richText: Scalars['RichText'];
   linkURL?: Maybe<Scalars['String']>;
   linkText?: Maybe<Scalars['String']>;
+  linkTarget?: Maybe<Scalars['String']>;
+  hideButton: Scalars['Boolean'];
+  styleOption?: Maybe<Scalars['String']>;
+  templateOption?: Maybe<Scalars['String']>;
+  layoutOption?: Maybe<Scalars['String']>;
+  imageID?: Maybe<Scalars['ID']>;
 };
 
 export type ListicleBlock = {
