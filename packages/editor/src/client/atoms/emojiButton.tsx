51721b8c5e6b4f837382fd9bf45cfeb1c2868283
Nicolas Roos <nicolas.k.roos@gmail.com>
2020-11-17 09:28:18 +0100

    chore(editor): import css in less file

--- packages/editor/src/client/atoms/emojiButton.tsx
+++ packages/editor/src/client/atoms/emojiButton.tsx
@@ -5,7 +5,6 @@ import {Icon, Popover, Whisper} from 'rsuite'
 import {SVGIcon} from 'rsuite/lib/@types/common'
 import {IconNames} from 'rsuite/lib/Icon/Icon'
 
-import 'emoji-mart/css/emoji-mart.css'
 import './emojiButton.less'
 import {Picker, BaseEmoji} from 'emoji-mart'
 
