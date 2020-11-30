1eb4c3b8f49ed314b79412a031172a984ae94a3b
faebebin <fabianbndr@gmail.com>
2020-11-30 14:55:46 +0100

    Get correct (single) ref source of Popover visibility state.

--- packages/editor/src/client/atoms/emojiButton.tsx
+++ packages/editor/src/client/atoms/emojiButton.tsx
@@ -1,4 +1,4 @@
-import React, {forwardRef, ButtonHTMLAttributes, useState, useRef} from 'react'
+import React, {ButtonHTMLAttributes, useCallback, useRef, useState} from 'react'
 import {useSlate} from 'slate-react'
 
 import {Icon, Popover, Whisper} from 'rsuite'
@@ -11,54 +11,20 @@ import {Picker, BaseEmoji} from 'emoji-mart'
 export interface EmojiButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   readonly icon: IconNames | SVGIcon
   readonly iconActive?: IconNames | SVGIcon
-  readonly active?: boolean
 }
-
-export const BaseEmojiButton = forwardRef<HTMLButtonElement, EmojiButtonProps>(
-  function BaseEmojiButton({icon, iconActive, active, ...props}, ref) {
-    icon = iconActive && active ? iconActive : icon
-    return (
-      <button
-        style={{
-          border: active ? 'blue 1px solid' : '',
-          fontSize: 16,
-
-          cursor: 'pointer',
-          borderRadius: 3,
-          backgroundColor: 'transparent',
-
-          padding: 2
-        }}
-        ref={ref}
-        {...props}>
-        <Icon icon={icon} element={icon} />
-      </button>
-    )
-  }
-)
 
 export function EmojiButton({icon, iconActive}: EmojiButtonProps) {
   const editor = useSlate()
-  const [isEmojiPickerOpen, setIsEmojiPickerOpen] = useState(false)
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false)
 
   const triggerRef = useRef<any>(null)
 
-  const open = () => {
-    if (triggerRef.current) {
-      triggerRef.current!.open()
-      // TODO rather be done with forwardRef ?
-      setIsEmojiPickerOpen(true)
-    }
-  }
-  const close = () => {
-    if (triggerRef.current) {
-      triggerRef.current!.close()
-      setIsEmojiPickerOpen(false)
-    }
-  }
+  const popOverRef = useCallback((node: any) => {
+    setIsPopoverOpen(!!node)
+  }, [])
 
   const emojiPicker = (
-    <Popover>
+    <Popover ref={popOverRef}>
       <Picker
         onSelect={({native}: BaseEmoji) => {
           editor.insertText(native)
@@ -67,17 +33,26 @@ export function EmojiButton({icon, iconActive}: EmojiButtonProps) {
     </Popover>
   )
 
+  icon = isPopoverOpen && iconActive ? iconActive : icon
   return (
     <div>
       <Whisper placement="top" speaker={emojiPicker} ref={triggerRef} trigger="none">
-        <BaseEmojiButton
-          icon={isEmojiPickerOpen ? iconActive || icon : icon}
-          active={isEmojiPickerOpen}
+        <button
+          style={{
+            border: isPopoverOpen ? 'blue 1px solid' : '',
+            fontSize: 16,
+            cursor: 'pointer',
+            borderRadius: 3,
+            backgroundColor: 'transparent',
+            padding: 2
+          }}
           onMouseDown={e => {
             e.preventDefault()
-            isEmojiPickerOpen ? close() : open()
-          }}
-        />
+            !isPopoverOpen ? triggerRef.current!.open() : triggerRef.current!.close()
+            console.log(!!isPopoverOpen)
+          }}>
+          <Icon icon={icon} element={icon} />
+        </button>
       </Whisper>
     </div>
   )
