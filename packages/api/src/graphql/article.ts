6aac8b06247d4cd513256543bbfd54ed431dcc66
Pascal Witzig <pascal.witzig@gmail.com>
2020-12-15 17:35:10 +0100

    wip(editor): set type of socialMediaAuthors to GraphQLNonNull

--- packages/api/src/graphql/article.ts
+++ packages/api/src/graphql/article.ts
@@ -268,7 +268,7 @@ export const GraphQLPublicArticle: GraphQLObjectType<
     socialMediaTitle: {type: GraphQLString},
     socialMediaDescription: {type: GraphQLString},
     socialMediaAuthors: {
-      type: GraphQLList(GraphQLAuthor),
+      type: GraphQLNonNull(GraphQLList(GraphQLAuthor)),
       resolve: createProxyingResolver(({socialMediaAuthorIDs}, args, {loaders}) => {
         return Promise.all(
           socialMediaAuthorIDs.map(socialMediaAuthorIDs =>
