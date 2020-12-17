8a9d9ef9032c0363c444661e2bc611ebd88d3ab1
Pascal Witzig <pascal.witzig@gmail.com>
2020-12-17 10:57:21 +0100

    wip(editor): fix bug in pageTemplateContainer so that title/description is correctly displayed if no socialMedia-Metadata exists

--- examples/website/src/shared/route/pageTemplateContainer.tsx
+++ examples/website/src/shared/route/pageTemplateContainer.tsx
@@ -122,9 +122,12 @@ export function PageTemplateContainer({slug, id}: PageTemplateContainerProps) {
         {description && <meta name="description" content={description} />}
         <link rel="canonical" href={canonicalURL} />
 
-        <meta property="og:title" content={socialMediaTitle ?? title} />
+        <meta property="og:title" content={socialMediaTitle !== '' ? socialMediaTitle : title} />
         {(socialMediaDescription || description) && (
-          <meta property="og:description" content={socialMediaDescription ?? description} />
+          <meta
+            property="og:description"
+            content={socialMediaDescription !== '' ? socialMediaDescription : description}
+          />
         )}
         <meta property="og:type" content="website" />
         <meta property="og:url" content={canonicalURL} />
