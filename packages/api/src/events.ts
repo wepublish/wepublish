e59fc534a6089d39dad0f6bc4b9e07740e3e15c9
Nicolas Roos <nicolas.k.roos@gmail.com>
2020-12-18 11:57:59 +0100

    feat(api): event emitters now typed

--- packages/api/src/events.ts
+++ packages/api/src/events.ts
@@ -1,21 +1,53 @@
 import {EventEmitter} from 'events'
+import TypedEmitter from 'typed-emitter'
 import {Context} from './context'
 import {User} from './db/user'
+import {Article} from './db/article'
+import {Peer} from './db/peer'
+import {Page} from './db/page'
+interface ModelEvents<T> {
+  create: (context: Context, model: T) => void
+  update: (context: Context, model: T) => void
+  delete: (context: Context, id: string) => void
+}
 
-const eventEmitter = new EventEmitter()
+interface PublishableModelEvents<T> extends ModelEvents<T> {
+  publish: (context: Context, model: T) => void
+  unpublish: (context: Context, model: T) => void
+}
 
-eventEmitter.on('PostCreateUser', async (context: Context, user: User) => {
+export type UserModelEventsEmitter = TypedEmitter<ModelEvents<User>>
+export const userModelEvents = new EventEmitter() as UserModelEventsEmitter
+
+export type PeerModelEventsEmitter = TypedEmitter<ModelEvents<Peer>>
+export const peerModelEvents = new EventEmitter() as PeerModelEventsEmitter
+
+export type ArticleModelEventEmitter = TypedEmitter<PublishableModelEvents<Article>>
+export const articleModelEvents = new EventEmitter() as ArticleModelEventEmitter
+
+export type PageModelEventEmitter = TypedEmitter<PublishableModelEvents<Page>>
+export const pageModelEvents = new EventEmitter() as PageModelEventEmitter
+
+export type EventsEmitter =
+  | UserModelEventsEmitter
+  | PeerModelEventsEmitter
+  | ArticleModelEventEmitter
+  | PageModelEventEmitter
+
+userModelEvents.on('create', (context, model) => {
   setImmediate(async () => {
-    console.log('start of PostCrateUser Event')
     await context.dbAdapter.userRole.createUserRole({
       input: {
-        name: `${user.name}-Role`,
-        description: `UserRole for ${user.name}`,
+        name: `${model.name}-Role`,
+        description: `UserRole for ${model.name}`,
         permissionIDs: []
       }
     })
-    console.log('end of PostCreateUser Event')
   })
 })
 
-export default eventEmitter
+articleModelEvents.on('publish', (context, model) => {
+  setImmediate(() => {
+    console.log(`Published Article ${model.published?.title}`)
+  })
+})
