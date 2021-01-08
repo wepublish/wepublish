3a29d562017e0672e03393e80655954e1db5e184
faebebin <fabianbndr@gmail.com>
2021-01-08 15:07:23 +0100

    Also test create table function

---
+++ packages/editor/__tests__/specs/elements.test.ts
@@ -0,0 +1,86 @@
+import {Element as SlateElement} from 'slate'
+import {BlockFormat} from '../../src/client/blocks/richTextBlock/editor/formats'
+import {pTest} from '../utils'
+import {
+  emptyTextParagraph,
+  emptyCellsTable
+} from '../../src/client/blocks/richTextBlock/editor/elements'
+
+interface EmptyCellsTableTest {
+  name: string
+  createTable: SlateElement[]
+  output: SlateElement[]
+}
+
+pTest(
+  'emptyCellsTable',
+  [
+    {
+      name: 'should return single cell table with subsequent paragraph',
+      createTable: emptyCellsTable(1, 1),
+      output: [
+        {
+          type: BlockFormat.Table,
+          children: [
+            {
+              type: BlockFormat.TableRow,
+              children: [
+                {
+                  type: BlockFormat.TableCell,
+                  borderColor: 'black',
+                  children: [emptyTextParagraph()]
+                }
+              ]
+            }
+          ]
+        },
+        emptyTextParagraph()
+      ]
+    },
+    {
+      name: 'should return 2 rows with 2 cells each',
+      createTable: emptyCellsTable(2, 2),
+      output: [
+        {
+          type: BlockFormat.Table,
+          children: [
+            {
+              type: BlockFormat.TableRow,
+              children: [
+                {
+                  type: BlockFormat.TableCell,
+                  borderColor: 'black',
+                  children: [emptyTextParagraph()]
+                },
+                {
+                  type: BlockFormat.TableCell,
+                  borderColor: 'black',
+                  children: [emptyTextParagraph()]
+                }
+              ]
+            },
+            {
+              type: BlockFormat.TableRow,
+              children: [
+                {
+                  type: BlockFormat.TableCell,
+                  borderColor: 'black',
+                  children: [emptyTextParagraph()]
+                },
+                {
+                  type: BlockFormat.TableCell,
+                  borderColor: 'black',
+                  children: [emptyTextParagraph()]
+                }
+              ]
+            }
+          ]
+        },
+        emptyTextParagraph()
+      ]
+    }
+  ],
+  (t: EmptyCellsTableTest) => {
+    expect(t.createTable).toEqual(t.output)
+  }
+)
