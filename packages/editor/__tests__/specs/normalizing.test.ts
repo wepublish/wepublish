a767facc380faf6f6bfc037327a8c07208453347
faebebin <fabianbndr@gmail.com>
2021-01-08 14:47:05 +0100

    Improve code structure

--- packages/editor/__tests__/specs/normalizing.test.ts
+++ packages/editor/__tests__/specs/normalizing.test.ts
@@ -8,18 +8,6 @@ import {
   emptyCellsTable
 } from '../../src/client/blocks/richTextBlock/editor/elements'
 
-const invalidTable: SlateElement[] = [
-  {
-    type: BlockFormat.Table,
-    children: [emptyTextParagraph()]
-  },
-  {
-    type: BlockFormat.TableCell,
-    borderColor: 'black',
-    children: [emptyTextParagraph()]
-  }
-]
-
 interface WithNormalizeNodeTest {
   name: string
   entryData: SlateElement[]
@@ -30,13 +18,33 @@ pTest(
   'withNormalizeNode',
   [
     {
-      name: 'should return valid data',
+      name: 'should return unmodified valid data',
       entryData: emptyCellsTable(1, 1),
       normalizedData: emptyCellsTable(1, 1)
     },
     {
+      name: 'should return valid table given a nakedTableRow',
+      entryData: [
+        {
+          type: BlockFormat.TableRow,
+          children: [emptyTextParagraph()]
+        }
+      ],
+      normalizedData: emptyCellsTable(1, 1)
+    },
+    {
       name: 'returns given string its length equals maxLength',
-      entryData: invalidTable,
+      entryData: [
+        {
+          type: BlockFormat.Table,
+          children: [emptyTextParagraph()]
+        },
+        {
+          type: BlockFormat.TableCell,
+          borderColor: 'black',
+          children: [emptyTextParagraph()]
+        }
+      ],
       normalizedData: [...emptyCellsTable(1, 1), ...emptyCellsTable(1, 1)]
     }
   ],
--- packages/editor/src/client/blocks/richTextBlock/editor/normalizing.ts
+++ packages/editor/src/client/blocks/richTextBlock/editor/normalizing.ts
@@ -20,54 +20,6 @@ export function withNormalizeNode<T extends ReactEditor>(editor: T): T {
 
   editor.normalizeNode = entry => {
     const [node, path] = entry
-
-    const ensureChildType = (type: BlockFormat, extraWrapperAttrs?: {[key: string]: any}) => {
-      if (SlateElement.isElement(node)) {
-        for (const [child, childPath] of SlateNode.children(editor, path)) {
-          if (!SlateElement.isElement(child) || child.type !== type) {
-            Transforms.wrapNodes(
-              editor,
-              {type, children: child.children as SlateElement[], ...extraWrapperAttrs},
-              {
-                at: childPath,
-                mode: 'all'
-              }
-            )
-            return
-          }
-        }
-      }
-    }
-
-    const ensureParentType = (type: BlockFormat, extraWrapperAttrs?: {[key: string]: any}) => {
-      const parent = SlateNode.parent(editor, path)
-      if (!SlateElement.isElement(parent) || parent.type !== type) {
-        Transforms.wrapNodes(
-          editor,
-          {type, children: node.children as SlateElement[], ...extraWrapperAttrs}, // TODO simply =node  ?
-          {
-            at: path,
-            mode: 'all'
-          }
-        )
-      }
-    }
-
-    const ensureSubsequentType = (type: BlockFormat, extraWrapperAttrs?: {[key: string]: any}) => {
-      // Type of next Sibling node at the bottom
-      const pathOfSubsequent: Path = [path[0] + 1]
-      const subsequentNode = SlateNode.has(editor, pathOfSubsequent)
-        ? SlateNode.get(editor, pathOfSubsequent)
-        : null
-      if (
-        !subsequentNode ||
-        !SlateElement.isElement(subsequentNode) ||
-        subsequentNode.type !== BlockFormat.Paragraph
-      ) {
-        Transforms.insertNodes(editor, emptyTextParagraph(), {at: pathOfSubsequent})
-      }
-    }
-
     // const ensurePrecedentType = (type: BlockFormat, extraWrapperAttrs?: {[key: string]: any}) => {
     // if neede, use fn name
     // Type of previous Sibling node on top
@@ -88,6 +40,54 @@ export function withNormalizeNode<T extends ReactEditor>(editor: T): T {
         case BlockFormat.TableCell:
           ensureParentType(BlockFormat.TableRow)
           return
+      }
+      // TODO ensure ol / li structure (see at bottom)
+    }
+
+    function ensureChildType(type: BlockFormat, extraWrapperAttrs?: {[key: string]: any}) {
+      if (SlateElement.isElement(node)) {
+        for (const [child, childPath] of SlateNode.children(editor, path)) {
+          if (!SlateElement.isElement(child) || child.type !== type) {
+            Transforms.wrapNodes(
+              editor,
+              {type, children: child.children as SlateElement[], ...extraWrapperAttrs},
+              {
+                at: childPath,
+                mode: 'all'
+              }
+            )
+            return
+          }
+        }
+      }
+    }
+
+    function ensureParentType(type: BlockFormat, extraWrapperAttrs?: {[key: string]: any}) {
+      const parent = SlateNode.parent(editor, path)
+      if (!SlateElement.isElement(parent) || parent.type !== type) {
+        Transforms.wrapNodes(
+          editor,
+          {type, children: node.children as SlateElement[], ...extraWrapperAttrs}, // TODO simply =node  ?
+          {
+            at: path,
+            mode: 'all'
+          }
+        )
+      }
+    }
+
+    function ensureSubsequentType(type: BlockFormat, extraWrapperAttrs?: {[key: string]: any}) {
+      // Type of next Sibling node at the bottom
+      const pathOfSubsequent: Path = [path[0] + 1]
+      const subsequentNode = SlateNode.has(editor, pathOfSubsequent)
+        ? SlateNode.get(editor, pathOfSubsequent)
+        : null
+      if (
+        !subsequentNode ||
+        !SlateElement.isElement(subsequentNode) ||
+        subsequentNode.type !== BlockFormat.Paragraph
+      ) {
+        Transforms.insertNodes(editor, emptyTextParagraph(), {at: pathOfSubsequent})
       }
     }
 
