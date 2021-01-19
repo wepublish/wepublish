ff7621f05fb85f79c058e42d2141b1d3d604f06b
Nicolas Roos <nicolas.k.roos@gmail.com>
2021-01-19 13:25:18 +0100

    test(api): added basic test for mailchimp mail provider

---
+++ packages/api/__tests__/specs/mailchimpMailProvider.ts
@@ -0,0 +1,62 @@
+// import {Mandrill} from 'mandrill-api'
+// jest.mock('mandrill-api')
+const mockSend = jest.fn()
+jest.mock('mandrill-api', () => {
+  return {
+    Mandrill: jest.fn().mockImplementation(() => {
+      return {
+        messages: {
+          send: mockSend
+        }
+      }
+    })
+  }
+})
+
+import {MailchimpMailProvider} from '../../src/mails/MailchimpMailProvider'
+
+let mailChimpMailProvider: MailchimpMailProvider
+
+describe('Mailchimp Mail Provider', () => {
+  test('should be able to be created', () => {
+    mailChimpMailProvider = new MailchimpMailProvider({
+      baseURL: 'https://mailchimp.com',
+      apiKey: 'fakeAPIkey',
+      webhookEndpointSecret: 'fakeSecret',
+      fromAddress: 'dev@wepublish.ch',
+      id: 'mailchimp',
+      name: 'Mailchimp'
+    })
+
+    expect(mailChimpMailProvider).toBeDefined()
+  })
+
+  test('sendMail should call mandrill send', async () => {
+    await mailChimpMailProvider.sendMail({
+      message: 'hello Test',
+      subject: 'test subject',
+      recipient: 'test@test.com',
+      replyToAddress: 'dev@test.com',
+      mailLogID: 'fakeMailLogID'
+    })
+
+    expect(mockSend).toHaveBeenCalledTimes(1)
+    expect(mockSend).toHaveBeenCalledWith({
+      message: {
+        html: `<p>hello Test</p>`,
+        text: 'hello Test',
+        subject: 'test subject',
+        from_email: 'dev@wepublish.ch',
+        to: [
+          {
+            email: 'test@test.com',
+            type: 'to'
+          }
+        ],
+        metadata: {
+          mail_log_id: 'fakeMailLogID'
+        }
+      }
+    })
+  })
+})
