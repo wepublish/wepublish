b8f0a0493dd3bba90602b2bd195b91b82e309a49
bishoymelek <bishoymelekwadie@gmail.com>
2021-01-26 12:29:41 +0200

    remove JSX.Element return type

--- examples/website/src/shared/teasers/imageTeaser.tsx
+++ examples/website/src/shared/teasers/imageTeaser.tsx
@@ -125,7 +125,7 @@ export function ImageTeaser({
   lead,
   route,
   preTitle
-}: LightTeaserProps): JSX.Element {
+}: LightTeaserProps) {
   const css = useStyle({isSingle})
   return (
     <div className={css(ImageTeaserStyle)}>
