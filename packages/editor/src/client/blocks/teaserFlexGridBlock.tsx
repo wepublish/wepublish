4ae4c0847eb7bb8f04bd167a6e95fe3a532362bc
faebebin <fabianbndr@gmail.com>
2021-02-06 11:54:04 +0100

    First draft adding teaserFlexGridBlock component

---
+++ packages/editor/src/client/blocks/teaserFlexGridBlock.tsx
@@ -0,0 +1,351 @@
+import React, {useState, ReactNode} from 'react'
+import GridLayout from 'react-grid-layout'
+
+import {PlaceholderInput} from '../atoms/placeholderInput'
+import {PlaceholderImage} from '../atoms/placeholderImage'
+import {BlockProps} from '../atoms/blockList'
+import {Overlay} from '../atoms/overlay'
+import {Typography} from '../atoms/typography'
+
+import {IconButton, Drawer, Panel, Icon, Avatar} from 'rsuite'
+
+import './teaserFlexGridBlock.less'
+import nanoid from 'nanoid'
+
+import {TeaserFlexGridBlockValue, TeaserFlex, TeaserType} from './types'
+
+import {TeaserSelectAndEditPanel} from '../panel/teaserSelectAndEditPanel'
+import {TeaserEditPanel} from '../panel/teaserEditPanel'
+import {ImageRefFragment, TeaserStyle, PeerWithProfileFragment} from '../api'
+
+import {useTranslation} from 'react-i18next'
+
+interface GridProps {
+  numColumns: number
+  children?: ReactNode
+}
+
+export function TeaserFlexGridBlock({value, onChange}: BlockProps<TeaserFlexGridBlockValue>) {
+  const [editTeaser, setEditTeaser] = useState<TeaserFlex | null>(null) // TODO undefined instead null
+
+  const [isEditModalOpen, setEditModalOpen] = useState(false)
+  const [isChooseModalOpen, setChooseModalOpen] = useState(false)
+
+  const {teasers, numColumns, numRows} = value
+
+  // function handleTeaserLinkChange(index: number, teaserLink: TeaserFlex | null) {
+  //   onChange({
+  //     numColumns,
+  //     numRows,
+  //     teasers: Object.assign([], teasers, {
+  //       [index]: [nanoid(), teaserLink || null]
+  //     })
+  //   })
+  // }
+
+  // document.documentElement.style.cursor = 'grabbing'
+  // document.documentElement.style.cursor = ''
+  // document.body.style.pointerEvents = 'none'
+  // document.body.style.pointerEvents = ''
+  // disabled={teasers.length === 1}
+
+  return (
+    <>
+      <GridLayout className="layout" cols={12} rowHeight={30} width={1200}>
+        {teasers.map(([{i, ...layout}, teaser]) => (
+          <TeaserBlock
+            key={i}
+            data-grid={layout}
+            teaser={teaser}
+            numColumns={numColumns}
+            showGrabCursor={teasers.length !== 1}
+            onEdit={() => {
+              setEditTeaser(teaser)
+              setEditModalOpen(true)
+            }}
+            onChoose={() => {
+              setEditTeaser(teaser)
+              setChooseModalOpen(true)
+            }}
+            onRemove={() => {
+              // handleTeaserLinkChange(teaser, null)
+            }}
+          />
+        ))}
+      </GridLayout>
+      <Drawer show={isEditModalOpen} size={'sm'} onHide={() => setEditModalOpen(false)}>
+        <TeaserEditPanel
+          initialTeaser={editTeaser!}
+          // initialTeaser={editTeaser[1]!}
+          onClose={() => setEditModalOpen(false)}
+          onConfirm={teaser => {
+            setEditModalOpen(false)
+            // handleTeaserLinkChange(editIndex, teaser) TODO (maybe just extend type)
+          }}
+        />
+      </Drawer>
+      <Drawer show={isChooseModalOpen} size={'sm'} onHide={() => setChooseModalOpen(false)}>
+        <TeaserSelectAndEditPanel
+          onClose={() => setChooseModalOpen(false)}
+          onSelect={teaser => {
+            setChooseModalOpen(false)
+            // handleTeaserLinkChange(editIndex, teaser) TODO (maybe just extend type)
+          }}
+        />
+      </Drawer>
+    </>
+  )
+}
+
+export interface TeaserBlockProps {
+  teaser: TeaserFlex | null
+  showGrabCursor: boolean
+  numColumns: number
+  onEdit: () => void
+  onChoose: () => void
+  onRemove: () => void
+}
+
+export function TeaserBlock({
+  teaser,
+  numColumns,
+  showGrabCursor,
+  onEdit,
+  onChoose,
+  onRemove
+}: TeaserBlockProps) {
+  return (
+    <Panel
+      data-grid={1}
+      bodyFill={true}
+      style={{
+        cursor: showGrabCursor ? 'grab' : '',
+        height: 300,
+        overflow: 'hidden',
+        zIndex: 1
+      }}>
+      <PlaceholderInput onAddClick={onChoose}>
+        {teaser && (
+          <div
+            style={{
+              position: 'relative',
+              width: '100%',
+              height: '100%'
+            }}>
+            {contentForTeaser(teaser, numColumns)}
+
+            <div
+              style={{
+                position: 'absolute',
+                zIndex: 1,
+                right: 0,
+                top: 0
+              }}>
+              <IconButton
+                icon={<Icon icon="file" />}
+                onClick={onChoose}
+                style={{
+                  margin: 10
+                }}
+              />
+              <IconButton
+                icon={<Icon icon="pencil" />}
+                onClick={onEdit}
+                style={{
+                  margin: 10
+                }}
+              />
+              <IconButton
+                icon={<Icon icon="trash" />}
+                onClick={onRemove}
+                style={{
+                  margin: 10
+                }}
+              />
+            </div>
+          </div>
+        )}
+      </PlaceholderInput>
+    </Panel>
+  )
+}
+
+export function contentForTeaser(teaser: TeaserFlex, numColumns: number) {
+  const {t} = useTranslation()
+  switch (teaser.type) {
+    case TeaserType.Article: {
+      const states = []
+
+      if (teaser?.article?.draft) states.push(t('articleEditor.panels.stateDraft'))
+      if (teaser?.article?.pending) states.push(t('articleEditor.panels.statePending'))
+      if (teaser?.article?.published) states.push(t('articleEditor.panels.statePublished'))
+
+      return (
+        <TeaserContent
+          style={teaser.style}
+          image={teaser.image ?? teaser.article.latest.image ?? undefined}
+          preTitle={teaser.preTitle ?? teaser.article.latest.preTitle ?? undefined}
+          title={teaser.title ?? teaser.article.latest.title}
+          lead={teaser.lead ?? teaser.article.latest.lead ?? undefined}
+          states={states}
+          numColumns={numColumns}
+        />
+      )
+    }
+
+    case TeaserType.PeerArticle: {
+      const states = []
+
+      if (teaser?.article?.draft) states.push(t('articleEditor.panels.stateDraft'))
+      if (teaser?.article?.pending) states.push(t('articleEditor.panels.statePending'))
+      if (teaser?.article?.published) states.push(t('articleEditor.panels.statePublished'))
+
+      return (
+        <TeaserContent
+          style={teaser.style}
+          image={teaser.image ?? teaser.article?.latest.image ?? undefined}
+          preTitle={teaser.preTitle ?? teaser.article?.latest.preTitle ?? undefined}
+          title={teaser.title ?? teaser.article?.latest.title}
+          lead={teaser.lead ?? teaser.article?.latest.lead ?? undefined}
+          states={states}
+          peer={teaser.peer}
+          numColumns={numColumns}
+        />
+      )
+    }
+
+    case TeaserType.Page: {
+      const states = []
+
+      if (teaser?.page?.draft) states.push(t('articleEditor.panels.stateDraft'))
+      if (teaser?.page?.pending) states.push(t('articleEditor.panels.statePending'))
+      if (teaser?.page?.published) states.push(t('articleEditor.panels.statePublished'))
+
+      return (
+        <TeaserContent
+          style={teaser.style}
+          image={teaser.image ?? teaser.page.latest.image ?? undefined}
+          title={teaser.title ?? teaser.page.latest.title}
+          lead={teaser.lead ?? teaser.page.latest.description ?? undefined}
+          states={states}
+          numColumns={numColumns}
+        />
+      )
+    }
+
+    default:
+      return null
+  }
+}
+
+export interface TeaserContentProps {
+  style: TeaserStyle
+  preTitle?: string
+  title?: string
+  lead?: string
+  image?: ImageRefFragment
+  states?: string[]
+  peer?: PeerWithProfileFragment
+  numColumns: number
+}
+
+function labelForTeaserStyle(style: TeaserStyle) {
+  switch (style) {
+    case TeaserStyle.Default:
+      return 'Default'
+
+    case TeaserStyle.Light:
+      return 'Light'
+
+    case TeaserStyle.Text:
+      return 'Text'
+  }
+}
+
+export function TeaserContent({
+  style,
+  preTitle,
+  title,
+  lead,
+  image,
+  states,
+  peer,
+  numColumns
+}: TeaserContentProps) {
+  const label = labelForTeaserStyle(style)
+  const {t} = useTranslation()
+  const stateJoin = states?.join(' / ')
+
+  return (
+    <>
+      <div
+        style={{
+          position: 'absolute',
+          width: '100%',
+          height: '100%'
+        }}>
+        {image ? (
+          <img
+            style={{
+              width: '100%',
+              height: '100%'
+            }}
+            src={numColumns === 1 ? image.column1URL ?? '' : image.column6URL ?? ''}
+          />
+        ) : (
+          <PlaceholderImage />
+        )}
+      </div>
+
+      <Overlay bottom={0} width="100%" padding={10}>
+        <div
+          style={{
+            marginBottom: 10
+          }}>
+          {preTitle && (
+            <Typography variant="subtitle1" color="white" spacing="small" ellipsize>
+              {preTitle}
+            </Typography>
+          )}
+          <Typography variant="body2" color="white" spacing="small">
+            {title || t('articleEditor.panels.untitled')}
+          </Typography>
+          {lead && (
+            <Typography variant="subtitle1" color="white" ellipsize>
+              {lead}
+            </Typography>
+          )}
+        </div>
+        {peer && (
+          <div
+            style={{
+              display: 'flex',
+              marginBottom: 10
+            }}>
+            <Avatar src={peer.profile?.logo?.squareURL ?? undefined} circle />
+          </div>
+        )}
+        <div
+          style={{
+            display: 'flex',
+            flexWrap: 'wrap'
+          }}>
+          <div
+            style={{
+              flexShrink: 0,
+              marginRight: 10
+            }}>
+            <Typography variant="subtitle1" color="gray">
+              {t('articleEditor.panels.teaserStyle', {label})}
+            </Typography>
+          </div>
+          <div style={{flexShrink: 0}}>
+            <Typography variant="subtitle1" color="gray">
+              {t('articleEditor.panels.status', {stateJoin})}
+            </Typography>
+          </div>
+        </div>
+      </Overlay>
+    </>
+  )
+}
