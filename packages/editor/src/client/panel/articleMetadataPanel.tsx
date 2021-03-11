5b9583e5ab56f506dcbec047742289d230bc55b0
bishoymelek <bishoymelekwadie@gmail.com>
2021-03-11 18:44:16 +0200

    cleanup

--- packages/editor/src/client/panel/articleMetadataPanel.tsx
+++ packages/editor/src/client/panel/articleMetadataPanel.tsx
@@ -67,12 +67,7 @@ export interface ArticleMetadataPanelProps {
   onChange?(value: ArticleMetadata): void
 }
 
-export function ArticleMetadataPanel({
-  value,
-  infoData,
-  onClose,
-  onChange
-}: ArticleMetadataPanelProps) {
+export function ArticleMetadataPanel({value, infoData, onClose, onChange}: ArticleMetadataPanelProps) {
   const {
     preTitle,
     title,
@@ -124,12 +119,12 @@ export function ArticleMetadataPanel({
     fetchPolicy: 'network-only'
   })
 
-  const preTitleMax = 30
-  const seoTitleMax = 70
-  const titleMax = 140
-  const leadMax = 350
-  const socialMediaTitleMax = 100
-  const socialMediaDescriptionMax = 140
+  const preTitleMax = 30;
+  const seoTitleMax = 70;
+  const titleMax = 140;
+  const leadMax = 350;
+  const socialMediaTitleMax = 100;
+  const socialMediaDescriptionMax = 140;
 
   function currentContent() {
     switch (activeKey) {
@@ -141,9 +136,8 @@ export function ArticleMetadataPanel({
                 <Message showIcon type="info" description={t('pageEditor.panels.metadataInfo')} />
               </FormGroup>
               <FormGroup>
-                <ControlLabel>
-                  {t('articleEditor.panels.socialMediaTitle')}
-                  <label style={{float: 'right'}}> {value.socialMediaTitle?.length}</label>
+                <ControlLabel>{t('articleEditor.panels.socialMediaTitle')}
+                <label style={{float: 'right'}}> {value.socialMediaTitle?.length}</label>
                 </ControlLabel>
                 <FormControl
                   value={socialMediaTitle || ''}
@@ -152,17 +146,12 @@ export function ArticleMetadataPanel({
                   }}
                 />
                 {value.socialMediaTitle && value.socialMediaTitle!.length > socialMediaTitleMax && (
-                  <label style={{color: 'gold'}}>
-                    {t('articleEditor.panels.charCountWarning', {
-                      charCountWarning: socialMediaTitleMax
-                    })}
-                  </label>
-                )}
+                <label style={{color: 'gold'}}>{t("articleEditor.panels.charCountWarning",{charCountWarning: socialMediaTitleMax})}</label>
+                 ) }
               </FormGroup>
               <FormGroup>
-                <ControlLabel>
-                  {t('articleEditor.panels.socialMediaDescription')}
-                  <label style={{float: 'right'}}> {value.socialMediaDescription?.length} </label>
+                <ControlLabel>{t('articleEditor.panels.socialMediaDescription')}
+                <label style={{float: 'right'}}> {value.socialMediaDescription?.length} </label>
                 </ControlLabel>
                 <FormControl
                   rows={5}
@@ -172,14 +161,9 @@ export function ArticleMetadataPanel({
                     onChange?.({...value, socialMediaDescription})
                   }}
                 />
-                {value.socialMediaDescription &&
-                  value.socialMediaDescription!.length > socialMediaDescriptionMax && (
-                    <label style={{color: 'gold'}}>
-                      {t('articleEditor.panels.charCountWarning', {
-                        charCountWarning: socialMediaDescriptionMax
-                      })}
-                    </label>
-                  )}
+                { value.socialMediaDescription && value.socialMediaDescription!.length > socialMediaDescriptionMax && (
+                <label style={{color: 'gold'}}>{t("articleEditor.panels.charCountWarning",{charCountWarning: socialMediaDescriptionMax})}</label>
+                 ) }
               </FormGroup>
               <FormGroup>
                 <ControlLabel>{t('articleEditor.panels.socialMediaAuthors')}</ControlLabel>
@@ -219,41 +203,32 @@ export function ArticleMetadataPanel({
         return (
           <Panel>
             <Form fluid={true}>
-              <div style={{paddingBottom: '20px'}}>
-                {t('articleEditor.panels.totalCharCount', {totalCharCount: infoData.charCount})}
-              </div>
+            <div style={{paddingBottom: '20px'}}>{t("articleEditor.panels.totalCharCount",{totalCharCount: infoData.charCount})}</div>
               <FormGroup>
-                <ControlLabel>
-                  {t('articleEditor.panels.preTitle')}
-                  <label style={{float: 'right'}}> {`${value.preTitle.length}`} </label>
+                <ControlLabel>{t('articleEditor.panels.preTitle')}
+                <label style={{float: 'right'}}> {`${value.preTitle.length}`} </label>
                 </ControlLabel>
                 <FormControl
                   value={preTitle}
                   onChange={preTitle => onChange?.({...value, preTitle})}
                 />
-                {value.preTitle.length > preTitleMax && (
-                  <label style={{color: 'gold'}}>
-                    {t('articleEditor.panels.charCountWarning', {charCountWarning: preTitleMax})}
-                  </label>
-                )}
+                { value.preTitle.length > preTitleMax && (
+                <label style={{color: 'gold'}}>{t("articleEditor.panels.charCountWarning",{charCountWarning: preTitleMax})}</label>
+                 ) }
               </FormGroup>
               <FormGroup>
-                <ControlLabel>
-                  {t('articleEditor.panels.title')}
-                  <label style={{float: 'right'}}> {`${value.title.length}`} </label>
+                <ControlLabel>{t('articleEditor.panels.title')}
+                <label style={{float: 'right'}}> {`${value.title.length}`} </label>
                 </ControlLabel>
                 <FormControl value={title} onChange={title => onChange?.({...value, title})} />
                 <HelpBlock>{t('articleEditor.panels.titleHelpBlock')}</HelpBlock>
-                {value.title.length > titleMax && (
-                  <label style={{color: 'gold'}}>
-                    {t('articleEditor.panels.charCountWarning', {charCountWarning: titleMax})}
-                  </label>
-                )}
+                { value.title.length > titleMax && (
+                <label style={{color: 'gold'}}>{t("articleEditor.panels.charCountWarning",{charCountWarning: titleMax})}</label>
+                 ) }
               </FormGroup>
               <FormGroup>
-                <ControlLabel>
-                  {t('articleEditor.panels.lead')}
-                  <label style={{float: 'right'}}> {`${value.lead.length}`} </label>
+                <ControlLabel>{t('articleEditor.panels.lead')}
+                <label style={{float: 'right'}}> {`${value.lead.length}`} </label>
                 </ControlLabel>
                 <FormControl
                   rows={5}
@@ -264,16 +239,13 @@ export function ArticleMetadataPanel({
                   }}
                 />
                 <HelpBlock>{t('articleEditor.panels.leadHelpBlock')}</HelpBlock>
-                {value.lead.length > leadMax && (
-                  <label style={{color: 'gold'}}>
-                    {t('articleEditor.panels.charCountWarning', {charCountWarning: leadMax})}
-                  </label>
-                )}
+                { value.lead.length > leadMax && (
+                <label style={{color: 'gold'}}>{t("articleEditor.panels.charCountWarning",{charCountWarning: leadMax})}</label>
+                 ) }
               </FormGroup>
               <FormGroup>
-                <ControlLabel>
-                  {t('articleEditor.panels.seoTitle')}
-                  <label style={{float: 'right'}}> {`${value.seoTitle.length}`} </label>
+                <ControlLabel>{t('articleEditor.panels.seoTitle')}
+                <label style={{float: 'right'}}> {`${value.seoTitle.length}`} </label>
                 </ControlLabel>
                 <FormControl
                   value={seoTitle}
@@ -290,11 +262,10 @@ export function ArticleMetadataPanel({
                     </a>
                   </Trans>
                 </HelpBlock>
-                {value.seoTitle.length > seoTitleMax && (
-                  <label style={{color: 'gold'}}>
-                    {t('articleEditor.panels.charCountWarning', {charCountWarning: seoTitleMax})}
-                  </label>
-                )}
+                { value.seoTitle.length > seoTitleMax && (
+                <label style={{color: 'gold'}}>{t("articleEditor.panels.charCountWarning",{charCountWarning: seoTitleMax})}</label>
+                 ) }
+        
               </FormGroup>
               <FormGroup>
                 <ControlLabel>{t('articleEditor.panels.slug')}</ControlLabel>
