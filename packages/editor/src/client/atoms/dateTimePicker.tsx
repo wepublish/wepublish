3ca72aac90687126017d1b0196252efbffe2b4de
Penina Cheshin <penk85@gmail.com>
2021-04-08 14:40:29 +0200

    resolve linting errors

--- packages/editor/src/client/atoms/dateTimePicker.tsx
+++ packages/editor/src/client/atoms/dateTimePicker.tsx
@@ -33,39 +33,37 @@ export function DateTimePicker({
         <DatePicker
           style={{marginRight: 8}}
           placement="auto"
-          value={dateTime ? dateTime : undefined}
+          value={dateTime}
           cleanable={false}
           format="DD MMM YYYY"
           ranges={
-            dateRanges
-              ? dateRanges
-              : [
-                  {
-                    label: t('dateTimePicker.today'),
-                    value: new Date()
-                  },
-                  {
-                    label: t('dateTimePicker.tomorrow'),
-                    value: () => {
-                      const tomorrow = new Date()
-                      tomorrow.setDate(new Date().getDate() + 1)
-                      return tomorrow
-                    }
-                  },
-                  {
-                    label: t('dateTimePicker.nextSaturday'),
-                    value: () => {
-                      const nextSaturday = new Date()
-                      const remainingDaysInWeek = 6 - new Date().getDay()
-                      nextSaturday.setDate(
-                        remainingDaysInWeek
-                          ? nextSaturday.getDate() + remainingDaysInWeek
-                          : nextSaturday.getDate() + 7
-                      )
-                      return nextSaturday
-                    }
-                  }
-                ]
+            dateRanges ?? [
+              {
+                label: t('dateTimePicker.today'),
+                value: new Date()
+              },
+              {
+                label: t('dateTimePicker.tomorrow'),
+                value: () => {
+                  const tomorrow = new Date()
+                  tomorrow.setDate(new Date().getDate() + 1)
+                  return tomorrow
+                }
+              },
+              {
+                label: t('dateTimePicker.nextSaturday'),
+                value: () => {
+                  const nextSaturday = new Date()
+                  const remainingDaysInWeek = 6 - new Date().getDay()
+                  nextSaturday.setDate(
+                    remainingDaysInWeek
+                      ? nextSaturday.getDate() + remainingDaysInWeek
+                      : nextSaturday.getDate() + 7
+                  )
+                  return nextSaturday
+                }
+              }
+            ]
           }
           onChange={value => {
             if (dateTime && value) {
@@ -79,33 +77,31 @@ export function DateTimePicker({
         <DatePicker
           placement="auto"
           format="HH:mm"
-          value={dateTime ? dateTime : undefined}
+          value={dateTime}
           cleanable={false}
           ranges={
-            timeRanges
-              ? timeRanges
-              : [
-                  {
-                    label: t('dateTimePicker.now'),
-                    value: new Date()
-                  },
-                  {
-                    label: t('dateTimePicker.hour', {hour: '6'}),
-                    value: () => {
-                      const six = new Date()
-                      six.setHours(6, 0, 0)
-                      return six
-                    }
-                  },
-                  {
-                    label: t('dateTimePicker.hour', {hour: '14'}),
-                    value: () => {
-                      const two = new Date()
-                      two.setHours(14, 0, 0)
-                      return two
-                    }
-                  }
-                ]
+            timeRanges ?? [
+              {
+                label: t('dateTimePicker.now'),
+                value: new Date()
+              },
+              {
+                label: t('dateTimePicker.hour', {hour: '6'}),
+                value: () => {
+                  const six = new Date()
+                  six.setHours(6, 0, 0)
+                  return six
+                }
+              },
+              {
+                label: t('dateTimePicker.hour', {hour: '14'}),
+                value: () => {
+                  const two = new Date()
+                  two.setHours(14, 0, 0)
+                  return two
+                }
+              }
+            ]
           }
           onChange={value => {
             if (dateTime && value) {
