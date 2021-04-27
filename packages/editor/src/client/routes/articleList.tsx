1501e63f454bd5938f5a2f0a50f22ed5dff2870a
HanaAyman <hanaayman1997@gmail.com>
2021-04-27 10:05:33 +0200

    fix

--- packages/editor/src/client/routes/articleList.tsx
+++ packages/editor/src/client/routes/articleList.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react'
 // import {ShepherdTour, ShepherdTourContext, TourMethods} from 'react-shepherd'
 import Shepherd from 'shepherd.js'
 
