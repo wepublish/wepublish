92c7867473f1535cbc8e983914100b9beb3cefa3
HanaAyman <hanaayman1997@gmail.com>
2021-05-10 11:09:55 +0200

    fix

--- packages/editor/src/client/atoms/dateTimePicker.tsx
+++ packages/editor/src/client/atoms/dateTimePicker.tsx
@@ -90,9 +90,9 @@ export function DateTimePicker({
               {
                 label: t('dateTimePicker.hour', {hour: '5'}),
                 value: () => {
-                  const date = new Date()
-                  date.setHours(5, 0, 0)
-                  return date
+                  const time = new Date()
+                  time.setHours(5, 0, 0)
+                  return time
                 }
               },
               {
