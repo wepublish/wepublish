b40e1c71c3e6adb7a433bfffeb07c97a585bd311
bishoymelek <bishoymelekwadie@gmail.com>
2021-05-27 14:49:45 +0200

    cleanup

--- packages/api-db-mongodb/src/db/comment.ts
+++ packages/api-db-mongodb/src/db/comment.ts
@@ -124,6 +124,7 @@ export class MongoDBCommentAdapter implements DBCommentAdapter {
 
     const limitCount = Math.min(limit.count, MaxResultsPerPage)
     const sortDirection = limit.type === LimitType.First ? order : -order
+
     const cursorData = cursor.type !== InputCursorType.None ? Cursor.from(cursor.data) : undefined
 
     const expr =
