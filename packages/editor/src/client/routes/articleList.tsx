21a9ed8857ae0cb0324ede5567cbf0787c806108
Penina Cheshin <penk85@gmail.com>
2021-05-31 10:56:42 +0200

    fix time format

--- packages/editor/src/client/routes/articleList.tsx
+++ packages/editor/src/client/routes/articleList.tsx
@@ -138,19 +138,23 @@ export function ArticleList() {
             setSortOrder(sortType)
             setSortField(sortColumn)
           }}>
-          <Column width={200} align="left" resizable sortable>
-            <HeaderCell>{t('articles.overview.published')}</HeaderCell>
+          <Column width={210} align="left" resizable sortable>
+            <HeaderCell>{t('articles.overview.publicationDate')}</HeaderCell>
             <Cell dataKey="published">
               {(articleRef: ArticleRefFragment) =>
                 articleRef.published?.publishedAt
-                  ? new Date(articleRef.published.publishedAt).toDateString()
+                  ? `${new Date(articleRef.published.publishedAt).toDateString()} ${new Date(
+                      articleRef.published.publishedAt
+                    ).toLocaleTimeString()}`
                   : articleRef.pending?.publishAt
-                  ? new Date(articleRef.pending.publishAt).toDateString()
+                  ? `${new Date(articleRef.pending.publishAt).toDateString()} ${new Date(
+                      articleRef.pending.publishAt
+                    ).toLocaleTimeString()}`
                   : t('articles.overview.notPublished')
               }
             </Cell>
           </Column>
-          <Column width={200} align="left" resizable sortable>
+          <Column width={210} align="left" resizable sortable>
             <HeaderCell>{t('articles.overview.updated')}</HeaderCell>
             <Cell dataKey="modifiedAt">
               {({modifiedAt}: ArticleRefFragment) =>
