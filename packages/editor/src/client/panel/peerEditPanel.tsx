267e0b386ea28de0fa62692abc7281ce4f126bed
Penina Cheshin <penk85@gmail.com>
2021-06-01 10:37:46 +0200

    props name

--- packages/editor/src/client/panel/peerEditPanel.tsx
+++ packages/editor/src/client/panel/peerEditPanel.tsx
@@ -28,7 +28,7 @@ import {useTranslation} from 'react-i18next'
 import {DescriptionList, DescriptionListItem} from '../atoms/descriptionList'
 import {RichTextBlock} from '../blocks/richTextBlock/richTextBlock'
 
-export interface ImageEditPanelProps {
+export interface PeerEditPanelProps {
   id?: string
   hostURL: string
 
@@ -36,7 +36,7 @@ export interface ImageEditPanelProps {
   onSave?(): void
 }
 
-export function PeerEditPanel({id, hostURL, onClose, onSave}: ImageEditPanelProps) {
+export function PeerEditPanel({id, hostURL, onClose, onSave}: PeerEditPanelProps) {
   const [name, setName] = useState('')
   const [slug, setSlug] = useState('')
   const [urlString, setURLString] = useState('')
