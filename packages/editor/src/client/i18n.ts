3d08258a59d53ba0f3ff950c007410c65a337982
Nicolas Roos <nicolas.k.roos@gmail.com>
2021-07-19 09:28:40 +0200

    feat(editor): added french and german as language options

--- packages/editor/src/client/i18n.ts
+++ packages/editor/src/client/i18n.ts
@@ -3,12 +3,16 @@ import {initReactI18next} from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 
 import en from './locales/en.json'
+import de from './locales/de.json'
+import fr from './locales/fr.json'
 
 i18n.use(LanguageDetector).use(initReactI18next).init({
   fallbackLng: 'en',
   debug: false,
   resources: {
-    en
+    en,
+    de,
+    fr
   }
 })
 
