a74b7c681c6cad952ea7c3c1a595f5abf34fe019
Nicolas Roos <nicolas.k.roos@gmail.com>
2021-08-04 13:11:44 +0200

    chore(app): update issuer

--- config/create-gke-manifest.js
+++ config/create-gke-manifest.js
@@ -808,6 +808,7 @@ async function applyApiServer() {
         'nginx.ingress.kubernetes.io/ssl-redirect': 'true',
         'nginx.ingress.kubernetes.io/proxy-body-size': '10m',
         'nginx.ingress.kubernetes.io/proxy-read-timeout': '30',
+        'cert-manager.io/issuer': 'wepublish-dev-prod-issuer'
         // 'cert-manager.io/cluster-issuer': 'letsencrypt-production'
       }
     },
