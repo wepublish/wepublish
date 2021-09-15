e3819cf0e127df2301483fc0ceff22ce57d6a581
Camille Ory <camilleory@Camilles-MacBook-Air.local>
2021-09-15 10:35:15 +0200

    fns-set up

--- packages/editor/src/client/i18n.ts
+++ packages/editor/src/client/i18n.ts
@@ -2,27 +2,26 @@ import i18n from 'i18next'
 import {initReactI18next} from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 
+import {de as deLocale} from 'date-fns/locale'
+
 import en from './locales/en.json'
 import de from './locales/de.json'
 import fr from './locales/fr.json'
-import moment from 'moment'
+// import moment from 'moment'
 
 import {format as formatDate, isDate} from 'date-fns'
 
-import {nl} from 'date-fns/locale' // import all locales we need
-
-const locales = {en, nl} // used to look up the required locale
-
-// i18n.init({
-interpolation: {
-  format: (value, format, lng) => {
-    if (isDate(value)) {
-      const locale = locales[lng]
-      return formatDate(value, format, {locale})
+i18n.init({
+  interpolation: {
+    format: (value, format, lng) => {
+      if (isDate(value) && format !== undefined && lng !== undefined) {
+        const locale = locales[lng]
+        return formatDate(value, format, {locale})
+      }
     }
   }
-}
-// })
+})
+
 // i18n.init({
 //   interpolation: {
 //     format: function (value, format, lng) {
@@ -42,8 +41,8 @@ i18n.use(LanguageDetector).use(initReactI18next).init({
   }
 })
 
-i18n.on('languageChanged', function (lng) {
-  moment.locale(lng)
-})
+// i18n.on('languageChanged', function (lng) {
+//   moment.locale(lng)
+// })
 
 export default i18n
