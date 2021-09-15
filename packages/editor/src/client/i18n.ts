5be2ede3688a8152b9bedb5a95c30e9bc0161812
Camille Ory <camilleory@Camilles-MacBook-Air.local>
2021-09-15 11:06:26 +0200

    format date

--- packages/editor/src/client/i18n.ts
+++ packages/editor/src/client/i18n.ts
@@ -2,25 +2,25 @@ import i18n from 'i18next'
 import {initReactI18next} from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 
-import {de as deLocale} from 'date-fns/locale'
+// import {de as deLocale} from 'date-fns/locale'
 
 import en from './locales/en.json'
 import de from './locales/de.json'
 import fr from './locales/fr.json'
 // import moment from 'moment'
 
-import {format as formatDate, isDate} from 'date-fns'
+// import {format as formatDate, isDate} from 'date-fns'
 
-i18n.init({
-  interpolation: {
-    format: (value, format, lng) => {
-      if (isDate(value) && format !== undefined && lng !== undefined) {
-        const locale = locales[lng]
-        return formatDate(value, format, {locale})
-      }
-    }
-  }
-})
+// i18n.init({
+//   interpolation: {
+//     format: (value, format, lng) => {
+//       if (isDate(value) && format !== undefined && lng !== undefined) {
+//         const locale = locales[lng]
+//         return formatDate(value, format, {locale})
+//       }
+//     }
+//   }
+// })
 
 // i18n.init({
 //   interpolation: {
