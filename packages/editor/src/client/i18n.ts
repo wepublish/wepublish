8264008b6335fc902acd499376b317ba1cb392bd
Camille Ory <camilla.ory@gmail.com>
2021-09-21 17:51:19 +0200

    ready to review

--- packages/editor/src/client/i18n.ts
+++ packages/editor/src/client/i18n.ts
@@ -5,19 +5,8 @@ import LanguageDetector from 'i18next-browser-languagedetector'
 import en from './locales/en.json'
 import de from './locales/de.json'
 import fr from './locales/fr.json'
-import moment from 'moment'
-
-i18n.init({
-  interpolation: {
-    format: function (value, format, lng) {
-      if (value instanceof Date) return moment(value).format(format)
-      return value
-    }
-  }
-})
 
 import {format as formatDate, isDate} from 'date-fns'
-
 import {de as deLocale, enGB as enLocale, fr as frLocale} from 'date-fns/locale'
 
 export interface DateFormatLocale {
@@ -52,8 +41,8 @@ i18n.use(LanguageDetector).use(initReactI18next).init({
   }
 })
 
-i18n.on('languageChanged', function (lng) {
-  moment.locale(lng)
-})
+// i18n.on('languageChanged', function (lng) {
+//   moment.locale(lng)
+// })
 
 export default i18n
