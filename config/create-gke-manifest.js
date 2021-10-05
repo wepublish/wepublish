f500f6e2fc23e6730738f69dbb198664ca14be62
Nicolas Roos <nicolas.k.roos@gmail.com>
2021-10-05 19:16:35 +0200

    chore(app): wildcard test

--- config/create-gke-manifest.js
+++ config/create-gke-manifest.js
@@ -835,7 +835,7 @@ async function applyApiServer() {
       tls: [
         {
           hosts: [domainAPI],
-          secretName: `wildcard-wepublish-dev-tls`
+          secretName: `development-wepublish-wildcard-tls`
         }
       ]
     }
