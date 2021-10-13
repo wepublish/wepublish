66891758e889bd5adfef47244e2a8f02126324f9
Nicolas Roos <nicolas.k.roos@gmail.com>
2021-10-13 20:41:35 +0200

    test(app): debugging

--- tests/e2e.ts
+++ tests/e2e.ts
@@ -1,6 +1,6 @@
 import { ClientFunction, Role, Selector, t } from "testcafe";
-// import {slugify} from '../config/utilities'
-// import * as process from "process";
+import {slugify} from '../config/utilities'
+import * as process from "process";
 
 const loginName = Selector('input.username')
 const loginPassword = Selector('input.password')
@@ -19,14 +19,14 @@ const articleTitleInput = Selector('textarea').withAttribute('placeholder', 'Tit
 const articleLeadInput = Selector('textarea').withAttribute('placeholder', 'Lead Text ')
 
 
-/*const EDITOR_URL = process.env.BRANCH_NAME ? `https://editor.${slugify(process.env.BRANCH_NAME.substring(0,12))}.wepublish.dev` : process.env.E2E_TEST_EDITOR_URL
-const WEBSITE_URL = process.env.BRANCH_NAME ? `https://www.${slugify(process.env.BRANCH_NAME.substring(0,12))}.wepublish.dev` : process.env.E2E_TEST_WEBSITE_URL*/
+const EDITOR_URL = process.env.BRANCH_NAME ? `https://editor.${slugify(process.env.BRANCH_NAME.substring(0,12))}.wepublish.dev` : process.env.E2E_TEST_EDITOR_URL
+const WEBSITE_URL = process.env.BRANCH_NAME ? `https://www.${slugify(process.env.BRANCH_NAME.substring(0,12))}.wepublish.dev` : process.env.E2E_TEST_WEBSITE_URL
 
 // const EDITOR_URL = 'https://editor.ftestingin.wepublish.dev'
 // const WEBSITE_URL = 'https://www.ftestingin.wepublish.dev'
 
-const EDITOR_URL = 'http://127.0.0.1:3000'
-const WEBSITE_URL = 'http://127.0.0.1:5000'
+// const EDITOR_URL = 'http://127.0.0.1:3000'
+// const WEBSITE_URL = 'http://127.0.0.1:5000'
 
 export default async function consoleOut() {
   const { error } = await t.getBrowserConsoleMessages();
