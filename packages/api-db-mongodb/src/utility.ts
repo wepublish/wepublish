578c276c871209dcb95f5ea953c9ef72f77fe242
bishoy melek <bishoymelekwadie@gmail.com>
2021-10-27 16:22:52 +0200

    fixes in transforming richtext to str

--- packages/api-db-mongodb/src/utility.ts
+++ packages/api-db-mongodb/src/utility.ts
@@ -40,7 +40,13 @@ export function richTextToString(blocksString: string, nodes: any = []) {
   const richTextNode = nodes.reduce((string: string, node: any) => {
     if (!node.text && !node.children) return string
     if (node.text) {
-      return `${string} ${node.text}`
+      if (string.endsWith(' ') || node.text.startsWith(' ')) {
+        return `${string}${node.text}`
+      } else if (string === '') {
+        return `${node.text}`
+      } else {
+        return `${string} ${node.text}`
+      }
     }
     return richTextToString(string, node.children)
   }, blocksString)
