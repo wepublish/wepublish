e2c5147fccb0133f048453056367845554cc42b7
Camille Ory <camilla.ory@gmail.com>
2021-11-11 13:53:17 +0100

    feat(editor): removed line added by mistake

--- packages/editor/src/client/panel/publishArticlePanel.tsx
+++ packages/editor/src/client/panel/publishArticlePanel.tsx
@@ -7,7 +7,6 @@ import {Button, Message, Modal} from 'rsuite'
 
 import {DescriptionList, DescriptionListItem} from '../atoms/descriptionList'
 import {DateTimePicker} from '../atoms/dateTimePicker'
-import {appendFile} from 'fs'
 
 export interface PublishArticlePanelProps {
   initialPublishDate?: Date
