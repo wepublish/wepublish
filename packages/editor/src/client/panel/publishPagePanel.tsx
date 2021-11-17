32d14b0190658c2224430c173b4e5c751b65811b
Camille Ory <camilla.ory@gmail.com>
2021-11-17 10:44:51 +0100

    clean up

--- packages/editor/src/client/panel/publishPagePanel.tsx
+++ packages/editor/src/client/panel/publishPagePanel.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 
-import {Button, HelpBlock, Message, Modal} from 'rsuite'
+import {Button, Message, Modal} from 'rsuite'
 
 import {DescriptionList, DescriptionListItem} from '../atoms/descriptionList'
 
