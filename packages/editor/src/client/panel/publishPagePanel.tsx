0713f6ed70006352aee62e5842814aefaa4482c8
Camille Ory <camilla.ory@gmail.com>
2021-11-17 10:39:09 +0100

    missing fields highlighted in orange in page publish panel

--- packages/editor/src/client/panel/publishPagePanel.tsx
+++ packages/editor/src/client/panel/publishPagePanel.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 
-import {Button, Message, Modal} from 'rsuite'
+import {Button, HelpBlock, Message, Modal} from 'rsuite'
 
 import {DescriptionList, DescriptionListItem} from '../atoms/descriptionList'
 
@@ -34,6 +34,12 @@ export function PublishPagePanel({
 
   const {t} = useTranslation()
 
+  const missingInfoStyle = {
+    borderRadius: '8px',
+    padding: '6px',
+    backgroundColor: '#FFEBCD'
+  }
+
   return (
     <>
       <Modal.Header>
@@ -60,16 +66,57 @@ export function PublishPagePanel({
 
         <DescriptionList>
           <DescriptionListItem label={t('pageEditor.panels.title')}>
-            {metadata.title}
+            {!metadata.title ? (
+              <div style={missingInfoStyle}>{t('pageEditor.panels.enterTitle')}</div>
+            ) : (
+              metadata.title
+            )}
           </DescriptionListItem>
           <DescriptionListItem label={t('pageEditor.panels.description')}>
-            {metadata.description}
+            {!metadata.description ? (
+              <div style={missingInfoStyle}>{t('pageEditor.panels.enterDescription')}</div>
+            ) : (
+              metadata.description
+            )}{' '}
           </DescriptionListItem>
           <DescriptionListItem label={t('pageEditor.panels.slug')}>
-            {metadata.slug}
+            {metadata.slug || '-'}
           </DescriptionListItem>
           <DescriptionListItem label={t('pageEditor.panels.tags')}>
-            {metadata.tags.join(', ')}
+            {metadata.tags.join(', ') || '-'}
+          </DescriptionListItem>
+          <DescriptionListItem label={t('pageEditor.panels.image')}>
+            {!metadata.image ? (
+              <div style={missingInfoStyle}>{t('pageEditor.panels.enterImage')}</div>
+            ) : (
+              metadata.image.filename
+            )}
+          </DescriptionListItem>
+
+          <DescriptionListItem label={t('pageEditor.panels.socialMediaTitle')}>
+            {!metadata.socialMediaTitle ? (
+              <div style={missingInfoStyle}>{t('pageEditor.panels.enterSocialMediaTitle')}</div>
+            ) : (
+              metadata.socialMediaTitle
+            )}
+          </DescriptionListItem>
+
+          <DescriptionListItem label={t('pageEditor.panels.socialMediaDescription')}>
+            {!metadata.socialMediaDescription ? (
+              <div style={missingInfoStyle}>
+                {t('pageEditor.panels.enterSocialMediaDescription')}
+              </div>
+            ) : (
+              metadata.socialMediaDescription
+            )}
+          </DescriptionListItem>
+
+          <DescriptionListItem label={t('pageEditor.panels.socialMediaImage')}>
+            {!metadata.socialMediaImage ? (
+              <div style={missingInfoStyle}>{t('pageEditor.panels.enterSocialMediaImage')}</div>
+            ) : (
+              metadata.socialMediaImage.filename
+            )}
           </DescriptionListItem>
         </DescriptionList>
       </Modal.Body>
