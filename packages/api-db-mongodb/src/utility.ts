79bf9a7c8642b86bd066ba3b4899c1603630c643
Nicolas Roos <nicolas.k.roos@gmail.com>
2021-12-01 16:17:08 +0100

    feat(app): added new field slug to paymentMethod. Display slug in editor. Migration for existing paymentMethods

--- packages/api-db-mongodb/src/utility.ts
+++ packages/api-db-mongodb/src/utility.ts
@@ -35,3 +35,40 @@ export enum MongoErrorCode {
 export function escapeRegExp(string: string) {
   return string.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&')
 }
+
+// copied from packages/editor
+export function slugify(str: string) {
+  return str
+    .toLowerCase()
+    .trim()
+    .replace(/[ÀÁÂÃÄÅÆĀĂĄẠẢẤẦẨẪẬẮẰẲẴẶ]/gi, 'a')
+    .replace(/[ÇĆĈČ]/gi, 'c')
+    .replace(/[ÐĎĐÞ]/gi, 'd')
+    .replace(/[ÈÉÊËĒĔĖĘĚẸẺẼẾỀỂỄỆ]/gi, 'e')
+    .replace(/[ĜĞĢǴ]/gi, 'g')
+    .replace(/[ĤḦ]/gi, 'h')
+    .replace(/[ÌÍÎÏĨĪĮİỈỊ]/gi, 'i')
+    .replace(/[Ĵ]/gi, 'j')
+    .replace(/[Ĳ]/gi, 'ij')
+    .replace(/[Ķ]/gi, 'k')
+    .replace(/[ĹĻĽŁ]/gi, 'l')
+    .replace(/[Ḿ]/gi, 'm')
+    .replace(/[ÑŃŅŇ]/gi, 'n')
+    .replace(/[ÒÓÔÕÖØŌŎŐỌỎỐỒỔỖỘỚỜỞỠỢǪǬƠ]/gi, 'o')
+    .replace(/[Œ]/gi, 'oe')
+    .replace(/[ṕ]/gi, 'p')
+    .replace(/[ŔŖŘ]/gi, 'r')
+    .replace(/[ßŚŜŞŠ]/gi, 's')
+    .replace(/[ŢŤ]/gi, 't')
+    .replace(/[ÙÚÛÜŨŪŬŮŰŲỤỦỨỪỬỮỰƯ]/gi, 'u')
+    .replace(/[ẂŴẀẄ]/gi, 'w')
+    .replace(/[ẍ]/gi, 'x')
+    .replace(/[ÝŶŸỲỴỶỸ]/gi, 'y')
+    .replace(/[ŹŻŽ]/gi, 'z')
+    .replace(/[·/_,:;\\']/gi, '-')
+    .replace(/\s+/g, '-')
+    .replace(/[^\w\-]+/g, '') //eslint-disable-line
+    .replace(/--+/g, '-')
+    .replace(/^-+/, '')
+    .replace(/-+$/, '')
+}
