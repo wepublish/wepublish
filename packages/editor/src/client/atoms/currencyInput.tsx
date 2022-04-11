ae0c84a3efa8d42e29563d401e1825f3af775d12
Camille Ory <camilla.ory@gmail.com>
2022-04-11 11:50:28 +0200

    regex validation

--- packages/editor/src/client/atoms/currencyInput.tsx
+++ packages/editor/src/client/atoms/currencyInput.tsx
@@ -1,5 +1,6 @@
+import {text} from 'express'
 import React, {useState, useEffect} from 'react'
-import {Input, InputGroup} from 'rsuite'
+import {Input, InputGroup, Message} from 'rsuite'
 
 export interface CurrencyInputProps {
   currency: string
@@ -17,10 +18,13 @@ export function CurrencyInput({
   onChange
 }: CurrencyInputProps) {
   const [amount, setAmount] = useState<number | string | any>(centAmount / 100)
+  const [message, setMessage] = useState(false)
 
   useEffect(() => {
     setAmount((centAmount / 100).toFixed(2))
   }, [centAmount])
+
+  const toFloat = (text: String) => text.replace(/[^0-9,\.?]/gm, '').replace(',', '.')
 
   return (
     <div>
@@ -29,33 +33,26 @@ export function CurrencyInput({
         <Input
           value={amount as string}
           step={step}
-          type="number"
+          pattern={/[+-]?([0-9]*[.]{0,1})?[0-9]+/}
           disabled={disabled}
           lang="en"
           onChange={amount => {
-            setAmount(parseFloat(amount))
-            console.log(amount.replace(',', '.'))
+            if (!amount || !toFloat(amount).match(/[+-]?([0-9]*[.]{0,1})?[0-9]+/)) {
+              setMessage(true)
+              setAmount('')
+            } else {
+              setMessage(false)
+              setAmount(toFloat(amount))
+            }
           }}
           onBlur={() => {
-            onChange(Math.round(parseFloat(amount as string) * 100))
+            if (amount) {
+              onChange(Math.round(parseFloat(amount as string) * 100))
+            }
           }}
         />
-
-        {/* <input
-          value={amount}
-          step={step}
-          type="number"
-          disabled={disabled}
-          lang='en'
-          onChange={amount => {
-            setAmount(parseFloat(amount))
-            console.log(amount)
-          }}
-          onBlur={() => {
-            onChange(Math.round(parseFloat(amount) * 100))
-          }}> */}
-        {/* </input> */}
       </InputGroup>
+      {message ? <Message type="error" description=" Please enter a number" /> : ''}
     </div>
   )
 }
