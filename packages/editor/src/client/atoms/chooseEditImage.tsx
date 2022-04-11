6a77086cf5d4671640bda504414f58cf414cb819
Penina Cheshin <penk85@gmail.com>
2022-04-11 16:20:08 +0200

    add max height

--- packages/editor/src/client/atoms/chooseEditImage.tsx
+++ packages/editor/src/client/atoms/chooseEditImage.tsx
@@ -27,7 +27,7 @@ export function ChooseEditImage({
   const {t} = useTranslation()
   header = header ?? t('chooseEditImage.header')
   return (
-    <Panel header={header} bodyFill={true} style={{height: 450, marginBottom: 10}}>
+    <Panel header={header} bodyFill={true}>
       {!image && disabled === true && <Placeholder.Graph />}
       <PlaceholderInput onAddClick={() => openChooseModalOpen?.()}>
         {image && (
