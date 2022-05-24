9bd8fc0afe775f370db1ff0463a2952e932114ad
Nicolas Roos <nicolas.k.roos@gmail.com>
2022-05-24 20:50:04 +0200

    fix(api): fixed sorting for subscription

--- packages/api/src/graphql/subscription.ts
+++ packages/api/src/graphql/subscription.ts
@@ -9,7 +9,6 @@ import {
   GraphQLBoolean,
   GraphQLID
 } from 'graphql'
-import {UserSort} from '../db/user'
 import {
   GraphQLDateFilter,
   GraphQLMetadataProperty,
@@ -21,7 +20,7 @@ import {Context} from '../context'
 import {GraphQLDateTime} from 'graphql-iso-date'
 import {GraphQLMemberPlan, GraphQLPaymentPeriodicity, GraphQLPublicMemberPlan} from './memberPlan'
 import {GraphQLPaymentMethod, GraphQLPublicPaymentMethod} from './paymentMethod'
-import {Subscription, SubscriptionDeactivationReason} from '../db/subscription'
+import {Subscription, SubscriptionDeactivationReason, SubscriptionSort} from '../db/subscription'
 import {GraphQLUser} from './user'
 import {NotFound} from '../error'
 import {isTempUser, removePrefixTempUser} from '../utility'
@@ -149,8 +148,8 @@ export const GraphQLSubscriptionFilter = new GraphQLInputObjectType({
 export const GraphQLSubscriptionSort = new GraphQLEnumType({
   name: 'SubscriptionSort',
   values: {
-    CREATED_AT: {value: UserSort.CreatedAt},
-    MODIFIED_AT: {value: UserSort.ModifiedAt}
+    CREATED_AT: {value: SubscriptionSort.CreatedAt},
+    MODIFIED_AT: {value: SubscriptionSort.ModifiedAt}
   }
 })
 
