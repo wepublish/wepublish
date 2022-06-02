694b936adba036b24575cbd42095f4b02e3db3a7
Karl Merkli <karlmerkli@gmail.com>
2022-06-02 15:28:03 +0200

    test(editor): replace enzyme with @testing-library/react to fix tests (#613)

--- packages/editor/__tests__/setup.ts
+++
@@ -1,5 +0,0 @@
-import Enzyme from 'enzyme'
-// Experimental React 17 Enzyme adapter
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
-
-Enzyme.configure({adapter: new Adapter()})
--- packages/editor/__tests__/utils.ts
+++ packages/editor/__tests__/utils.ts
@@ -1,23 +1,14 @@
 // https://github.com/wesbos/waait/blob/master/index.js
-import {act} from 'react-dom/test-utils'
-import Enzyme from 'enzyme'
+import {act} from '@testing-library/react'
 
 export function wait(amount = 0) {
   return new Promise(resolve => setTimeout(resolve, amount))
 }
 
-// Use this in your test after mounting if you need just need to let the query finish without updating the wrapper
+// Use this in your test after mounting if you need just need to let the query finish
 export async function actWait(amount = 0) {
   await act(async () => {
     await wait(amount)
-  })
-}
-
-// Use this in your test after mounting if you want the query to finish and update the wrapper
-export async function updateWrapper(wrapper: Enzyme.ReactWrapper, amount = 0) {
-  await act(async () => {
-    await wait(amount)
-    wrapper.update()
   })
 }
 
