081ed7ddac5cbf8092a8624e2abbc885493d0521
Karl Merkli <karlmerkli@gmail.com>
2022-08-04 12:23:39 +0200

    fix(api): challenge validUntil should be a date

--- packages/api/src/graphql/challenge.ts
+++ packages/api/src/graphql/challenge.ts
@@ -1,11 +1,12 @@
 import {GraphQLObjectType, GraphQLString} from 'graphql'
 import {Context} from '../context'
+import {GraphQLDate} from 'graphql-iso-date'
 
 export const GraphQLChallenge: GraphQLObjectType<Context> = new GraphQLObjectType<Context>({
   name: 'Challenge',
   fields: () => ({
     challenge: {type: GraphQLString},
     challengeID: {type: GraphQLString},
-    validUntil: {type: GraphQLString}
+    validUntil: {type: GraphQLDate}
   })
 })
