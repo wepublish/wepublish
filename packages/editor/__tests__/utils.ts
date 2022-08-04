a9f0fd0baaf535eb421c9292cf4741e466a8a755
Penina Cheshin <penk85@gmail.com>
2022-08-04 11:31:29 +0200

    add create permissions to tests

--- packages/editor/__tests__/utils.ts
+++ packages/editor/__tests__/utils.ts
@@ -20,31 +20,57 @@ export function pTest<T extends Named>(name: string, cases: T[], test: (testCase
   describe(name, () => cases.forEach((t: T) => it(t.name, () => test(t))))
 }
 
-const CanGetAuthor = {
+export const CanGetAuthor = {
   id: 'CAN_GET_AUTHOR',
   description: 'Allows to get author',
   deprecated: false
 }
 
-const CanGetPeers = {
+export const CanCreateAuthor = {
+  id: 'CAN_CREATE_AUTHOR',
+  description: 'Allows to create author',
+  deprecated: false
+}
+
+export const CanGetPeers = {
   id: 'CAN_GET_PEERS',
   description: 'Allows to get all peers',
   deprecated: false
 }
 
-const CanCreateToken = {
+export const CanCreatePeer = {
+  id: 'CAN_CREATE_PEER',
+  description: 'Allows to create peer',
+  deprecated: false
+}
+
+export const CanCreateToken = {
   id: 'CAN_CREATE_TOKEN',
   description: 'Allows to create tokens',
   deprecated: false
 }
 
-const CanGetUserRole = {
+export const CanGetUserRole = {
   id: 'CAN_GET_USER_ROLE',
   description: 'Allows to get an user role',
   deprecated: false
 }
 
-export const AllPermissions = [CanGetAuthor, CanGetPeers, CanCreateToken, CanGetUserRole]
+export const CanCreateUserRole = {
+  id: 'CAN_CREATE_USER_ROLE',
+  description: 'Allows to create a user role',
+  deprecated: false
+}
+
+export const AllPermissions = [
+  CanGetAuthor,
+  CanCreateAuthor,
+  CanGetPeers,
+  CanCreatePeer,
+  CanCreateToken,
+  CanGetUserRole,
+  CanCreateUserRole
+]
 
 const adminRole = {
   id: 'admin',
