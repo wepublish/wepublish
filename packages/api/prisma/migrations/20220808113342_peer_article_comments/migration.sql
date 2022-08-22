aaee524d5d01a44c1215cb86cbc93d4af85ca7a2
Itrulia <karlmerkli@gmail.com>
2022-08-22 12:10:41 +0200

    Delete migration.sql

--- packages/api/prisma/migrations/20220808113342_peer_article_comments/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterEnum
-ALTER TYPE "CommentItemType" ADD VALUE 'peerArticle';
-
--- AlterTable
-ALTER TABLE "comments" ADD COLUMN     "peerId" TEXT;
-
--- AddForeignKey
-ALTER TABLE "comments" ADD CONSTRAINT "comments_peerId_fkey" FOREIGN KEY ("peerId") REFERENCES "peers"("id") ON DELETE SET NULL ON UPDATE CASCADE;
