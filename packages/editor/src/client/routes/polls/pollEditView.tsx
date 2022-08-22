3e0a85d0a52958d8a868610c544c87b10c7fdd97
michael-scheurer <michael.scheurer@seccom.ch>
2022-08-22 17:32:09 +0200

    add first input field

--- packages/editor/src/client/routes/polls/pollEditView.tsx
+++ packages/editor/src/client/routes/polls/pollEditView.tsx
@@ -1,13 +1,14 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import {useTranslation} from 'react-i18next'
 import {useParams} from 'react-router-dom'
-import {FlexboxGrid, Message, toaster} from 'rsuite'
+import {Col, FlexboxGrid, Form, Message, Row, toaster} from 'rsuite'
 
-import {usePollQuery} from '../../api'
+import {FullPoll, usePollQuery} from '../../api'
 import {ModelTitle} from '../../atoms/modelTitle'
 
 export function PollEditView() {
   const params = useParams()
+  const [poll, setPoll] = useState<FullPoll | undefined>(undefined)
 
   const {data, loading, error} = usePollQuery({
     variables: {
@@ -29,20 +30,63 @@ export function PollEditView() {
     }
   }, [error])
 
+  /**
+   * Watching poll
+   */
+  useEffect(() => {
+    if (data?.poll) {
+      setPoll(data.poll)
+    } else {
+      setPoll(undefined)
+    }
+  }, [data])
+
+  /**
+   * FUNCTION
+   */
+
+  // todo: implement
+  function saveOrUpdate() {}
+
   return (
     <>
-      <FlexboxGrid>
-        <FlexboxGrid.Item colspan={24}>
-          <ModelTitle
-            loading={loading}
-            title={data?.poll?.question || t('pollList.noQuestion')}
-            loadingTitle={t('pollEditView.loadingTitle')}
-            saveTitle={t('pollEditView.saveTitle')}
-            saveAndCloseTitle={t('pollEditView.saveAndCloseTitle')}
-            closePath="/polls"
-          />
-        </FlexboxGrid.Item>
-      </FlexboxGrid>
+      <Form onSubmit={validationPassed => validationPassed && saveOrUpdate()}>
+        <FlexboxGrid>
+          {/* model title */}
+          <FlexboxGrid.Item colspan={24}>
+            <ModelTitle
+              loading={loading}
+              title={poll?.question || t('pollList.noQuestion')}
+              loadingTitle={t('pollEditView.loadingTitle')}
+              saveTitle={t('pollEditView.saveTitle')}
+              saveAndCloseTitle={t('pollEditView.saveAndCloseTitle')}
+              closePath="/polls"
+            />
+          </FlexboxGrid.Item>
+
+          {/* content */}
+          <FlexboxGrid.Item colspan={12}>
+            <Row style={{width: '100%'}}>
+              <Col xs={24}>
+                <Form.Group controlId="question">
+                  <Form.ControlLabel>{t('pollEditView.question')}</Form.ControlLabel>
+                  <Form.Control
+                    name="question"
+                    value={poll?.question || undefined}
+                    disabled={loading}
+                    onChange={(value: string) => {
+                      if (!poll) {
+                        return
+                      }
+                      setPoll({...poll, question: value})
+                    }}
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+          </FlexboxGrid.Item>
+        </FlexboxGrid>
+      </Form>
     </>
   )
 }
