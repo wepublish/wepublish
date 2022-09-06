c0db11c54bcd7ac552ffbae8faa2129803804a05
Tomasz Durka <tomasz@durka.pl>
2022-09-06 16:23:03 +0200

    Make sure wepublish/api builds

--- packages/api/src/media/karmaMediaAdapter.ts
+++ packages/api/src/media/karmaMediaAdapter.ts
@@ -1,9 +1,8 @@
 import {URL} from 'url'
 import FormData from 'form-data'
-import {ImageTransformation, UploadImage} from '../db/image'
+import {ImageTransformation, ImageWithFocalPoint, UploadImage} from '../db/image'
 import fetch from 'node-fetch'
 import {FileUpload} from 'graphql-upload'
-import {ImageWithFocalPoint} from '../../lib'
 import {ArrayBufferUpload, MediaAdapter} from './mediaAdapter'
 
 export class MediaServerError extends Error {
