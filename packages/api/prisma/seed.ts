742cce602d9636c12a249cb5ece3c007f02d4e77
Tomasz Durka <tomasz@durka.pl>
2022-09-11 21:53:28 +0200

    Add admin user to packages/api seed

--- packages/api/prisma/seed.ts
+++ packages/api/prisma/seed.ts
@@ -1,5 +1,6 @@
 import {PrismaClient} from '@prisma/client'
 import {SettingName} from '@wepublish/api'
+import {hashPassword} from '../src'
 
 const seedSettings = (prisma: PrismaClient) => [
   prisma.setting.upsert({
@@ -112,6 +113,28 @@ const seedRoles = (prisma: PrismaClient) => [
   })
 ]
 
+const seedUsers = async (prisma: PrismaClient) => [
+  prisma.user.upsert({
+    where: {
+      id: 'admin'
+    },
+    update: {},
+    create: {
+      id: 'admin',
+      email: 'admin@wepublish.ch',
+      emailVerifiedAt: new Date(),
+      name: 'Admin',
+      active: true,
+      roleIDs: ['admin'],
+      password: await hashPassword('123')
+    }
+  })
+]
+
 export async function seed(prisma: PrismaClient) {
-  return prisma.$transaction([...seedRoles(prisma), ...seedSettings(prisma)])
+  return prisma.$transaction([
+    ...seedRoles(prisma),
+    ...seedSettings(prisma),
+    ...(await seedUsers(prisma))
+  ])
 }
