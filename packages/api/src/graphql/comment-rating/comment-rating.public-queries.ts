bd8d657aa21dc155aa1927efd01df8667e509299
Itrulia <karlmerkli@gmail.com>
2022-09-14 10:49:15 +0200

    Update packages/api/src/graphql/comment-rating/comment-rating.public-queries.ts
    Co-authored-by: michael-scheurer <70939054+michael-scheurer@users.noreply.github.com>

--- packages/api/src/graphql/comment-rating/comment-rating.public-queries.ts
+++ packages/api/src/graphql/comment-rating/comment-rating.public-queries.ts
@@ -2,7 +2,6 @@ import {PrismaClient} from '@prisma/client'
 
 export const getRatingSystem = (commentRatingSystem: PrismaClient['commentRatingSystem']) => {
   return commentRatingSystem.findFirst({
-    where: {},
     include: {
       answers: true
     }
