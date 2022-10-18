dd7aa04d842535c01bb5220fec557d764f10708b
Kamil <kamil@disruptivelabs.io>
2022-10-18 17:03:18 +0200

    feat(wpc-870): reject comment without a reason

--- packages/editor/src/client/api/index.ts
+++ packages/editor/src/client/api/index.ts
@@ -1177,7 +1177,7 @@ export type MutationApproveCommentArgs = {
 
 export type MutationRejectCommentArgs = {
   id: Scalars['ID'];
-  rejectionReason: CommentRejectionReason;
+  rejectionReason?: Maybe<CommentRejectionReason>;
 };
 
 
@@ -3314,7 +3314,7 @@ export type ApproveCommentMutation = (
 
 export type RejectCommentMutationVariables = Exact<{
   id: Scalars['ID'];
-  rejectionReason: CommentRejectionReason;
+  rejectionReason?: Maybe<CommentRejectionReason>;
 }>;
 
 
@@ -6663,7 +6663,7 @@ export type ApproveCommentMutationHookResult = ReturnType<typeof useApproveComme
 export type ApproveCommentMutationResult = Apollo.MutationResult<ApproveCommentMutation>;
 export type ApproveCommentMutationOptions = Apollo.BaseMutationOptions<ApproveCommentMutation, ApproveCommentMutationVariables>;
 export const RejectCommentDocument = gql`
-    mutation RejectComment($id: ID!, $rejectionReason: CommentRejectionReason!) {
+    mutation RejectComment($id: ID!, $rejectionReason: CommentRejectionReason) {
   rejectComment(id: $id, rejectionReason: $rejectionReason) {
     state
     rejectionReason
