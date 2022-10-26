9ae0c9adba249496cc71ad6b7e6a8f8ae26d81b4
Penina Cheshin <penk85@gmail.com>
2022-10-26 14:04:02 +0200

    migration for guid

--- packages/api/prisma/migrations/20221024073255_guids/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "articles" ADD COLUMN     "source" TEXT;
-
--- AlterTable
-ALTER TABLE "authors" ADD COLUMN     "source" TEXT;
-
--- AlterTable
-ALTER TABLE "images" ADD COLUMN     "origin" TEXT;
---
+++ packages/api/prisma/migrations/20221026074225_articles_images_authors_guid/migration.sql
@@ -0,0 +1,26 @@
+-- AlterTable
+ALTER TABLE "articles" ADD COLUMN     "origin" TEXT,
+ALTER COLUMN "id" SET DEFAULT gen_random_uuid();
+
+-- AlterTable
+ALTER TABLE "authors" ADD COLUMN     "origin" TEXT,
+ALTER COLUMN "id" SET DEFAULT gen_random_uuid();
+
+-- AlterTable
+ALTER TABLE "images" ADD COLUMN     "origin" TEXT,
+ALTER COLUMN "id" SET DEFAULT gen_random_uuid();
+
+-- UPDATE IDs on articles table to guid
+ALTER TABLE "articles" ADD "oldId" TEXT NULL;
+UPDATE "articles" SET "oldId" = "id";
+
+UPDATE "articles" SET "id" = gen_random_uuid();
+
+ALTER TABLE "navigations.links" DROP CONSTRAINT "navigations.links_articleID_fkey";
+ALTER TABLE "articles" ALTER COLUMN "id" TYPE uuid USING "id"::uuid;
+ALTER TABLE "navigations.links" ALTER COLUMN "articleID" TYPE uuid USING "articleID"::uuid;
+UPDATE "comments" SET "itemID" = "articles"."id" FROM "articles" WHERE "comments"."itemID" = "articles"."oldId" AND "comments"."itemType" = 'article';
+
+ALTER TABLE "articles" DROP COLUMN "oldId";
+ALTER TABLE "navigations.links" ADD CONSTRAINT "navigations.links_articleID_fkey" FOREIGN KEY ("articleID") REFERENCES "articles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
