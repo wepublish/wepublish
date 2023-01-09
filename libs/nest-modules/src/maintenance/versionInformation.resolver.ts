ab1c4c6846caad075a282392543496568009f3b8
Tomasz Durka <tomasz@durka.pl>
2023-01-09 11:34:48 +0100

    Add ApiModule and MaintenanceModule

--- libs/nest-modules/src/maintenance/versionInformation.resolver.ts
+++ libs/nest-modules/src/maintenance/versionInformation.resolver.ts
@@ -1,13 +1,13 @@
 import {Query, Resolver} from '@nestjs/graphql'
 import {VersionInformation} from './models/versionInformation.model'
-import * as lerna from '/lerna.json'
+import {version} from '../../../../lerna.json'
 
 @Resolver(of => VersionInformation)
 export class VersionInformationResolver {
   @Query(returns => VersionInformation, {name: 'versionInformation'})
   async getVersionInformation() {
     return {
-      version: lerna.version
+      version
     }
   }
 }
