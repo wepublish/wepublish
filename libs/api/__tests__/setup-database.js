8cd88c773a13f5b158153ab55221604af91084e1
Karl Merkli <karlmerkli@gmail.com>
2023-01-20 15:57:51 +0100

    revert(api): setup database to last version

--- libs/api/__tests__/setup-database.js
+++ libs/api/__tests__/setup-database.js
@@ -4,5 +4,5 @@ export default async () => {
   const databaseUrl = 'postgresql://postgres@localhost:5432/wepublish_test?schema=public'
   process.env.DATABASE_URL = databaseUrl
 
-  await execa(`npx`, ['prisma', 'migrate', 'deploy'])
+  await execa(`npx`, ['prisma', 'migrate', 'reset', '--force'])
 }
