38ef4b2f85fe74e0c479faa05d5a7c2d8236c11b
michael-scheurer <michael.scheurer@seccom.ch>
2023-01-23 16:37:33 +0100

    add mui table

--- libs/membership/editor/src/lib/settings/subscriptionSettings.tsx
+++ libs/membership/editor/src/lib/settings/subscriptionSettings.tsx
@@ -1,18 +1,50 @@
 import React from 'react'
 import {ListViewContainer, ListViewHeader} from '../../../../../../apps/editor/src/app/ui/listView'
-import {Typography} from '@mui/material'
+import {
+  TableContainer,
+  Typography,
+  Table,
+  TableHead,
+  TableBody,
+  TableCell,
+  TableRow,
+  Paper
+} from '@mui/material'
 
 export default function () {
+  // todo: continue with fetching SubscriptionCommunicationFlow
+
   return (
     <>
       <ListViewContainer>
         <ListViewHeader>
           <Typography variant="h2">Abo-Einstellungen</Typography>
           <Typography variant="subtitle1">
-            Dies sind die Standard-Einstellungen, die Du für jeden Memberplan überschreiben kannst.
+            Du kannst diese Einstellungen für jeden Memberplan überschreiben.
           </Typography>
         </ListViewHeader>
       </ListViewContainer>
+
+      <TableContainer style={{marginTop: '16px'}}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <b>Tage bis zur Abo-Erneuerung</b>
+              </TableCell>
+              <TableCell>
+                <b>Aktion</b>
+              </TableCell>
+              <TableCell>
+                <b>Standard-Mail</b>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow></TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   )
 }
