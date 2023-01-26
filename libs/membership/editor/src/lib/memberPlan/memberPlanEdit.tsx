a57cff9cabccb2515759aca87635c0f3339e80f7
michael-scheurer <michael.scheurer@seccom.ch>
2023-01-26 11:01:41 +0100

    working version

--- libs/membership/editor/src/lib/memberPlan/memberPlanEdit.tsx
+++ libs/membership/editor/src/lib/memberPlan/memberPlanEdit.tsx
@@ -6,20 +6,24 @@ import {useNavigate, useParams} from 'react-router-dom'
 import {useTranslation} from 'react-i18next'
 import {createCheckedPermissionComponent} from '../../../../../../apps/editor/src/app/atoms/permissionControl'
 import {
+  AvailablePaymentMethod,
   FullMemberPlanFragment,
+  FullPaymentMethodFragment,
   ImageRefFragment,
   PaymentMethod,
   useMemberPlanQuery,
+  usePaymentMethodListQuery,
   useUpdateMemberPlanMutation
 } from '@wepublish/editor/api'
-import {Drawer, FlexboxGrid, Form, Panel, Schema, TagPicker, Toggle} from 'rsuite'
+import {CheckPicker, Drawer, FlexboxGrid, Form, Panel, Schema, TagPicker, Toggle} from 'rsuite'
 import {ChooseEditImage} from '../../../../../../apps/editor/src/app/atoms/chooseEditImage'
 import {ImageSelectPanel} from '../../../../../../apps/editor/src/app/panel/imageSelectPanel'
 import {ImageEditPanel} from '../../../../../../apps/editor/src/app/panel/imageEditPanel'
-import {toggleRequiredLabel} from '../../../../../../apps/editor/src/app/toggleRequiredLabel'
 import {CurrencyInput} from '../../../../../../apps/editor/src/app/atoms/currencyInput'
 import {RichTextBlock} from '../../../../../../apps/editor/src/app/blocks/richTextBlock/richTextBlock'
 import {RichTextBlockValue} from '../../../../../../apps/editor/src/app/blocks/types'
+import {ALL_PAYMENT_PERIODICITIES, generateID} from '../../../../../../apps/editor/src/app/utility'
+import {ListInput, ListValue} from '../../../../../../apps/editor/src/app/atoms/listInput'
 
 function MemberPlanEdit() {
   /**
@@ -37,6 +41,9 @@ function MemberPlanEdit() {
   const [close, setClose] = useState<boolean>(false)
   const [isChooseModalOpen, setChooseModalOpen] = useState(false)
   const [isEditModalOpen, setEditModalOpen] = useState(false)
+  const [availablePaymentMethods, setAvailablePaymentMethods] = useState<
+    ListValue<AvailablePaymentMethod>[]
+  >([])
 
   /**
    * Services
@@ -45,19 +52,33 @@ function MemberPlanEdit() {
     variables: {id: memberPlanId!},
     fetchPolicy: 'network-only'
   })
+  const {data: paymentMethodData, loading: paymentMethodLoading} = usePaymentMethodListQuery({
+    fetchPolicy: 'network-only'
+  })
 
   const [updateMemberPlanMutation, {loading: memberPlanUpdating}] = useUpdateMemberPlanMutation()
 
   useEffect(() => {
-    setMemberPlan(memberPlanData?.memberPlan)
+    const memberPlan = memberPlanData?.memberPlan
+    setMemberPlan(memberPlan)
+    setAvailablePaymentMethods(
+      (memberPlan?.availablePaymentMethods || []).map(availablePaymentMethod => ({
+        id: generateID(),
+        value: availablePaymentMethod
+      }))
+    )
   }, [memberPlanData])
 
   /**
-   * Loading indicator
+   * Memos
    */
   const loading: boolean = useMemo(() => {
-    return !!memberPlanLoading || !!memberPlanUpdating
-  }, [memberPlanLoading, memberPlanUpdating])
+    return !!memberPlanLoading || !!memberPlanUpdating || !!paymentMethodLoading
+  }, [memberPlanLoading, memberPlanUpdating, paymentMethodLoading])
+
+  const paymentMethods: FullPaymentMethodFragment[] = useMemo(() => {
+    return paymentMethodData?.paymentMethods || []
+  }, [paymentMethodData])
 
   /**
    * Schema validation
@@ -83,7 +104,7 @@ function MemberPlanEdit() {
           imageID: memberPlan.image?.id,
           description: memberPlan.description,
           active: memberPlan.active,
-          availablePaymentMethods: memberPlan.availablePaymentMethods.map(value => ({
+          availablePaymentMethods: availablePaymentMethods.map(({value}) => ({
             paymentPeriodicities: value.paymentPeriodicities,
             forceAutoRenewal: value.forceAutoRenewal,
             paymentMethodIDs: value.paymentMethods.map((pm: PaymentMethod) => pm.id)
@@ -153,7 +174,7 @@ function MemberPlanEdit() {
                   </FlexboxGrid.Item>
 
                   {/* name */}
-                  <FlexboxGrid.Item colspan={24}>
+                  <FlexboxGrid.Item colspan={12}>
                     <Form.ControlLabel>{t('memberPlanEdit.name')}</Form.ControlLabel>
                     <Form.Control
                       name="name"
@@ -168,7 +189,7 @@ function MemberPlanEdit() {
                   </FlexboxGrid.Item>
 
                   {/* slug */}
-                  <FlexboxGrid.Item colspan={24}>
+                  <FlexboxGrid.Item colspan={12}>
                     <Form.ControlLabel>{t('memberPlanEdit.slug')}</Form.ControlLabel>
                     <Form.Control
                       name="slug"
@@ -201,24 +222,12 @@ function MemberPlanEdit() {
                       />
                     </div>
                   </FlexboxGrid.Item>
-
-                  {/* minimal monthly amount */}
-                  <FlexboxGrid.Item colspan={24}>
-                    <Form.ControlLabel>{t('memberPlanEdit.amountPerMonthMin')}</Form.ControlLabel>
-                    <CurrencyInput
-                      name="currency"
-                      currency="CHF"
-                      centAmount={memberPlan?.amountPerMonthMin || 0}
-                      disabled={loading}
-                      onChange={centAmount => {
-                        if (!memberPlan) {
-                          return
-                        }
-                        setMemberPlan({...memberPlan, amountPerMonthMin: centAmount})
-                      }}
-                    />
-                  </FlexboxGrid.Item>
-
+                </FlexboxGrid>
+              </Panel>
+            </FlexboxGrid.Item>
+            <FlexboxGrid.Item colspan={12}>
+              <Panel bordered>
+                <FlexboxGrid>
                   {/* tags */}
                   <FlexboxGrid.Item colspan={24}>
                     <Form.ControlLabel>{t('memberPlanEdit.tags')}</Form.ControlLabel>
@@ -240,6 +249,96 @@ function MemberPlanEdit() {
                         setMemberPlan({...memberPlan, tags: tagsValue})
                       }}
                     />
+                  </FlexboxGrid.Item>
+
+                  {/* minimal monthly amount */}
+                  <FlexboxGrid.Item colspan={24}>
+                    <Form.ControlLabel>{t('memberPlanEdit.amountPerMonthMin')}</Form.ControlLabel>
+                    <CurrencyInput
+                      name="currency"
+                      currency="CHF"
+                      centAmount={memberPlan?.amountPerMonthMin || 0}
+                      disabled={loading}
+                      onChange={centAmount => {
+                        if (!memberPlan) {
+                          return
+                        }
+                        setMemberPlan({...memberPlan, amountPerMonthMin: centAmount})
+                      }}
+                    />
+                  </FlexboxGrid.Item>
+
+                  <FlexboxGrid.Item colspan={24}>
+                    {/* payment method settings */}
+                    <ListInput
+                      value={availablePaymentMethods}
+                      disabled={loading}
+                      onChange={app => setAvailablePaymentMethods(app)}
+                      defaultValue={{
+                        forceAutoRenewal: false,
+                        paymentPeriodicities: [],
+                        paymentMethods: []
+                      }}>
+                      {({value, onChange}) => (
+                        <FlexboxGrid>
+                          {/* force auto-renew */}
+                          <FlexboxGrid.Item colspan={24}>
+                            <Form.ControlLabel>{t('memberPlanList.autoRenewal')}</Form.ControlLabel>
+                            <Toggle
+                              checked={value.forceAutoRenewal}
+                              disabled={loading}
+                              onChange={forceAutoRenewal => onChange({...value, forceAutoRenewal})}
+                            />
+                            <Form.HelpText>
+                              {t('memberPlanList.autoRenewalDescription')}
+                            </Form.HelpText>
+                          </FlexboxGrid.Item>
+
+                          {/* payment periodicity */}
+                          <FlexboxGrid.Item colspan={24}>
+                            <Form.ControlLabel>
+                              {t('memberPlanList.paymentPeriodicities')}
+                            </Form.ControlLabel>
+                            <CheckPicker
+                              virtualized
+                              value={value.paymentPeriodicities}
+                              data={ALL_PAYMENT_PERIODICITIES.map(pp => ({
+                                value: pp,
+                                label: t(`memberPlanList.paymentPeriodicity.${pp}`)
+                              }))}
+                              onChange={paymentPeriodicities =>
+                                onChange({...value, paymentPeriodicities})
+                              }
+                              block
+                              placement="auto"
+                            />
+                          </FlexboxGrid.Item>
+
+                          {/* payment method selection */}
+                          <FlexboxGrid.Item colspan={24}>
+                            <Form.ControlLabel>
+                              {t('memberPlanList.paymentMethods')}
+                            </Form.ControlLabel>
+                            <CheckPicker
+                              virtualized
+                              value={value.paymentMethods.map(pm => pm.id)}
+                              data={paymentMethods.map(pm => ({value: pm.id, label: pm.name}))}
+                              onChange={paymentMethodIDs => {
+                                onChange({
+                                  ...value,
+                                  paymentMethods: paymentMethodIDs
+                                    .map(pmID => paymentMethods.find(pm => pm.id === pmID))
+                                    .filter(pm => pm !== undefined)
+                                    .map(pm => pm as PaymentMethod)
+                                })
+                              }}
+                              block
+                              placement="auto"
+                            />
+                          </FlexboxGrid.Item>
+                        </FlexboxGrid>
+                      )}
+                    </ListInput>
                   </FlexboxGrid.Item>
                 </FlexboxGrid>
               </Panel>
