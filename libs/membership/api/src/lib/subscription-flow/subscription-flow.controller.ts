cb76ced9a9f8a290cfb2b2e37d9571b2b524f2a9
Elias Summermatter <elias.summermatter@seccom.ch>
2023-02-02 14:57:02 +0100

    Refactor function into controller

---
+++ libs/membership/api/src/lib/subscription-flow/subscription-flow.controller.ts
@@ -0,0 +1,41 @@
+import {PrismaService} from '@wepublish/api'
+
+export class SubscriptionFlowController {
+  constructor(private readonly prismaService: PrismaService) {}
+  async getFlow(defaultFlowOnly: boolean) {
+    let where = {}
+    if (defaultFlowOnly) {
+      where = {
+        default: true
+      }
+    }
+
+    return this.prismaService['subscriptionFlow'].findMany({
+      where,
+      include: {
+        memberPlan: true,
+        subscribeMailTemplate: true,
+        paymentMethods: true,
+        invoiceCreationMailTemplate: {
+          include: {
+            mailTemplate: true
+          }
+        },
+        renewalSuccessMailTemplate: true,
+        renewalFailedMailTemplate: true,
+        deactivationUnpaidMailTemplate: {
+          include: {
+            mailTemplate: true
+          }
+        },
+        deactivationByUserMailTemplate: true,
+        reactivationMailTemplate: true,
+        additionalIntervals: {
+          include: {
+            mailTemplate: true
+          }
+        }
+      }
+    })
+  }
+}
--- libs/membership/api/src/lib/subscription-flow/subscription-flow.provider.ts
+++ libs/membership/api/src/lib/subscription-flow/subscription-flow.provider.ts
@@ -1,46 +1,17 @@
 import {Args, Query, Resolver} from '@nestjs/graphql'
 import {SubscriptionFlowModel} from './subscription-flow.model'
 import {PrismaService} from '@wepublish/api'
+import {SubscriptionFlowController} from './subscription-flow.controller'
 
 @Resolver(of => [SubscriptionFlowProvider])
 export class SubscriptionFlowProvider {
-  constructor(private readonly prismaService: PrismaService) {}
+  private controller
+  constructor(private readonly prismaService: PrismaService) {
+    this.controller = new SubscriptionFlowController(prismaService)
+  }
 
   @Query(returns => [SubscriptionFlowModel], {name: 'SubscriptionFlows'})
-  async subscriptionFlowSettings(@Args('defaultFlow') defaultFlow: boolean) {
-    let where = {}
-    if (defaultFlow) {
-      where = {
-        default: true
-      }
-    }
-
-    return await this.prismaService['subscriptionFlow'].findMany({
-      where,
-      include: {
-        memberPlan: true,
-        subscribeMailTemplate: true,
-        paymentMethods: true,
-        invoiceCreationMailTemplate: {
-          include: {
-            mailTemplate: true
-          }
-        },
-        renewalSuccessMailTemplate: true,
-        renewalFailedMailTemplate: true,
-        deactivationUnpaidMailTemplate: {
-          include: {
-            mailTemplate: true
-          }
-        },
-        deactivationByUserMailTemplate: true,
-        reactivationMailTemplate: true,
-        additionalIntervals: {
-          include: {
-            mailTemplate: true
-          }
-        }
-      }
-    })
+  async subscriptionFlowSettings(@Args('defaultFlowOnly') defaultFlowOnly: boolean) {
+    return await this.controller.getFlow(defaultFlowOnly)
   }
 }
