e0af1aa571ef6228ad1ace7eb71dc3e4b70c134f
Elias Summermatter <elias.summermatter@seccom.ch>
2023-02-15 15:13:19 +0100

    Cleanedup some unused stuff

--- libs/membership/api/src/lib/subscription-flow/subscription-flow.model.ts
+++ libs/membership/api/src/lib/subscription-flow/subscription-flow.model.ts
@@ -1,16 +1,6 @@
 import {Field, InputType, Int, ObjectType, registerEnumType} from '@nestjs/graphql'
 import {PaymentPeriodicity, SubscriptionEvent} from '@prisma/client'
 
-export enum SubscriptionIntervalTypes {
-  ADDITIONAL_INTERVAL,
-  CREATE_INVOICES,
-  DEACTIVATION_UNPAID_INVOICE
-}
-
-registerEnumType(SubscriptionIntervalTypes, {
-  name: 'SubscriptionIntervalTypes'
-})
-
 registerEnumType(PaymentPeriodicity, {
   name: 'PaymentPeriodicity'
 })
@@ -58,18 +48,6 @@ export class SubscriptionInterval {
 }
 
 @ObjectType()
-export class SubscriptionIntervalCreated {
-  @Field(() => Int)
-  id!: number
-  @Field(() => Int, {nullable: true})
-  daysAwayFromEnding?: number
-  @Field(type => Int)
-  mailTemplateId!: number
-  @Field(() => SubscriptionEvent)
-  event!: SubscriptionEvent
-}
-
-@ObjectType()
 export class SubscriptionFlowModel {
   @Field(() => Int)
   id!: number
@@ -88,42 +66,6 @@ export class SubscriptionFlowModel {
 }
 
 // Input
-
-@InputType()
-export class MailTemplateRefInput {
-  @Field(() => Int)
-  id!: number
-}
-
-@InputType()
-class MemberPlanRefInput {
-  @Field()
-  id!: string
-}
-
-@InputType()
-class PaymentMethodRefInput {
-  @Field()
-  id!: string
-}
-
-@InputType()
-export class AdditionalIntervalCreateInput {
-  @Field(() => Int)
-  subscriptionFlowId!: number
-  @Field(() => Int, {nullable: true})
-  daysAwayFromEnding?: number
-  @Field(type => MailTemplateRef)
-  mailTemplate!: MailTemplateRef
-  @Field(() => SubscriptionEvent)
-  event!: SubscriptionEvent
-}
-
-@InputType()
-export class AdditionalIntervalDeleteInput {
-  @Field(() => Int)
-  additionalIntervalId!: number
-}
 
 @InputType()
 export class SubscriptionIntervalCreateInput {
@@ -155,10 +97,10 @@ export class SubscriptionIntervalDeleteInput {
 
 @InputType()
 export class SubscriptionFlowModelCreateInput {
-  @Field(type => MemberPlanRefInput)
-  memberPlan!: MemberPlanRefInput
-  @Field(type => [PaymentMethodRefInput])
-  paymentMethods!: PaymentMethodRefInput[]
+  @Field()
+  memberPlanId!: string
+  @Field(type => [String])
+  paymentMethodIds!: string[]
   @Field(type => [PaymentPeriodicity])
   periodicities!: PaymentPeriodicity[]
   @Field(type => [Boolean])
@@ -169,8 +111,8 @@ export class SubscriptionFlowModelCreateInput {
 export class SubscriptionFlowModelUpdateInput {
   @Field(() => Int)
   id!: number
-  @Field(type => [PaymentMethodRefInput])
-  paymentMethods!: PaymentMethodRefInput[]
+  @Field(type => [String])
+  paymentMethodIds!: string[]
   @Field(type => [PaymentPeriodicity])
   periodicities!: PaymentPeriodicity[]
   @Field(type => [Boolean])
