f34310570af64d804530a23f31ed34af318d26d7
Lukas_Skywalker <lukas.diener@hotmail.com>
2023-03-30 11:43:02 +0200

    Add OldContextService to MailTemplateModule

--- libs/membership/api/src/lib/mail-template/mail-template.module.ts
+++ libs/membership/api/src/lib/mail-template/mail-template.module.ts
@@ -1,11 +1,11 @@
 import {Module} from '@nestjs/common'
-import {PrismaService} from '@wepublish/api'
+import {OldContextService, PrismaService} from '@wepublish/api'
 import {PrismaModule} from '@wepublish/nest-modules'
 import {MailTemplateSyncService} from './mail-template-sync.service'
 import {MailTemplatesResolver} from './mail-template.resolver'
 
 @Module({
   imports: [PrismaModule],
-  providers: [PrismaService, MailTemplatesResolver, MailTemplateSyncService]
+  providers: [PrismaService, MailTemplatesResolver, MailTemplateSyncService, OldContextService]
 })
 export class MailTemplateModule {}
