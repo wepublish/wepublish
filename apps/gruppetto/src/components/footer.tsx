c1267058d6e11dcf6c7c478ec1b8c2ccd332bb92
Karl Merkli <karlmerkli@gmail.com>
2023-04-06 10:56:48 +0200

    stash

--- apps/gruppetto/src/components/footer.tsx
+++
@@ -1,7 +0,0 @@
-import {styled} from '@mui/material'
-import {Footer as WepFooter} from '@wepublish/website'
-
-export const Footer = styled(WepFooter)`
-  background-color: transparent;
-  color: ${({theme}) => theme.palette.text.primary};
-`
