361a17b1e8a1ea8c129e09c142e92d916dd462ba
Lukas_Skywalker <lukas.diener@hotmail.com>
2023-04-19 21:59:55 +0200

    Revert changes to database setup code (https://github.com/wepublish/wepublish/pull/822/files#r1171426312)

--- libs/membership/api/setup-database.js
+++ libs/membership/api/setup-database.js
@@ -1,9 +1,10 @@
 import execa from 'execa'
+import {displayName} from './jest.config'
 
 export default async () => {
-  const databaseUrl = `postgresql://postgres@localhost:5432/wepublish_test?schema=public`
+  const databaseUrl = `postgresql://postgres@localhost:5432/${displayName}?schema=public`
   process.env.DATABASE_URL = databaseUrl
   process.env.TZ = 'UTC'
 
-  await execa(`npx`, ['prisma', 'migrate', 'reset', '--force'])
+  await execa(`npx`, ['prisma', 'migrate', 'deploy'])
 }
