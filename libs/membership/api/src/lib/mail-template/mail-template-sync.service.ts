7091b5012a1b62e87f5b8dd5d5bcde98a0dc46ee
Lukas_Skywalker <lukas.diener@hotmail.com>
2023-04-19 22:39:35 +0200

    Extract private methods from MailTemplateSyncService (https://github.com/wepublish/wepublish/pull/822/files#r1164026651)

--- libs/membership/api/src/lib/mail-template/mail-template-sync.service.ts
+++ libs/membership/api/src/lib/mail-template/mail-template-sync.service.ts
@@ -12,52 +12,82 @@ export interface MailTemplateSyncDiff {
 export class MailTemplateSyncService {
   constructor(private prismaService: PrismaService, private oldContextService: OldContextService) {}
 
+  /**
+   * Synchronizes the local template list with the remote mail provider.
+   * This performs the following actions:
+   * - updates locally existing templates to match their remote counterpart
+   * - creates local templates for any new remote templates
+   * - marks any local templates as outdated if their remote counterpart is missing
+   */
   async synchronizeTemplates(): Promise<void> {
     const localTemplates = await this.prismaService.mailTemplate.findMany()
 
     const mailProvider = await this.oldContextService.context.mailContext.getProvider()
     const remoteTemplates = (await mailProvider.getTemplates()) as MailProviderTemplate[]
 
-    // find new and updated remote templates
-    for (const remoteTemplate of remoteTemplates) {
-      const localMatch = localTemplates.find(
-        localTemplate => localTemplate.externalMailTemplateId === remoteTemplate.uniqueIdentifier
-      )
-      if (localMatch) {
-        // template exists locally, update properties
-        await this.prismaService.mailTemplate.update({
-          where: {externalMailTemplateId: remoteTemplate.uniqueIdentifier},
-          data: {
-            name: remoteTemplate.name,
-            remoteMissing: false
-          }
-        })
-      } else {
-        // template is new
-        await this.prismaService.mailTemplate.create({
-          data: {
-            name: remoteTemplate.name,
-            externalMailTemplateId: remoteTemplate.uniqueIdentifier,
-            remoteMissing: false
-          }
-        })
-      }
+    const updatedRemoteTemplates = this.findUpdatedRemoteTemplates(localTemplates, remoteTemplates)
+    for (const remoteTemplate of updatedRemoteTemplates) {
+      await this.prismaService.mailTemplate.update({
+        where: {externalMailTemplateId: remoteTemplate.uniqueIdentifier},
+        data: {
+          name: remoteTemplate.name,
+          remoteMissing: false
+        }
+      })
     }
 
-    // find outdated local templates
-    for (const localTemplate of localTemplates) {
-      const remoteMatch = remoteTemplates.find(
+    const newRemoteTemplates = this.findNewRemoteTemplates(localTemplates, remoteTemplates)
+    for (const remoteTemplate of newRemoteTemplates) {
+      await this.prismaService.mailTemplate.create({
+        data: {
+          name: remoteTemplate.name,
+          externalMailTemplateId: remoteTemplate.uniqueIdentifier,
+          remoteMissing: false
+        }
+      })
+    }
+
+    const outdatedLocalTemplates = this.findOutdatedLocalTemplates(localTemplates, remoteTemplates)
+    for (const localTemplate of outdatedLocalTemplates) {
+      await this.prismaService.mailTemplate.update({
+        where: {externalMailTemplateId: localTemplate.externalMailTemplateId},
+        data: {
+          remoteMissing: true
+        }
+      })
+    }
+  }
+
+  private findUpdatedRemoteTemplates(
+    localTemplates: MailTemplate[],
+    remoteTemplates: MailProviderTemplate[]
+  ): MailProviderTemplate[] {
+    return remoteTemplates.filter(remoteTemplate => {
+      return localTemplates.find(
+        localTemplate => localTemplate.externalMailTemplateId === remoteTemplate.uniqueIdentifier
+      )
+    })
+  }
+
+  private findNewRemoteTemplates(
+    localTemplates: MailTemplate[],
+    remoteTemplates: MailProviderTemplate[]
+  ): MailProviderTemplate[] {
+    return remoteTemplates.filter(remoteTemplate => {
+      return !localTemplates.find(
+        localTemplate => localTemplate.externalMailTemplateId === remoteTemplate.uniqueIdentifier
+      )
+    })
+  }
+
+  private findOutdatedLocalTemplates(
+    localTemplates: MailTemplate[],
+    remoteTemplates: MailProviderTemplate[]
+  ): MailTemplate[] {
+    return localTemplates.filter(localTemplate => {
+      return !remoteTemplates.find(
         remoteTemplate => remoteTemplate.uniqueIdentifier === localTemplate.externalMailTemplateId
       )
-      if (!remoteMatch) {
-        // template was deleted remotely
-        await this.prismaService.mailTemplate.update({
-          where: {externalMailTemplateId: localTemplate.externalMailTemplateId},
-          data: {
-            remoteMissing: true
-          }
-        })
-      }
-    }
+    })
   }
 }
