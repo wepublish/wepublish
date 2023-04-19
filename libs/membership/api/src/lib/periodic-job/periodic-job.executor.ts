4f9ff5ac20e973e9213ead7938309c14174bd8b3
Lukas_Skywalker <lukas.diener@hotmail.com>
2023-04-19 09:51:46 +0200

    Extract cron schedule into a variable (https://github.com/wepublish/wepublish/pull/822#discussion_r1164034825)

--- libs/membership/api/src/lib/periodic-job/periodic-job.executor.ts
+++ libs/membership/api/src/lib/periodic-job/periodic-job.executor.ts
@@ -3,6 +3,8 @@ import {Injectable} from '@nestjs/common'
 import {PeriodicJobController, SubscriptionController} from '@wepublish/membership/api'
 import {OldContextService, PrismaService} from '@wepublish/api'
 
+const SCHEDULE = process.env.PERIODIC_JOB_EXECUTION_SCHEDULE || '0 * * * * *'
+
 @Injectable()
 export class PeriodicJobExecutor {
   constructor(
@@ -11,12 +13,10 @@ export class PeriodicJobExecutor {
   ) {}
 
   @Cron(
-    process.env.PERIODIC_JOB_EXECUTION_SCHEDULE || `0 0 3 * * *`,
+    SCHEDULE,
     // Allow only chron that run once a day example: [number] [number] [number] [star] [star] [star]
     {
-      disabled: !/[1-5]?[0-9] [1-5]?[0-9] [1-2]?[0-9] \* \* \*/.test(
-        process.env.PERIODIC_JOB_EXECUTION_SCHEDULE || `0 0 3 * * *`
-      )
+      disabled: !/[1-5]?[0-9] [1-5]?[0-9] [1-2]?[0-9] \* \* \*/.test(SCHEDULE)
     }
   )
   async handleCron() {
