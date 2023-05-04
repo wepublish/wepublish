1796e66ae519f2e5a4136cd7e4fdc11d74a76dcf
Lukas_Skywalker <lukas.diener@hotmail.com>
2023-05-04 15:22:05 +0200

    Mark data attribute as unused in UserDecorator

--- libs/membership/api/src/lib/user.decorator.ts
+++ libs/membership/api/src/lib/user.decorator.ts
@@ -1,7 +1,7 @@
 import {createParamDecorator, ExecutionContext} from '@nestjs/common'
 import {GqlExecutionContext} from '@nestjs/graphql'
 
-export const CurrentUser = createParamDecorator((data: unknown, context: ExecutionContext) => {
+export const CurrentUser = createParamDecorator((_data: unknown, context: ExecutionContext) => {
   const ctx = GqlExecutionContext.create(context)
   return ctx.getContext().req.user.user
 })
