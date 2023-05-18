a8d0ee99aa64379ffc0dde3120d09331f8cbf36f
Elias Summermatter <elias.summermatter@seccom.ch>
2023-05-18 14:03:23 +0200

    Added set to null clause

--- libs/api/prisma/migrations/20221229154844_unwrap_array_of_jsonb/migration.sql
+++ libs/api/prisma/migrations/20221229154844_unwrap_array_of_jsonb/migration.sql
@@ -6,18 +6,28 @@ $$;
 
 -- Migrate article revision blocks from jsob[] to jsonb
 ALTER TABLE "articles.revisions" ALTER COLUMN blocks TYPE jsonb USING jsonb_array_to_jsonb(blocks);
+UPDATE "articles.revisions" set "blocks" = '[]' where blocks IS NULL;
+ALTER TABLE "articles.revisions" ALTER COLUMN "blocks" SET NOT NULL;
 
 -- Migrate page revision blocks from jsob[] to jsonb
 ALTER TABLE "pages.revisions" ALTER COLUMN blocks TYPE jsonb USING jsonb_array_to_jsonb(blocks);
+UPDATE "pages.revisions" set "blocks" = '[]' where blocks IS NULL;
+ALTER TABLE "pages.revisions" ALTER COLUMN "blocks" SET NOT NULL;
 
 -- Migrate author bio from jsob[] to jsonb
 ALTER TABLE "authors" ALTER COLUMN bio TYPE jsonb USING jsonb_array_to_jsonb(bio);
+UPDATE authors set "bio" = '[]' where bio IS NULL;
+ALTER TABLE "authors" ALTER COLUMN "bio" SET NOT NULL;
 
 -- Migrate member plans description from jsob[] to jsonb
 ALTER TABLE "member.plans" ALTER COLUMN description TYPE jsonb USING jsonb_array_to_jsonb(description);
+UPDATE "member.plans" set "description" = '[]' where description IS NULL;
+ALTER TABLE "member.plans" ALTER COLUMN "description" SET NOT NULL;
 
 -- Migrate peer profiles callToActionText from jsob[] to jsonb
 ALTER TABLE "peerProfiles" ALTER COLUMN "callToActionText" TYPE jsonb USING jsonb_array_to_jsonb("callToActionText");
+UPDATE "peerProfiles" set "callToActionText" = '[]' where "callToActionText" IS NULL;
+ALTER TABLE "peerProfiles" ALTER COLUMN "callToActionText" SET NOT NULL;
 
 -- Cleanup function
 DROP FUNCTION jsonb_array_to_jsonb;
