241cd03cd638c43015c8696bfb19bf8f23ef4c6a
Elias Summermatter <elias.summermatter@seccom.ch>
2023-05-18 11:42:20 +0200

    b/WPC-1198-fixing-error-in-migration-to-convert-jsonb_array-to-jsonb

--- libs/api/prisma/migrations/20221229154844_unwrap_array_of_jsonb/migration.sql
+++ libs/api/prisma/migrations/20221229154844_unwrap_array_of_jsonb/migration.sql
@@ -1,84 +1,23 @@
+-- Create function to migrate jsob[] to jsonb
+CREATE OR REPLACE FUNCTION jsonb_array_to_jsonb(jsonb[]) RETURNS jsonb LANGUAGE SQL AS $$
+    SELECT jsonb_object_agg(key, value)
+    FROM unnest($1), jsonb_each(unnest)
+$$;
+
 -- Migrate article revision blocks from jsob[] to jsonb
-ALTER TABLE "articles.revisions" ADD COLUMN "blocks2" JSONB;
-
-UPDATE "articles.revisions"
-SET blocks2 = jsonBlocks
-FROM (
-    SELECT jsonb_agg(j::jsonb)
-    FROM (
-        SELECT unnest(blocks)
-        FROM "articles.revisions"
-    ) as x(j)
-) as x(jsonBlocks);
-
-ALTER TABLE "articles.revisions" ALTER COLUMN "blocks2" SET NOT NULL;
-ALTER TABLE "articles.revisions" DROP COLUMN "blocks";
-ALTER TABLE "articles.revisions" RENAME COLUMN "blocks2" TO "blocks";
+ALTER TABLE "articles.revisions" ALTER COLUMN blocks TYPE jsonb USING jsonb_array_to_jsonb(blocks);
 
 -- Migrate page revision blocks from jsob[] to jsonb
-ALTER TABLE "pages.revisions" ADD COLUMN "blocks2" JSONB;
-
-UPDATE "pages.revisions"
-SET blocks2 = jsonBlocks
-FROM (
-    SELECT jsonb_agg(j::jsonb)
-    FROM (
-        SELECT unnest(blocks)
-        FROM "pages.revisions"
-    ) as x(j)
-) as x(jsonBlocks);
-
-ALTER TABLE "pages.revisions" ALTER COLUMN "blocks2" SET NOT NULL;
-ALTER TABLE "pages.revisions" DROP COLUMN "blocks";
-ALTER TABLE "pages.revisions" RENAME COLUMN "blocks2" TO "blocks";
+ALTER TABLE "pages.revisions" ALTER COLUMN blocks TYPE jsonb USING jsonb_array_to_jsonb(blocks);
 
 -- Migrate author bio from jsob[] to jsonb
-ALTER TABLE "authors" ADD COLUMN "bio2" JSONB;
-
-UPDATE "authors"
-SET bio2 = jsonBio
-FROM (
-    SELECT jsonb_agg(j::jsonb)
-    FROM (
-        SELECT unnest(bio)
-        FROM "authors"
-    ) as x(j)
-) as x(jsonBio);
-
-ALTER TABLE "authors" ALTER COLUMN "bio2" SET NOT NULL;
-ALTER TABLE "authors" DROP COLUMN "bio";
-ALTER TABLE "authors" RENAME COLUMN "bio2" TO "bio";
+ALTER TABLE "authors" ALTER COLUMN bio TYPE jsonb USING jsonb_array_to_jsonb(bio);
 
 -- Migrate member plans description from jsob[] to jsonb
-ALTER TABLE "member.plans" ADD COLUMN "description2" JSONB;
-
-UPDATE "member.plans"
-SET description2 = jsonDescription
-FROM (
-    SELECT jsonb_agg(j::jsonb)
-    FROM (
-        SELECT unnest(description)
-        FROM "member.plans"
-    ) as x(j)
-) as x(jsonDescription);
-
-ALTER TABLE "member.plans" ALTER COLUMN "description2" SET NOT NULL;
-ALTER TABLE "member.plans" DROP COLUMN "description";
-ALTER TABLE "member.plans" RENAME COLUMN "description2" TO "description";
+ALTER TABLE "member.plans" ALTER COLUMN description TYPE jsonb USING jsonb_array_to_jsonb(description);
 
 -- Migrate peer profiles callToActionText from jsob[] to jsonb
-ALTER TABLE "peerProfiles" ADD COLUMN "callToActionText2" JSONB;
+ALTER TABLE "peerProfiles" ALTER COLUMN "callToActionText" TYPE jsonb USING jsonb_array_to_jsonb("callToActionText");
 
-UPDATE "peerProfiles"
-SET "callToActionText2" = jsonCallToActionText
-FROM (
-    SELECT jsonb_agg(j::jsonb)
-    FROM (
-        SELECT unnest("callToActionText")
-        FROM "peerProfiles"
-    ) as x(j)
-) as x(jsonCallToActionText);
-
-ALTER TABLE "peerProfiles" ALTER COLUMN "callToActionText2" SET NOT NULL;
-ALTER TABLE "peerProfiles" DROP COLUMN "callToActionText";
-ALTER TABLE "peerProfiles" RENAME COLUMN "callToActionText2" TO "callToActionText";
+-- Cleanup function
+DROP FUNCTION jsonb_array_to_jsonb;
