66d3095b27af1a9aeda127ba41e7bc98a775c9c2
michael-scheurer <michael.scheurer@seccom.ch>
2023-05-31 12:21:12 +0200

    solve poll info text update along proposition of https://github.com/wepublish/wepublish/pull/934#issuecomment-1567872836

--- apps/editor/src/app/routes/polls/pollEditView.tsx
+++ apps/editor/src/app/routes/polls/pollEditView.tsx
@@ -119,7 +119,7 @@ function PollEditView() {
       variables: {
         pollId: poll.id,
         question: poll.question,
-        infoText: poll.infoText || undefined,
+        infoText: poll.infoText,
         opensAt,
         closedAt,
         answers: poll.answers?.map(answer => {
