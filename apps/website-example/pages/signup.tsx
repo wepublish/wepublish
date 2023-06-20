c87581159be55dec0ba13044cccfd321fc250c95
Karl Merkli <karlmerkli@gmail.com>
2023-06-20 12:30:21 +0200

    refactor(website): add some text to the signup page

--- apps/website-example/pages/signup.tsx
+++ apps/website-example/pages/signup.tsx
@@ -1,10 +1,20 @@
-import {RegistrationFormContainer, useUser} from '@wepublish/website'
+import {styled} from '@mui/material'
+import {RegistrationFormContainer, useUser, useWebsiteBuilder} from '@wepublish/website'
 import {useRouter} from 'next/router'
 import {useEffect} from 'react'
+
+const SignupWrapper = styled('div')`
+  display: grid;
+  justify-content: center;
+  gap: ${({theme}) => theme.spacing(3)};
+`
 
 export default function SignUp() {
   const {hasUser} = useUser()
   const router = useRouter()
+  const {
+    elements: {H3}
+  } = useWebsiteBuilder()
 
   useEffect(() => {
     if (hasUser) {
@@ -12,5 +22,11 @@ export default function SignUp() {
     }
   }, [router, hasUser])
 
-  return <RegistrationFormContainer />
+  return (
+    <SignupWrapper>
+      <H3 component="h1">Registriere dich noch heute</H3>
+
+      <RegistrationFormContainer />
+    </SignupWrapper>
+  )
 }
