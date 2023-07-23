6632f45373edc809c213c426edd48f13a5c2f19e
Karl Merkli <karlmerkli@gmail.com>
2023-07-23 11:12:45 +0200

    refactor: add typeahead reporter and github reporter

--- jest.preset.js
+++ jest.preset.js
@@ -3,5 +3,7 @@ const nxPreset = require('@nx/jest/preset').default
 module.exports = {
   ...nxPreset,
   globalSetup: `${__dirname}/jest.setup.ts`,
-  coverageReporters: [...nxPreset.coverageReporters, 'text', 'json']
+  coverageReporters: [...nxPreset.coverageReporters, 'text'],
+  reporters: ['default', ['github-actions', {silent: false}]],
+  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname']
 }
