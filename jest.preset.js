30e30a8a5742fe27f39ce82fcd190a47af43aa09
antkiewiczk <antkiewiczk@gmail.com>
2023-07-28 13:15:52 +0200

    transformIgnorePatterns, fix tiktok video embed

--- jest.preset.js
+++ jest.preset.js
@@ -4,6 +4,9 @@ module.exports = {
   ...nxPreset,
   globalSetup: `${__dirname}/jest.setup.ts`,
   coverageReporters: [...nxPreset.coverageReporters, 'text'],
-  reporters: ['default', ['github-actions', {silent: false}]],
-  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname']
+  reporters: ['default', ['github-actions', { silent: false }]],
+  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
+  transformIgnorePatterns: [
+    "node_modules/react-tweet"
+  ],
 }
