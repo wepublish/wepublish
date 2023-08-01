558708c3c6c451ac2ce7f8e8cbd35855c52322e2
Katerina Skroumpelou <mandarini@users.noreply.github.com>
2023-08-01 18:33:01 +0300

    updated some deps and integrated config (#1014)

--- libs/website/.storybook/main.ts
+++ libs/website/.storybook/main.ts
@@ -1,9 +1,43 @@
 import {StorybookConfig} from '@storybook/react-webpack5'
-import rootMain from '../../../.storybook/main'
 
 export default {
-  ...rootMain,
-  core: {...rootMain.core, builder: 'webpack5'},
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+  docs: {
+    autodocs: false
+  },
+  env: config => ({
+    ...config,
+    NODE_ENV: 'production'
+  }),
   stories: ['../../**/src/lib/**/*.mdx', '../../**/src/lib/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [...(rootMain.addons ?? []), '@nx/react/plugins/storybook']
+  addons: [
+    '@nx/react/plugins/storybook',
+    '@storybook/addon-essentials',
+    'storybook-addon-apollo-client',
+    '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-storysource',
+      options: {
+        loaderOptions: {
+          prettierConfig: {
+            printWidth: 100,
+            semi: false,
+            singleQuote: true,
+            bracketSpacing: false,
+            jsxBracketSameLine: true,
+            trailingComma: 'none',
+            arrowParens: 'avoid'
+          }
+        }
+      }
+    },
+    '@storybook/addon-a11y',
+    '@storybook/addon-docs',
+    '@storybook/addon-styling',
+    '@storybook/addon-links',
+    'storybook-react-i18next'
+  ]
 } as StorybookConfig
