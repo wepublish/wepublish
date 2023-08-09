a1f3e02e5e21ca5c71a894f42ef1383de70cb653
Kamil Antkiewicz <antkiewiczk@gmail.com>
2023-08-09 16:43:32 +0200

    WPC-1067: Break Block (website builder) (#1021)
    Co-authored-by: Itrulia <karlmerkli@gmail.com>

--- libs/page/website/src/lib/page-container.stories.tsx
+++ libs/page/website/src/lib/page-container.stories.tsx
@@ -514,7 +514,69 @@ const page = {
       author: 'By an Author'
     },
     {
-      __typename: 'LinkPageBreakBlock'
+      __typename: 'LinkPageBreakBlock',
+      text: 'Break block test',
+      linkText: null,
+      linkURL: null,
+      styleOption: 'default',
+      richText: [
+        {
+          type: 'paragraph',
+          children: [
+            {
+              text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+            }
+          ]
+        },
+        {
+          type: 'paragraph',
+          children: [
+            {
+              text: ''
+            }
+          ]
+        },
+        {
+          type: 'heading-three',
+          children: [
+            {
+              text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+            }
+          ]
+        },
+        {
+          type: 'heading-three',
+          children: [
+            {
+              text: ''
+            }
+          ]
+        },
+        {
+          type: 'paragraph',
+          children: [
+            {
+              bold: true,
+              text: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+              italic: true
+            },
+            {
+              text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+              italic: true
+            },
+            {
+              text: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+              italic: true,
+              underline: true
+            }
+          ]
+        }
+      ],
+      linkTarget: null,
+      hideButton: false,
+      templateOption: 'none',
+      layoutOption: 'image-left',
+      image
     },
     {
       __typename: 'EmbedBlock',
