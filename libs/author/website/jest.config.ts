246a0ee9ccb23676c373943ce45381e4c6976a2b
Karl Merkli <karlmerkli@gmail.com>
2023-08-10 13:39:42 +0200

    test: replace swc with babel due to issues with mui & bindings

--- libs/author/website/jest.config.ts
+++ libs/author/website/jest.config.ts
@@ -2,45 +2,6 @@
 export default {
   displayName: 'author-website',
   preset: '../../../jest.preset.js',
-  transform: {
-    '^.+\\.[tj]sx?$': [
-      '@swc/jest',
-      {
-        jsc: {
-          transform: {
-            react: {
-              runtime: 'automatic',
-              importSource: '@emotion/react'
-            }
-          },
-          experimental: {
-            cacheRoot: 'node_modules/.cache/swc',
-            plugins: [
-              [
-                '@swc/plugin-emotion',
-                {
-                  importMap: {
-                    '@mui/material': {
-                      styled: {
-                        canonicalImport: ['@emotion/styled', 'default'],
-                        styledBaseImport: ['@mui/material', 'styled']
-                      }
-                    },
-                    '@mui/material/styles': {
-                      styled: {
-                        canonicalImport: ['@emotion/styled', 'default'],
-                        styledBaseImport: ['@mui/material/styles', 'styled']
-                      }
-                    }
-                  }
-                }
-              ]
-            ]
-          }
-        }
-      }
-    ]
-  },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../coverage/libs/author/website',
   setupFiles: ['./setup-tests.tsx']
