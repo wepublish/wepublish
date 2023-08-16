7421871e47e41512357eaa5e1a4e1c109d12e3b0
Karl Merkli <karlmerkli@gmail.com>
2023-08-16 13:42:39 +0200

    chore: upgraded nx

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,15 +1,23 @@
 export default {
+  // uncomment the property below if you want to apply some webpack config globally
+  // webpackFinal: async (config, { configType }) => {
+  //   // Make whatever fine-grained changes you need that should apply to all storybook configs
+  //   // Return the altered config
+  //   return config;
+  // },
   addons: [
     'storybook-addon-apollo-client',
     '@storybook/addon-interactions',
     '@storybook/addon-storysource',
     '@storybook/addon-a11y'
-  ]
-  // uncomment the property below if you want to apply some webpack config globally
-  // webpackFinal: async (config, { configType }) => {
-  //   // Make whatever fine-grained changes you need that should apply to all storybook configs
+  ],
 
-  //   // Return the altered config
-  //   return config;
-  // },
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
+  },
+
+  docs: {
+    autodocs: true
+  }
 } as any
--- libs/ui/.storybook/main.ts
+++ libs/ui/.storybook/main.ts
@@ -8,7 +8,7 @@ export default {
     '../src/lib/**/*.stories.mdx',
     '../src/lib/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  addons: ['@storybook/addon-essentials', ...rootMain.addons, '@nrwl/react/plugins/storybook'],
+  addons: ['@storybook/addon-essentials', ...rootMain.addons, '@nx/react/plugins/storybook'],
   webpackFinal: async (config: any, {configType}: any) => {
     // apply any global webpack configs that might have been specified in .storybook/main.js
     if (rootMain.webpackFinal) {
