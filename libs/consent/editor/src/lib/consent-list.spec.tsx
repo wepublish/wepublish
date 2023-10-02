5191b6b956bc4447c2af6fae78e40594ca27ad64
Itrulia <karlmerkli@gmail.com>
2023-10-02 09:57:57 +0200

    ci: dont collect coverage from storybook (#1059)

--- libs/consent/editor/src/lib/consent-list.spec.tsx
+++ libs/consent/editor/src/lib/consent-list.spec.tsx
@@ -1,12 +1,23 @@
-import React from 'react'
 import {render} from '@testing-library/react'
-import ConsentList from './consent-list'
+import {ConsentList} from './consent-list'
 import {BrowserRouter} from 'react-router-dom'
-
+import {MockLink} from '@apollo/client/testing'
 import fetch from 'jest-fetch-mock'
+import {ApolloClient, InMemoryCache} from '@apollo/client'
+import * as v2Client from '../apiClientv2'
+
 jest.setMock('node-fetch', fetch)
 
 describe('ConsentList', () => {
+  beforeEach(() => {
+    jest.spyOn(v2Client, 'getApiClientV2').mockReturnValue(
+      new ApolloClient({
+        cache: new InMemoryCache(),
+        link: new MockLink([], true, {showWarnings: false})
+      })
+    )
+  })
+
   it('should render successfully', () => {
     const {baseElement} = render(
       <BrowserRouter>
