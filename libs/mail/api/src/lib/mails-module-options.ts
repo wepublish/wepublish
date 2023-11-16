5a8667cf2f653d3be19bfe99f21c7571d302bc14
Itrulia <karlmerkli@gmail.com>
2023-11-16 16:36:17 +0100

    feat(mail-api): subscription changes for mails (#1097)

---
+++ libs/mail/api/src/lib/mails-module-options.ts
@@ -0,0 +1,12 @@
+import {ModuleAsyncOptions} from '@wepublish/utils/api'
+import {BaseMailProvider} from './mail-provider'
+
+export const MAILS_MODULE_OPTIONS = 'MAILS_MODULE_OPTIONS'
+
+export interface MailsModuleOptions {
+  defaultFromAddress: string
+  defaultReplyToAddress: string
+  mailProvider: BaseMailProvider
+}
+
+export type MailsModuleAsyncOptions = ModuleAsyncOptions<MailsModuleOptions>
