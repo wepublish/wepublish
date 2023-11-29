df19c428a131d32107cfaf769e9f23b1f1554d8e
Karl Merkli <karlmerkli@gmail.com>
2023-11-29 16:47:57 +0100

    refactor: make it even better

--- apps/gruppetto/src/safari-bug-hotfix.tsx
+++ apps/gruppetto/src/safari-bug-hotfix.tsx
@@ -3,7 +3,10 @@ import {styled} from '@mui/material'
 
 // temporary fix until it's properly fixed
 export const SafariBugHotfixTeaser = styled(Teaser)`
+  grid-template-rows: initial;
+
   ${ImageWrapper} {
+    height: auto;
     object-fit: contain;
   }
 `
