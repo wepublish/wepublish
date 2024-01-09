ac17bc339ade00a914d92aa4975aaa2c44905b2d
antkiewiczk <antkiewiczk@gmail.com>
2024-01-09 14:21:47 +0100

    reafactor removeNonMatchingKeys

--- removeNonMatchingKeys.js
+++ removeNonMatchingKeys.js
@@ -1,34 +1,44 @@
-const fs = require('fs');
+const fs = require('fs')
 
-const json1 = {} // paste first json here
-
-const json2 = {} // paste second json here
-
-let removedKeys = [];
-
-function removeNonMatchingKeys(obj1, obj2, parent = '') {
-    if (typeof obj1 !== 'object' || typeof obj2 !== 'object' || obj1 === null || obj2 === null) {
-        return;
+/**
+ * Recursively removes keys from 'sourceObj' that are not present in 'referenceObj'.
+ * @param {Object} sourceObj The source object from which keys are removed.
+ * @param {Object} referenceObj The reference object used to determine which keys should be removed from the source object.
+ * @param {String} parent Internal use only for tracking the key path.
+ * @param {Array} removedKeys List to keep track of all removed keys.
+ */
+function removeNonMatchingKeys(sourceObj, referenceObj, parent = '', removedKeys = []) {
+    if (
+        typeof sourceObj !== 'object' ||
+        typeof referenceObj !== 'object' ||
+        sourceObj === null ||
+        referenceObj === null
+    ) {
+        return
     }
 
-    Object.keys(obj1).forEach(key => {
-        const fullPath = parent ? `${parent}.${key}` : key;
-        if (obj2.hasOwnProperty(key)) {
-            if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
-                removeNonMatchingKeys(obj1[key], obj2[key], fullPath);
+    Object.keys(sourceObj).forEach(key => {
+        const fullPath = parent ? `${parent}.${key}` : key
+        if (referenceObj.hasOwnProperty(key)) {
+            if (typeof sourceObj[key] === 'object' && typeof referenceObj[key] === 'object') {
+                removeNonMatchingKeys(sourceObj[key], referenceObj[key], fullPath, removedKeys)
             }
         } else {
-            removedKeys.push(fullPath);
-            delete obj1[key];
+            removedKeys.push(fullPath)
+            delete sourceObj[key]
         }
-    });
+    })
 }
 
-removeNonMatchingKeys(json1, json2);
+const json1 = {} // paste first json here
+const json2 = {} // paste second json here
+let removedKeys = []
 
-fs.writeFileSync('./modified_jsonfile.json', JSON.stringify(json1, null, 2), 'utf8');
-fs.writeFileSync('./removed_keys.txt', removedKeys.join('\n'), 'utf8');
+removeNonMatchingKeys(json1, json2, '', removedKeys)
+
+fs.writeFileSync('./modified_jsonfile.json', JSON.stringify(json1, null, 2), 'utf8')
+fs.writeFileSync('./removed_keys.json', JSON.stringify(removedKeys, null, 2), 'utf8')
 
 // Console.log to indicate completion
-console.log('Modified JSON saved to ./modified_jsonfile.json');
-console.log('JSON with removed keys saved to ./removed_keys.txt');
+console.log('Modified JSON saved to ./modified_jsonfile.json')
+console.log('JSON with removed keys saved to ./removed_keys.json')
