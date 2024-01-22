0db56f48854dcfe18b3a6b6a6041f808dc4c1d6f
Elias Summermatter <elias.summermatter@seccom.ch>
2024-01-22 16:48:46 +0100

    Added replacer to configuration read function

--- apps/api-example/src/readConfig.ts
+++ apps/api-example/src/readConfig.ts
@@ -1,5 +1,7 @@
 import fs from 'fs'
 import YAML from 'yaml'
+import {MappedReplacer} from 'mapped-replacer'
+import {addToStringReplaceMap} from '../../../libs/payment/api/src/lib/payment-provider/bexio/bexio-utils'
 
 type General = {
   apolloPlayground: boolean
@@ -79,7 +81,29 @@ type Config = {
   challenge: Challenge
 }
 
+function extractReplacer(input: string): string[] {
+  const regex = /\${\s*(.*?)\s*}/g
+  let matches
+  const results: string[] = []
+  while ((matches = regex.exec(input)) !== null) {
+    if (matches.index === regex.lastIndex) {
+      regex.lastIndex++
+    }
+    results.push(matches[1])
+  }
+  return results
+}
+
 export async function readConfig(path: string): Promise<Config> {
+  const stringReplaceMap = new MappedReplacer()
   const file = fs.readFileSync(path, 'utf8')
-  return YAML.parse(file)
+  const replacers = extractReplacer(file)
+  for (const replacer of replacers) {
+    let insertValue = process.env[replacer]
+    if (typeof insertValue === 'undefined') {
+      throw new Error(`In config yaml used envoirment variable <${replacer}> not definded! `)
+    }
+    stringReplaceMap.addRule(`$\{${replacer}\}`, `${insertValue}`)
+  }
+  return YAML.parse(stringReplaceMap.replace(file))
 }
