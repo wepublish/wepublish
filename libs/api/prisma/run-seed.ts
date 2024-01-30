b86b463a4f67d864fc0e953ec0dde89f0954c47c
Elias Summermatter <elias.summermatter@seccom.ch>
2024-01-30 14:37:45 +0100

    Added working docker build

--- libs/api/prisma/run-seed.ts
+++ libs/api/prisma/run-seed.ts
@@ -1,6 +1,23 @@
 import {PrismaClient} from '@prisma/client'
 import {seed as rootSeed} from './seed'
-import {hashPassword, generateSecureRandomPassword} from '../src/lib/db/user'
+import bcrypt from 'bcrypt'
+import {randomBytes} from 'crypto'
+
+const generateSecureRandomPassword = (length: number) => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789._-'
+  let password = ''
+  const characterCount = characters.length
+  const maxValidValue = 256 - (256 % characterCount)
+
+  while (password.length < length) {
+    const randomValue = randomBytes(1)[0]
+    if (randomValue < maxValidValue) {
+      const index = randomValue % characterCount
+      password += characters.charAt(index)
+    }
+  }
+  return password
+}
 
 export async function runSeed() {
   const prisma = new PrismaClient()
@@ -20,7 +37,11 @@ export async function runSeed() {
     }
   })
   if (!admin) {
-    console.log(`Bootstrapping initial admin user ${email} with password: ${randomPassword}`)
+    console.log(
+      '\x1b[31m\x1b[1m%s\x1b[0m',
+      `Bootstrapping initial admin user ${email} with password: ${randomPassword}`
+    )
+
     await prisma.user.upsert({
       where: {
         id: 'admin'
@@ -33,11 +54,14 @@ export async function runSeed() {
         name: 'Admin',
         active: true,
         roleIDs: [adminUserRole.id],
-        password: await hashPassword(randomPassword)
+        password: await bcrypt.hash(randomPassword, 11)
       }
     })
   } else {
-    console.log('Skipping bootstrapping of initial admin user because the user already exist...')
+    console.log(
+      '\x1b[32m\x1b[1m%s\x1b[0m',
+      'Skipping bootstrapping of initial admin user because the user already exist...'
+    )
   }
 
   await prisma.$disconnect()
