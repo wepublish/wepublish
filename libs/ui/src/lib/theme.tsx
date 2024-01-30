8fba0cb31aa59127f089ebcf7baabb79179c42a9
Kamil Antkiewicz <antkiewiczk@gmail.com>
2024-01-30 13:15:28 +0100

    WPC-1458: change footer and header for website builder according to designs (#1146)
    * change footer and header for website builder according to designs
    
    * improve ui, add children to navbar, update snapshots
    
    * update footer snapshots
    
    * Update libs/navigation/website/src/lib/navbar/navbar.tsx
    
    Co-authored-by: Itrulia <karlmerkli@gmail.com>
    
    * Update libs/navigation/website/src/lib/navbar/navbar.tsx
    
    Co-authored-by: Itrulia <karlmerkli@gmail.com>
    
    * Update libs/navigation/website/src/lib/navbar/navbar.tsx
    
    Co-authored-by: Itrulia <karlmerkli@gmail.com>
    
    * Update libs/navigation/website/src/lib/navbar/navbar.tsx
    
    Co-authored-by: Itrulia <karlmerkli@gmail.com>
    
    * Update libs/navigation/website/src/lib/navbar/navbar.tsx
    
    Co-authored-by: Itrulia <karlmerkli@gmail.com>
    
    * Update libs/navigation/website/src/lib/footer/footer.tsx
    
    Co-authored-by: Itrulia <karlmerkli@gmail.com>
    
    * code review refactor
    
    * remove hardcoded values, align to ally best practices, fix website-example, remove unnecessary links from navbar
    
    * update footer design
    
    * update failing snapshots
    
    ---------
    
    Co-authored-by: Itrulia <karlmerkli@gmail.com>

--- libs/ui/src/lib/theme.tsx
+++ libs/ui/src/lib/theme.tsx
@@ -1,10 +1,30 @@
 import {createTheme} from '@mui/material'
 
 export const theme = createTheme({
+  typography: {
+    fontFamily: ['Hanken Grotesk', 'Roboto', 'sans-serif'].join(',')
+  },
   palette: {
+    primary: {
+      main: '#e91e63'
+    },
     secondary: {
       main: '#000',
       contrastText: '#fff'
+    },
+    grey: {
+      800: '#1a1a1a'
+    }
+  },
+  components: {
+    MuiIconButton: {
+      styleOverrides: {
+        root: ({ownerState}) => ({
+          ...(ownerState.color === 'secondary' && {
+            color: '#fff'
+          })
+        })
+      }
     }
   }
 })
