cc75118390a123a5059aa0638d65109f461bc61b
Kamil Antkiewicz <antkiewiczk@gmail.com>
2024-02-28 09:30:23 +0000

    fix gruppetto linting (#1197)

--- apps/gruppetto/src/gruppetto-inner-teaser-wrapper.tsx
+++ apps/gruppetto/src/gruppetto-inner-teaser-wrapper.tsx
@@ -1,4 +1,4 @@
-import {styled, css} from '@mui/material'
+import {styled} from '@mui/material'
 import {TeaserInnerWrapper} from '@wepublish/website'
 
 export const GruppettoTeaserInnerWrapper = styled(TeaserInnerWrapper)`
