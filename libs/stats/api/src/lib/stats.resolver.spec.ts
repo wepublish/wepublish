95efc90f650eafc8ca098c3b65998953b4acc12e
antkiewiczk <antkiewiczk@gmail.com>
2024-03-01 10:30:51 +0100

    stats tests

--- libs/stats/api/src/lib/stats.resolver.spec.ts
+++ libs/stats/api/src/lib/stats.resolver.spec.ts
@@ -1,15 +1,12 @@
 import {Test, TestingModule} from '@nestjs/testing'
 import {INestApplication, Module} from '@nestjs/common'
 import request from 'supertest'
-import * as crypto from 'crypto'
 import {GraphQLModule} from '@nestjs/graphql'
 import {ApolloDriverConfig, ApolloDriver} from '@nestjs/apollo'
-import {PrismaClient, Prisma, Author, Article, ArticleRevision} from '@prisma/client'
+import {PrismaClient} from '@prisma/client'
 import {PrismaModule} from '@wepublish/nest-modules'
 import {StatsResolver} from './stats.resolver'
 import {StatsService} from './stats.service'
-
-export const generateRandomString = () => crypto.randomBytes(20).toString('hex')
 
 @Module({
   imports: [
@@ -25,86 +22,110 @@ export const generateRandomString = () => crypto.randomBytes(20).toString('hex')
 })
 export class AppModule {}
 
-const statsQuery = `
-  query stats() {
-    stats {
-      articlesCount
-      authorsCount
-      earliestArticle
-    }
+const articlesQuery = `
+  query articlesCount {
+    articlesCount
   }
 `
 
-export const mockAuthors: Prisma.AuthorCreateInput[] = [
-  {
-    name: 'Some name',
-    slug: 'some-name'
+const authorsQuery = `
+  query authorsCount {
+    authorsCount
   }
-]
+`
 
-export const mockArticles: Prisma.ArticleCreateInput[] = [
-  {
-    shared: true
+const firstArticleDateQuery = `
+  query firstArticleDate {
+    firstArticleDate
   }
-]
-
-export const mockArticlesRevisions: Prisma.ArticleRevisionCreateInput[] = [
-  {
-    blocks: [],
-    breaking: false,
-    hideAuthor: true,
-    publishedAt: new Date('02.03.2021')
-  }
-]
+`
 
 describe('StatsResolver', () => {
   let app: INestApplication
-  let prisma: PrismaClient
-  let authors: Author[] = []
-  let articles: Article[] = []
-  let articlesRevisions: ArticleRevision[] = []
+  let statsServiceMock: {[method in keyof StatsService]?: jest.Mock}
 
-  beforeAll(async () => {
+  beforeEach(async () => {
+    statsServiceMock = {
+      getArticlesCount: jest.fn(),
+      getAuthorsCount: jest.fn(),
+      getFirstArticleDate: jest.fn()
+    }
+
     const module: TestingModule = await Test.createTestingModule({
-      imports: [AppModule]
+      imports: [
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+          driver: ApolloDriver,
+          autoSchemaFile: true,
+          path: '/',
+          cache: 'bounded'
+        })
+      ],
+      providers: [
+        StatsResolver,
+        {
+          provide: StatsService,
+          useValue: statsServiceMock
+        },
+        {
+          provide: PrismaClient,
+          useValue: jest.fn()
+        }
+      ]
     }).compile()
-
-    prisma = module.get<PrismaClient>(PrismaClient)
-    await prisma.author.deleteMany()
-    await prisma.article.deleteMany()
-    await prisma.articleRevision.deleteMany()
 
     app = module.createNestApplication()
     await app.init()
-
-    authors = await Promise.all(mockAuthors.map(data => prisma.author.create({data})))
-    articles = await Promise.all(mockArticles.map(data => prisma.article.create({data})))
-    articlesRevisions = await Promise.all(
-      mockArticlesRevisions.map(data => prisma.articleRevision.create({data}))
-    )
   })
 
   afterAll(async () => {
-    await prisma.author.deleteMany()
-    await prisma.article.deleteMany()
-    await prisma.articleRevision.deleteMany()
     await app.close()
   })
 
-  test('stats query', async () => {
+  test('getArticlesCount query', async () => {
+    const mockedValue = 123
+    statsServiceMock.getArticlesCount?.mockResolvedValue(mockedValue)
+
     await request(app.getHttpServer())
       .post('')
       .send({
-        query: statsQuery,
+        query: articlesQuery,
         variables: {}
       })
       .expect(200)
       .expect(res => {
-        expect(res.body.data.stats).toMatchObject({
-          articlesCount: 7,
-          authorsCount: 7,
-          earliestArticle: 'dupa'
-        })
+        expect(res.body.data.articlesCount).toEqual(mockedValue)
+      })
+  })
+
+  test('getAuthorsCount query', async () => {
+    const mockedValue = 10
+    statsServiceMock.getAuthorsCount?.mockResolvedValue(mockedValue)
+
+    await request(app.getHttpServer())
+      .post('')
+      .send({
+        query: authorsQuery,
+        variables: {}
+      })
+      .expect(200)
+      .expect(res => {
+        expect(res.body.data.authorsCount).toEqual(mockedValue)
+      })
+  })
+
+  test('getFirstArticleDate query', async () => {
+    const mockedValue = new Date('2022-05-14T10:45:02.513Z')
+    statsServiceMock.getFirstArticleDate?.mockResolvedValue(mockedValue)
+
+    await request(app.getHttpServer())
+      .post('')
+      .send({
+        query: firstArticleDateQuery,
+        variables: {}
+      })
+      .expect(200)
+      .expect(res => {
+        expect(res.body.data.firstArticleDate).toEqual(mockedValue.toISOString())
       })
   })
 })
--- libs/stats/api/src/lib/stats.service.spec.ts
+++ libs/stats/api/src/lib/stats.service.spec.ts
@@ -1,151 +1,62 @@
-// import {Test, TestingModule} from '@nestjs/testing'
-// import {ConsentService} from './consent.service'
-// import {PrismaModule} from '@wepublish/nest-modules'
-// import {Consent, PrismaClient} from '@prisma/client'
-// import {mockConsents} from './consent.resolver.spec'
+import {Test, TestingModule} from '@nestjs/testing'
+import {StatsService} from './stats.service'
+import {PrismaClient} from '@prisma/client'
 
-// describe('StatsService', () => {
-//   let service: ConsentService
-//   let prisma: PrismaClient
-//   let consents: Consent[]
+describe('StatsService', () => {
+  let service: StatsService
+  let prismaMock: {
+    author: {[method in keyof PrismaClient['author']]?: jest.Mock}
+    article: {[method in keyof PrismaClient['article']]?: jest.Mock}
+    articleRevision: {[method in keyof PrismaClient['articleRevision']]?: jest.Mock}
+  }
 
-//   beforeAll(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       imports: [PrismaModule],
-//       providers: [ConsentService]
-//     }).compile()
+  beforeAll(() => {
+    jest.useFakeTimers()
+    jest.setSystemTime(new Date('2023-01-01'))
+  })
 
-//     prisma = module.get<PrismaClient>(PrismaClient)
-//     service = module.get<ConsentService>(ConsentService)
+  afterAll(() => {
+    jest.useRealTimers()
+  })
 
-//     consents = await Promise.all(mockConsents.map(data => prisma.consent.create({data})))
-//   })
+  beforeEach(async () => {
+    prismaMock = {
+      author: {
+        count: jest.fn()
+      },
+      article: {
+        count: jest.fn()
+      },
+      articleRevision: {
+        findFirst: jest.fn()
+      }
+    }
 
-//   test('should be defined', () => {
-//     expect(service).toBeDefined()
-//   })
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [StatsService, {provide: PrismaClient, useValue: prismaMock}]
+    }).compile()
 
-//   test('should get consents', async () => {
-//     const mockValue = Promise.resolve([
-//       {
-//         id: '1572bbfc-a03e-4586-b6a5-e9dab21d54d3',
-//         name: 'some-name',
-//         slug: 'some-slug',
-//         defaultValue: true
-//       },
-//       {
-//         id: '1572bbfc-a03e-4586-b6a5-e9dab21d54d3',
-//         name: 'some-other-name',
-//         slug: 'some-other-slug',
-//         defaultValue: false
-//       }
-//     ])
+    service = module.get<StatsService>(StatsService)
+  })
 
-//     const mockFunction = jest.spyOn(prisma.consent, 'findMany').mockReturnValue(mockValue as any)
+  it('should query for author count', async () => {
+    prismaMock.author.count?.mockResolvedValue(10)
+    await service.getAuthorsCount()
 
-//     const result = await service.consentList()
-//     expect(result).toMatchSnapshot()
-//     expect(mockFunction.mock.calls[0][0]).toMatchSnapshot()
-//   })
+    expect(prismaMock.author.count?.mock.calls[0]).toMatchSnapshot()
+  })
 
-//   test('should get a single consent by id', async () => {
-//     const mockValue = Promise.resolve([
-//       {
-//         id: '1572bbfc-a03e-4586-b6a5-e9dab21d54d3',
-//         name: 'some-name',
-//         slug: 'some-slug',
-//         defaultValue: true
-//       }
-//     ])
+  it('should query for article count', async () => {
+    prismaMock.article.count?.mockResolvedValue(123)
+    await service.getArticlesCount()
 
-//     const mockFunction = jest.spyOn(prisma.consent, 'findUnique').mockReturnValue(mockValue as any)
+    expect(prismaMock.article.count?.mock.calls[0]).toMatchSnapshot()
+  })
 
-//     const id = '1572bbfc-a03e-4586-b6a5-e9dab21d54d3'
-//     const result = await service.consent(id)
-//     expect(result).toMatchSnapshot()
-//     expect(mockFunction.mock.calls[0][0]).toMatchSnapshot()
-//   })
+  it('should query for articleRevision findFirst', async () => {
+    prismaMock.articleRevision.findFirst?.mockResolvedValue({})
+    await service.getFirstArticleDate()
 
-//   test('should create a consent', async () => {
-//     const mockValue = Promise.resolve([
-//       {
-//         id: '1572bbfc-a03e-4586-b6a5-e9dab21d54d3',
-//         name: 'some-name',
-//         slug: 'some-slug',
-//         defaultValue: true
-//       }
-//     ])
-
-//     const mockFunction = jest.spyOn(prisma.consent, 'create').mockReturnValue(mockValue as any)
-
-//     const consent = {
-//       name: 'some-name',
-//       slug: 'some-slug',
-//       defaultValue: true
-//     }
-
-//     const result = await service.createConsent(consent)
-//     expect(result).toMatchSnapshot()
-//     expect(mockFunction.mock.calls[0][0]).toMatchSnapshot()
-//   })
-
-//   test('should update a consent', async () => {
-//     const idToUpdate = consents[0].id
-
-//     const mockValue = Promise.resolve([
-//       {
-//         id: idToUpdate,
-//         consent: {
-//           defaultValue: false
-//         }
-//       }
-//     ])
-
-//     const mockFunction = jest.spyOn(prisma.consent, 'update').mockReturnValue(mockValue as any)
-
-//     const consent = {
-//       name: consents[0].name,
-//       slug: consents[0].slug,
-//       defaultValue: false
-//     }
-
-//     const result = await service.updateConsent({id: idToUpdate, ...consent})
-//     expect(result).toMatchObject([
-//       {
-//         id: idToUpdate,
-//         consent: {
-//           defaultValue: false
-//         }
-//       }
-//     ])
-//     expect(mockFunction.mock.calls[0][0]).toMatchObject({
-//       where: {
-//         id: idToUpdate
-//       }
-//     })
-//   })
-
-//   test('should delete a consent', async () => {
-//     const idToDelete = consents[0].id
-
-//     const mockValue = Promise.resolve([
-//       {
-//         id: idToDelete
-//       }
-//     ])
-
-//     const mockFunction = jest.spyOn(prisma.consent, 'delete').mockReturnValue(mockValue as any)
-
-//     const result = await service.deleteConsent(idToDelete)
-//     expect(result).toMatchObject([
-//       {
-//         id: idToDelete
-//       }
-//     ])
-//     expect(mockFunction.mock.calls[0][0]).toMatchObject({
-//       where: {
-//         id: idToDelete
-//       }
-//     })
-//   })
-// })
+    expect(prismaMock.articleRevision.findFirst?.mock.calls[0]).toMatchSnapshot()
+  })
+})
