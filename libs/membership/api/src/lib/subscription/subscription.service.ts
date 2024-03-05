eb148ae12834baae8a0996f720cdb268e7b98784
Elias Summermatter <elias.summermatter@seccom.ch>
2024-03-05 15:42:46 +0100

    Fixed lint

--- libs/membership/api/src/lib/subscription/subscription.service.ts
+++ libs/membership/api/src/lib/subscription/subscription.service.ts
@@ -16,7 +16,7 @@ import {
 } from '@prisma/client'
 import {PrismaService} from '@wepublish/nest-modules'
 import {PaymentProvider, PaymentsService} from '@wepublish/payment/api'
-import {add, endOfDay, startOfDay, sub} from 'date-fns'
+import {add, endOfDay, startOfDay} from 'date-fns'
 import {Action} from '../subscription-event-dictionary/subscription-event-dictionary.type'
 import {mapPaymentPeriodToMonths} from '@wepublish/utils/api'
 
