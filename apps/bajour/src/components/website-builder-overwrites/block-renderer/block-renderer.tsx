780d5ac1db946ceb9c2c060e4de46810268f32f0
Karl Merkli <karlmerkli@gmail.com>
2024-03-05 13:40:12 +0100

    fix(bajour): build

--- apps/bajour/src/components/website-builder-overwrites/block-renderer/block-renderer.tsx
+++ apps/bajour/src/components/website-builder-overwrites/block-renderer/block-renderer.tsx
@@ -1,7 +1,7 @@
 import {BlockRenderer, BuilderBlockRendererProps} from '@wepublish/website'
 import {cond} from 'ramda'
 
-import {Archive, ArchiveProps} from '../../bajour/archive/archive'
+import {Archive} from '../../bajour/archive/archive'
 import {isArchive} from '../../bajour/archive/is-archive'
 import {BestOfWePublish} from '../../bajour/best-of-wepublish/best-of-wepublish'
 import {isBestOfWePublish} from '../../bajour/best-of-wepublish/is-best-of-wepublish'
@@ -13,7 +13,7 @@ const extraBlockMap = cond([
   [isTeaserSlider, block => <TeaserSlider {...block} />],
   [isBestOfWePublish, block => <BestOfWePublish {...block} />],
   [isAnyBriefing, block => <BaselBriefing {...(block as BaselBriefingProps)} />],
-  [isArchive, block => <Archive {...(block as ArchiveProps)} />]
+  [isArchive, block => <Archive {...block} />]
 ])
 
 export const BajourBlockRenderer = (props: BuilderBlockRendererProps) => {
