29681e8eae72bd47b0dd9298ed295adee64b3fca
Karl Merkli <karlmerkli@gmail.com>
2024-03-08 16:47:57 +0100

    feat(bajour): add ssr data of navbar

--- apps/bajour/pages/event/index.tsx
+++ apps/bajour/pages/event/index.tsx
@@ -116,6 +116,9 @@ export const getStaticProps: GetStaticProps = async () => {
       }
     }),
     client.query({
+      query: ApiV1.NavigationListDocument
+    }),
+    client.query({
       query: ApiV1.PeerProfileDocument
     })
   ])
