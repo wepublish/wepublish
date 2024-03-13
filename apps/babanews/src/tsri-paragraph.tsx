0132c5de0021a44473d34d0556589c864030e4c9
Tomasz Durka <tomasz@durka.pl>
2024-03-13 11:39:49 +0100

    feat(babanews): Add babanews website based on website-example

---
+++ apps/babanews/src/tsri-paragraph.tsx
@@ -0,0 +1,23 @@
+import {css, Typography} from '@mui/material'
+import {ParagraphProps} from '@wepublish/ui'
+import {forwardRef} from 'react'
+
+const maxParagraphSize = css`
+  max-width: 75ch;
+`
+
+export const TsriParagraph = forwardRef<HTMLParagraphElement, ParagraphProps>(
+  function TsriParagraph({children, gutterBottom = true, ...props}: ParagraphProps, ref) {
+    return (
+      <Typography
+        {...props}
+        ref={ref}
+        variant="h6"
+        paragraph
+        gutterBottom={gutterBottom}
+        css={maxParagraphSize}>
+        {children}
+      </Typography>
+    )
+  }
+)
