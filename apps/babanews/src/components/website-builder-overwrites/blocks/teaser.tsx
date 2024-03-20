685e561a37a6e96c655485e2ab1aa09936dedaf4
antkiewiczk <antkiewiczk@gmail.com>
2024-03-20 13:27:06 +0100

    add support for teaser

--- apps/babanews/src/components/website-builder-overwrites/blocks/teaser.tsx
+++ apps/babanews/src/components/website-builder-overwrites/blocks/teaser.tsx
@@ -1,16 +1,17 @@
-import {BuilderTeaserProps} from '@wepublish/website'
+import {BuilderTeaserProps, Teaser} from '@wepublish/website'
 
 // import {ColTeaser} from './col-teaser'
 import {SingleTeaser} from './single-teaser'
 
 export const BabanewsTeaser = (props: BuilderTeaserProps) => {
-  // if (!props.numColumns) {
-  //   return <Teaser {...props} />
-  // }
+  if (!props.numColumns) {
+    return <SingleTeaser {...props} />
+  }
 
   // if (props.numColumns === 1) {
-  return <SingleTeaser {...props} />
+  //   return <SingleTeaser {...props} />
   // }
 
   // return <ColTeaser {...props} />
+  return <Teaser {...props} />
 }
