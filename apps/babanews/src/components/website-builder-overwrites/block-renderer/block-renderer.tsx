88923ba9d607c12a48b0544aa0d0731d2da9e32c
antkiewiczk <antkiewiczk@gmail.com>
2024-03-21 14:53:51 +0100

    BABA-17: instagram banner

--- apps/babanews/src/components/website-builder-overwrites/block-renderer/block-renderer.tsx
+++ apps/babanews/src/components/website-builder-overwrites/block-renderer/block-renderer.tsx
@@ -1,13 +1,17 @@
+import {useTheme} from '@mui/material'
 import {BlockRenderer, BuilderBlockRendererProps, useWebsiteBuilder} from '@wepublish/website'
 import {cond} from 'ramda'
 import {useMemo} from 'react'
 
 import {FullWidthBanner} from '../../babanews/fullwidth-banner/fullwidth-banner'
 import {isFullWidthBanner} from '../../babanews/fullwidth-banner/is-fullwidth-banner'
+import {InstagramBanner} from '../../babanews/instagram-banner/instagram-banner'
+import {isInstagramBanner} from '../../babanews/instagram-banner/is-instagram-banner'
 import {Container} from '../../layout/container'
 import {FullWidthContainer} from '../../layout/full-width-container'
 
 export const BabanewsBlockRenderer = (props: BuilderBlockRendererProps) => {
+  const theme = useTheme()
   const {
     blocks: {TeaserGrid}
   } = useWebsiteBuilder()
@@ -21,6 +25,16 @@ export const BabanewsBlockRenderer = (props: BuilderBlockRendererProps) => {
             <FullWidthContainer>
               <Container>
                 <FullWidthBanner {...block} />
+              </Container>
+            </FullWidthContainer>
+          )
+        ],
+        [
+          isInstagramBanner,
+          block => (
+            <FullWidthContainer backgroundColor={theme.palette.common.black}>
+              <Container>
+                <InstagramBanner {...block} />
               </Container>
             </FullWidthContainer>
           )
