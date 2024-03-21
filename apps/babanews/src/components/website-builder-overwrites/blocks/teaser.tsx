880707132f39ae04f01058edb624e29ed25b9129
antkiewiczk <antkiewiczk@gmail.com>
2024-03-21 11:45:55 +0100

    remove col-teaser

--- apps/babanews/src/components/website-builder-overwrites/blocks/teaser.tsx
+++ apps/babanews/src/components/website-builder-overwrites/blocks/teaser.tsx
@@ -1,6 +1,5 @@
 import {BuilderTeaserProps, Teaser} from '@wepublish/website'
 
-// import {ColTeaser} from './col-teaser'
 import {SingleTeaser} from './single-teaser'
 
 export const BabanewsTeaser = (props: BuilderTeaserProps) => {
@@ -8,10 +7,5 @@ export const BabanewsTeaser = (props: BuilderTeaserProps) => {
     return <SingleTeaser {...props} />
   }
 
-  // if (props.numColumns === 1) {
-  //   return <SingleTeaser {...props} />
-  // }
-
-  // return <ColTeaser {...props} />
   return <Teaser {...props} />
 }
