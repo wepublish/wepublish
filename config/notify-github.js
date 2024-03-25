0468502e4ac418b13c47c7918b288c04c2c042af
Tomasz Durka <tomasz@durka.pl>
2024-03-25 15:56:15 +0100

    chore(ci/cd): streamline

--- config/notify-github.js
+++ config/notify-github.js
@@ -10,21 +10,25 @@ async function main() {
         WEBSITE_URL,
         MEDIA_SERVER_URL,
         PROJECTS,
-        PROJECT_URL_PATTERN,
+        PROJECT_URL_SUFFIX,
     } = process.env
 
     let deploymentMessage = `<a href="https://github.com/wepublish/wepublish/pull/${PR_NUMBER}">PR ${PR_NUMBER}</a> with branch \`${BRANCH_NAME}\` has been deployed to:
+
+<h2>Main</h2>
   - Website: ${WEBSITE_URL}
   - Editor: ${EDITOR_URL}
   - Public API: ${API_URL}/v1
   - Private API: ${API_URL}/v1/admin
   - Media: ${MEDIA_SERVER_URL}
+
+<h2>Projects:</h2>
 `
 
     const projects = JSON.parse(PROJECTS);
     for (const project of projects) {
         deploymentMessage += `
-  - ${project}: ${PROJECT_URL_PATTERN.replace('{PROJECT}', project)}`
+  - ${project}: https://${project}${PROJECT_URL_SUFFIX}`
     }
     console.log(deploymentMessage)
 }
