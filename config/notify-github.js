f11d4fe6f262e5274dee4755799e08cbb62a51fe
Tomasz Durka <tomasz@durka.pl>
2024-03-25 13:35:42 +0100

    chore(ci/cd): Test the message

--- config/notify-github.js
+++ config/notify-github.js
@@ -1,14 +1,31 @@
 const {setFailed} = require('@actions/core')
+const {readdir} = require("fs/promises");
 
 async function main() {
-    const {BRANCH_NAME, PR_NUMBER, API_URL, EDITOR_URL, WEBSITE_URL, MEDIA_SERVER_URL} = process.env
+    const {
+        BRANCH_NAME,
+        PR_NUMBER,
+        API_URL,
+        EDITOR_URL,
+        WEBSITE_URL,
+        MEDIA_SERVER_URL,
+        WEBSITE_CLIENT_URL_PATTERN
+    } = process.env
 
-    const deploymentMessage = `<a href="https://github.com/wepublish/wepublish/pull/${PR_NUMBER}">PR ${PR_NUMBER}</a> with branch \`${BRANCH_NAME}\` has been deployed to:
+    let deploymentMessage = `<a href="https://github.com/wepublish/wepublish/pull/${PR_NUMBER}">PR ${PR_NUMBER}</a> with branch \`${BRANCH_NAME}\` has been deployed to:
   - Website: ${WEBSITE_URL}
   - Editor: ${EDITOR_URL}
   - Public API: ${API_URL}/v1
   - Private API: ${API_URL}/v1/admin
   - Media: ${MEDIA_SERVER_URL}`
+
+    const ignoreList = ['api-example', 'website-example', 'editor'];
+    const files = await readdir(__dirname + '/apps');
+    for (const app of files) {
+        if (!ignoreList.includes(app)) {
+            deploymentMessage += `\n- ${app}: ${WEBSITE_CLIENT_URL_PATTERN.replace('{APP}', app)}`
+        }
+    }
 
     console.log(deploymentMessage)
 }
