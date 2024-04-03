fe70552c8cbac722a4157ad4adf4e35204a07200
Itrulia <karlmerkli@gmail.com>
2024-04-03 13:42:59 +0200

    Update apps/babanews/pages/index.tsx

--- apps/babanews/pages/index.tsx
+++ apps/babanews/pages/index.tsx
@@ -12,6 +12,7 @@ import getConfig from 'next/config'
 const Frontpage = styled(PageContainer)`
   gap: ${({theme}) => theme.spacing(6)};
   padding-bottom: ${({theme}) => theme.spacing(6)};
+
   ${TeaserGridFlexBlockWrapper}:first-of-type
   ${TeaserWrapper}:first-of-type {
     ${TeaserDate} {
