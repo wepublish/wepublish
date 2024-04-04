fa27ec91d2ec2d9d71c3d79f6ddc51743bf54677
Kamil Antkiewicz <antkiewiczk@gmail.com>
2024-04-04 15:49:34 +0100

    WP-152: Update basic teaser design in website-builder (#1257)
    * update basic teaser design in website-builder
    
    * update more snapshots
    
    * updates from CR
    
    * update snapshots, fix duplicate import
    
    * rewrite styles, rename ImageWrapperStyled
    
    * use grid-template-areas, change secondary color in theme
    
    * update snapshots

--- libs/ui/src/lib/theme.tsx
+++ libs/ui/src/lib/theme.tsx
@@ -2,14 +2,21 @@ import {createTheme} from '@mui/material'
 
 export const theme = createTheme({
   typography: {
-    fontFamily: ['Hanken Grotesk', 'Roboto', 'sans-serif'].join(',')
+    fontFamily: ['Hanken Grotesk', 'Roboto', 'sans-serif'].join(','),
+    h4: {
+      fontWeight: '600',
+      fontSize: '26px',
+      '@media (min-width:900px)': {
+        fontSize: '32px'
+      }
+    }
   },
   palette: {
     primary: {
       main: '#e91e63'
     },
     secondary: {
-      main: '#000',
+      main: '#F5FF64',
       contrastText: '#fff'
     },
     grey: {
