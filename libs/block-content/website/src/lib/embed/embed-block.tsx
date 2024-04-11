969f1f151a68980cb0f97c593ee00727a808f4c3
Karl Merkli <karlmerkli@gmail.com>
2024-04-11 12:06:39 +0200

    feat(block-content-website): automatically adjust iframe height

--- libs/block-content/website/src/lib/embed/embed-block.tsx
+++ libs/block-content/website/src/lib/embed/embed-block.tsx
@@ -3,16 +3,22 @@ import {Block, EmbedBlock as EmbedBlockType} from '@wepublish/website/api'
 import {BuilderEmbedBlockProps} from '@wepublish/website/builder'
 import {css} from '@emotion/react'
 import {useMemo} from 'react'
+import IframeResizer from 'iframe-resizer-react'
 
 export const isEmbedBlock = (block: Block): block is EmbedBlockType =>
   block.__typename === 'EmbedBlock'
 
 export const EmbedBlockWrapper = styled('div')``
 
-export const EmbedBlockIframe = styled('iframe')<{aspectRatio: number}>`
+export const EmbedBlockIframe = styled(IframeResizer)<{aspectRatio: number | null}>`
   width: 100%;
   border: 0;
-  aspect-ratio: ${({aspectRatio}) => aspectRatio};
+
+  ${({aspectRatio}) =>
+    aspectRatio &&
+    css`
+      aspect-ratio: ${aspectRatio};
+    `}
 `
 
 export function EmbedBlock({
@@ -24,7 +30,7 @@ export function EmbedBlock({
   sandbox,
   className
 }: BuilderEmbedBlockProps) {
-  const ratio = width && height ? width / height : 1
+  const ratio = width && height ? width / height : null
 
   const styleCustomCss = useMemo(
     () => css`
@@ -41,7 +47,7 @@ export function EmbedBlock({
         src={url}
         title={title ?? undefined}
         allowFullScreen
-        sandbox={sandbox ?? undefined}
+        sandbox={sandbox || undefined}
       />
     </EmbedBlockWrapper>
   ) : (
