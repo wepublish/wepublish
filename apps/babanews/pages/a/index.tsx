d9adb450d0cf31d8ab211f444ae2ebe262bbe904
antkiewiczk <antkiewiczk@gmail.com>
2024-04-12 14:57:26 +0200

    move pagination to babanews/pages/a, reverse changes in basic article-list

--- apps/babanews/pages/a/index.tsx
+++ apps/babanews/pages/a/index.tsx
@@ -1,9 +1,62 @@
+import styled from '@emotion/styled'
+import {Pagination as MuiPagination} from '@mui/material'
 import {ApiV1, ArticleListContainer} from '@wepublish/website'
 import {GetStaticProps} from 'next'
 import getConfig from 'next/config'
+import {useEffect, useMemo, useState} from 'react'
+import {Reducer, useReducer} from 'react'
+
+export const Pagination = styled(MuiPagination)`
+  grid-column: 1 / 5;
+  display: grid;
+  justify-content: center;
+`
 
 export default function ArticleList() {
-  return <ArticleListContainer />
+  const [page, setPage] = useState(1)
+  const limit = 2
+
+  const [variables, onVariablesChange] = useReducer<
+    Reducer<Partial<ApiV1.ArticleListQueryVariables>, Partial<ApiV1.ArticleListQueryVariables>>
+  >(
+    (state, newVariables) => ({
+      ...state,
+      ...newVariables
+    }),
+    {
+      sort: ApiV1.ArticleSort.PublishedAt,
+      order: ApiV1.SortOrder.Descending,
+      skip: 0,
+      take: limit
+    } as Partial<ApiV1.ArticleListQueryVariables>
+  )
+
+  const {data} = ApiV1.useArticleListQuery({
+    fetchPolicy: 'cache-only',
+    variables
+  })
+
+  const paginationCount = useMemo(() => {
+    if (data?.articles.totalCount && data?.articles.totalCount > limit) {
+      return Math.ceil(data.articles.totalCount / limit)
+    }
+    return 1
+  }, [data?.articles.totalCount, limit])
+
+  useEffect(() => {
+    onVariablesChange({
+      skip: (page - 1) * limit
+    })
+  }, [page])
+
+  return (
+    <>
+      <ArticleListContainer variables={variables} onVariablesChange={onVariablesChange} />
+      {paginationCount > 1 && (
+        <Pagination page={page} count={paginationCount} onChange={(_, value) => setPage(value)} />
+      )}
+    </>
+  )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
--- libs/article/website/src/lib/article-list/article-list.tsx
+++ libs/article/website/src/lib/article-list/article-list.tsx
@@ -1,12 +1,5 @@
-import {Pagination, css, styled} from '@mui/material'
+import {css, styled} from '@mui/material'
 import {BuilderArticleListProps, useWebsiteBuilder} from '@wepublish/website/builder'
-import {useEffect, useMemo, useState} from 'react'
-
-export const PaginationStyled = styled(Pagination)`
-  grid-column: 1 / 5;
-  display: grid;
-  justify-content: center;
-`
 
 export const ArticleListWrapper = styled('article')`
   display: grid;
@@ -28,35 +21,14 @@ export const ArticleListWrapper = styled('article')`
   `}
 `
 
-export const ArticleList = ({data, className, onVariablesChange}: BuilderArticleListProps) => {
+export const ArticleList = ({data, className}: BuilderArticleListProps) => {
   const {ArticleListItem} = useWebsiteBuilder()
-  const [page, setPage] = useState(1)
-  const limit = 10
-
-  const articleListVariables = {
-    take: limit,
-    skip: (page - 1) * limit
-  }
-
-  const paginationCount = useMemo(() => {
-    if (data?.articles.totalCount && data?.articles.totalCount > limit) {
-      return Math.ceil(data.articles.totalCount / limit)
-    }
-    return 1
-  }, [data?.articles.totalCount, limit])
-
-  useEffect(() => {
-    onVariablesChange && onVariablesChange(articleListVariables)
-  }, [page])
 
   return (
     <ArticleListWrapper className={className}>
       {data?.articles?.nodes.map(article => (
         <ArticleListItem key={article.id} {...(article as any)} />
       ))}
-      {paginationCount > 1 && (
-        <PaginationStyled count={paginationCount} onChange={(_, value) => setPage(value)} />
-      )}
     </ArticleListWrapper>
   )
 }
