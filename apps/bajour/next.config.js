176f6d9f4a1036a0aa8ca1f55ea6f40471715ac3
Karl Merkli <karlmerkli@gmail.com>
2024-04-26 11:17:40 +0200

    refactor(website): re-use most of next.config.js

--- apps/bajour/next.config.js
+++ apps/bajour/next.config.js
@@ -1,7 +1,7 @@
 //@ts-check
 
 const {composePlugins, withNx} = require('@nx/next')
-const {join} = require('path')
+const wepNextConfig = require('../../libs/utils/website/src/lib/next.config')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.NODE_ENV === 'production',
   openAnalyzer: false
@@ -11,102 +11,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-  nx: {
-    svgr: true
-  },
-  poweredByHeader: false,
-  reactStrictMode: true,
+  ...wepNextConfig,
   publicRuntimeConfig: {
     env: {
       API_URL: process.env.API_URL || '',
       STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY || '',
       GA_ID: process.env.GA_ID || ''
     }
-  },
-  i18n: {
-    locales: ['gsw-CH'],
-    defaultLocale: 'gsw-CH'
-  },
-  compiler: {
-    emotion: {
-      sourceMap: true,
-      importMap: {
-        '@mui/material': {
-          styled: {
-            canonicalImport: ['@emotion/styled', 'default'],
-            styledBaseImport: ['@mui/material', 'styled']
-          }
-        },
-        '@mui/material/styles': {
-          styled: {
-            canonicalImport: ['@emotion/styled', 'default'],
-            styledBaseImport: ['@mui/material/styles', 'styled']
-          }
-        }
-      }
-    }
-  },
-  experimental: {
-    scrollRestoration: true,
-    outputFileTracingRoot: join(__dirname, '../../'),
-    outputFileTracingExcludes: {
-      '*': [
-        '**/node_modules/@swc/core-linux-x64-musl',
-        '**/node_modules/@swc/core-linux-x64-gnu ',
-        '**/node_modules/@esbuild/linux-x64'
-      ]
-    }
-  },
-  transpilePackages: ['@wepublish/ui', '@wepublish/website', 'react-tweet'],
-  async redirects() {
-    return [
-      {
-        source: '/home',
-        destination: '/',
-        permanent: false
-      },
-      {
-        source: '/a/:id((?!tag).*)/:slug',
-        destination: '/a/:slug',
-        permanent: false
-      },
-      {
-        source: '/tag/:slug',
-        destination: '/a/tag/:slug',
-        permanent: false
-      },
-      {
-        source: '/profile/dashboard',
-        destination: '/login',
-        permanent: false,
-        has: [
-          {
-            type: 'query',
-            key: 'jwt'
-          }
-        ]
-      },
-      {
-        source: '/profile/dashboard',
-        destination: '/profile',
-        permanent: false
-      },
-      {
-        source: '/member-uebersicht',
-        destination: '/mitmachen',
-        permanent: false
-      },
-      {
-        source: '/goenner-uebersicht',
-        destination: '/mitmachen',
-        permanent: false
-      },
-      {
-        source: '/spende',
-        destination: '/mitmachen',
-        permanent: false
-      }
-    ]
   }
 }
 
