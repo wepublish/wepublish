973352b500092d89bafdae49c41e6eed4be886cc
Michael Scheurer <michael.scheurer@seccom.ch>
2024-05-07 15:43:39 +0200

    feat/BAJ-128-briefing-with-scheduled-date

--- apps/bajour/src/utils/is-within-timeslot.ts
+++ apps/bajour/src/utils/is-within-timeslot.ts
@@ -1,5 +1,21 @@
-export const isWithinTimeslot = (showFrom = '6:00', showUntil = '11:00'): boolean => {
+export const isWithinTimeslot = (
+  showFrom = '6:00',
+  showUntil = '11:00',
+  scheduledDate: Date | undefined = undefined
+): boolean => {
   const now = new Date()
+
+  // check if the scheduled date is today.
+  if (scheduledDate) {
+    const isSameDay = scheduledDate.getDate() === now.getDate()
+    const isSameMonth = scheduledDate.getMonth() === now.getMonth()
+    const isSameYear = scheduledDate.getFullYear() === now.getFullYear()
+
+    // since we've got a date object, just return false, if the scheduled date isn't today.
+    if (!isSameDay || !isSameMonth || !isSameYear) {
+      return false
+    }
+  }
 
   const [showFromHour, showFromMinutes] = showFrom.split(':').map(Number)
   const [showUntilHour, showUntilMinutes] = showUntil.split(':').map(Number)
