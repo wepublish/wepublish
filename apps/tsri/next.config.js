9c3d92a59b5454046b26321ebd25a873312a325f
Elias Summermatter <elias.summermatter@seccom.ch>
2024-06-12 10:16:52 +0200

    Moved headers to general config

--- apps/tsri/next.config.js
+++ apps/tsri/next.config.js
@@ -16,29 +16,6 @@ const nextConfig = {
     env: {
       API_URL: process.env.API_URL || ''
     }
-  },
-  async headers() {
-    return [
-      {
-        // Apply these headers to all routes except /profile/*
-        source: '/((?!profile).*)',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 's-maxage=59, stale-while-revalidate=59, maxage=59, public'
-          }
-        ]
-      },
-      {
-        source: '/',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 's-maxage=59, stale-while-revalidate=59, maxage=59, public'
-          }
-        ]
-      }
-    ]
   }
 }
 
