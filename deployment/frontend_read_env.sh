9131bcd2120f7fa3cabbb7e7b7388b29f0565d05
Elias Summermatter <elias.summermatter@seccom.ch>
2024-06-26 14:07:52 +0200

    Build again

--- deployment/frontend_read_env.sh
+++ deployment/frontend_read_env.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 ENV=$1
 PROJECT=$2
+shift 2
 customVars=$(jq -r ".frontend.${ENV}.env | to_entries | map(\"\(.key)=\(.value|tostring)\") | .[]" apps/${PROJECT}/deployment.config.json)
 for var in $customVars; do
   envvars="${envvars}${var}\n"
+done
+for arg in "$@"; do
+    echo "${envvars}" |grep -v "$(echo $arg |cut -d '=' -f 1)=" > /dev/null
+    if [[ $? == 0 ]]; then
+      envvars="${envvars}${arg}\n"
+    fi
 done
 envvars="${envvars//'%'/'%25'}"
 envvars="${envvars//$'\n'/'%0A'}"
