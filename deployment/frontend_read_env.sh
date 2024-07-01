7b00690fd7781153a68567fcfc8f117dd9de7e5e
Elias Summermatter <elias.summermatter@seccom.ch>
2024-07-01 16:01:12 +0200

    Working on secrets

--- deployment/frontend_read_env.sh
+++ deployment/frontend_read_env.sh
@@ -2,17 +2,43 @@
 ENV=$1
 PROJECT=$2
 FORMAT=$3
+echo ${ENV} | tr '[:lower:]' '[:upper:]'
+SECRETS=$(cat secrets.json |jq "." |grep "DEPLOYMENT_$(echo ${ENV} | tr '[:lower:]' '[:upper:]')_" )
+echo "DEPLOYMENT_$(echo ${ENV} | tr '[:lower:]' '[:upper:]')_"
+cat secrets.json
+echo $SECRETS
+PROJECT_FILE=apps/${PROJECT}/deployment.config.json
 shift 3
-customVars=$(jq -r ".frontend.${ENV}.env | to_entries | map(\"\(.key)=\(.value|tostring)\") | .[]" apps/${PROJECT}/deployment.config.json)
+
+envvars=""
+customVars=$(jq -r ".frontend.${ENV}.env | to_entries | map(\"\(.key)=\(.value|tostring)\") | .[]" $PROJECT_FILE)
 for var in $customVars; do
   envvars="${envvars}${var}\n"
 done
+
+echo $(jq -r ".frontend.${ENV}.secret_env[]" "$PROJECT_FILE")
+for secret in $(jq -r ".frontend.${ENV}.secret_env[]" "$PROJECT_FILE"); do
+  echo "${secret}" |grep -v "$(echo $arg |cut -d '=' -f 1)=" > /dev/null
+  if [[ $? == 0 ]]; then
+    value=$(echo $SECRETS |grep "\"$secret\":" | cut -d':' -f 2 |sed 's/[ "]//g')
+    if [[ -z $value ]]; then
+      echo "$secret does not exist in context!"
+      continue
+    else
+      echo "Setting $secret to $value!"
+    fi
+    envvars="${envvars}${secret}=${value}\n"
+  fi
+done
+
+
 for arg in "$@"; do
     echo "${envvars}" |grep -v "$(echo $arg |cut -d '=' -f 1)=" > /dev/null
     if [[ $? == 0 ]]; then
       envvars="${envvars}${arg}\n"
     fi
 done
+
 if [[ $FORMAT == "yaml" ]]; then
   envvars=$(echo "$envvars" | sed 's/=/: /g')
 fi
