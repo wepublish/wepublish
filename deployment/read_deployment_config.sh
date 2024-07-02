c45ee61648b44593c3a55a055b113079b5706e8a
Elias Summermatter <elias.summermatter@seccom.ch>
2024-07-02 23:28:06 +0200

    Test123

--- deployment/read_deployment_config.sh
+++ deployment/read_deployment_config.sh
@@ -1,11 +1,25 @@
+#!/bin/bash
 KEY=$1
 VALUE=$2
+PROJECT=$3
 files=$(find apps -mindepth 2 -maxdepth 2 -type f -name deployment.config.json)
 for file in $files; do
   if [[ $(jq "$KEY" "$file") == "$VALUE" ]]; then
     targets=$(echo $targets $(dirname "$file" | xargs -n 1 basename))
   fi
 done
+
+# If project is passed check for the project
+if [[ ! -z $PROJECT ]]; then
+  if [[ -z $(echo "@$targets@" |sed "s/ /@/g" |grep "@${PROJECT}@") ]]; then
+    echo "Project <$PROJECT> has no frontend, skipping..."
+    exit 99
+  else
+    echo "Project <$PROJECT> has frontend."
+    exit 0
+  fi
+fi
+
 matrix="{\"target\":["
 for target in $targets; do
 	matrix="${matrix}\"${target}\","
