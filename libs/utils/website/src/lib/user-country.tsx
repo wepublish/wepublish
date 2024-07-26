77bc2fb7239187f3b58e131cc87c9d0f48f0010f
Itrulia <karlmerkli@gmail.com>
2024-07-26 14:47:12 +0200

    feat(utils-website): add hook to get a users country from cloudflare (#1439)

---
+++ libs/utils/website/src/lib/user-country.tsx
@@ -0,0 +1,42 @@
+import getConfig from 'next/config'
+import {useEffect, useState} from 'react'
+
+const fetchUserCountry = (initArgs?: Parameters<typeof fetch>[1]) => {
+  const {publicRuntimeConfig} = getConfig()
+
+  const url =
+    process.env.NODE_ENV === 'production'
+      ? `${publicRuntimeConfig.API_ENV}/cdn-cgi/trace`
+      : 'https://cloudflare-dns.com/cdn-cgi/trace'
+
+  return fetch(url, initArgs).then(async res => {
+    const text = await res.text()
+
+    const matches = text.match(/loc=(.*)/)
+
+    return matches?.[1]
+  })
+}
+
+export const useUserCountry = () => {
+  const [userCountry, setUserCountry] = useState<string>()
+
+  useEffect(() => {
+    const controller = new AbortController()
+
+    fetchUserCountry({
+      cache: 'force-cache',
+      signal: controller.signal
+    })
+      .then(setUserCountry)
+      .catch(error => {
+        if (!controller.signal.aborted) {
+          return Promise.reject(error)
+        }
+      })
+
+    return () => controller.abort()
+  }, [])
+
+  return userCountry
+}
