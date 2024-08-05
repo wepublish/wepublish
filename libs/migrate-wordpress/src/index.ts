9c63b71f64a9220970edb829fdb73a30a131d50e
Tomasz Durka <tomasz@durka.pl>
2024-08-06 01:57:22 +0200

    Prepare for creating authors and images

--- libs/migrate-wordpress/src/index.ts
+++ libs/migrate-wordpress/src/index.ts
@@ -1,7 +1,16 @@
 import axios from 'axios'
 import cheerio from 'cheerio'
-import {BlockType, BlockWithoutJSON, RichTextNode} from '@wepublish/api'
+import {Author, BlockType, BlockWithoutJSON, Image, RichTextNode} from '@wepublish/api'
 import {htmlToSlate} from 'slate-serializers'
+
+type WordpressAuthor = {
+  id: number
+  name: string
+  url: string
+  description: string
+  link: string
+  slug: string
+}
 
 interface WordPressPost {
   id: number
@@ -17,18 +26,16 @@ interface WordPressPost {
         alt_text: string
       }
     ]
-    author: {
-      id: number
-      name: string
-      url: string
-      description: string
-      link: string
-      slug: string
-    }[]
+    author: WordpressAuthor[]
   }
 }
 
-interface MigratedArticle {
+type WordpressImage = {
+  url: string
+  alt: string
+}
+
+interface ArticleProps {
   title: string
   date: string
   slug: string
@@ -47,7 +54,17 @@ const fetchPosts = async (page: number, perPage: number): Promise<WordPressPost[
   return response.data
 }
 
-const migratePost = (post: WordPressPost): MigratedArticle => {
+const createAuthor = async (author: WordpressAuthor): Promise<Author> => {
+  console.log('create author', author)
+  return {} as Author
+}
+
+const createImage = async (image: WordpressImage): Promise<Image> => {
+  console.log('create image', image)
+  return {id: image.url} as Image
+}
+
+const migratePost = async (post: WordPressPost): Promise<ArticleProps> => {
   const {title, content, date, slug, link, _embedded} = post
 
   let htmlContent: RichTextNode | undefined
@@ -56,67 +73,82 @@ const migratePost = (post: WordPressPost): MigratedArticle => {
   const $ = cheerio.load(content.rendered)
   const blocks: BlockWithoutJSON[] = []
 
+  // Authors
+  for (const author of _embedded?.author ?? []) {
+    await createAuthor(author)
+  }
+
   if (_embedded?.['wp:featuredmedia']?.length) {
+    const featuredImage = await createImage({
+      url: _embedded?.['wp:featuredmedia'][0].source_url,
+      alt: _embedded?.['wp:featuredmedia'][0].alt_text
+    })
+
     blocks.push({
       type: BlockType.Image,
-      imageID: _embedded?.['wp:featuredmedia'][0].source_url,
-      caption: _embedded?.['wp:featuredmedia'][0].alt_text
+      imageID: featuredImage.id,
+      caption: featuredImage.title
     })
   }
 
-  $('body')
-    .children()
-    .each((i, el: any) => {
-      // console.log(`${i}: ${el.tagName}`)
-      // console.log($.html(el))
-      switch (el.tagName) {
-        // case 'img':
-        case 'figure':
+  const nodes = $('body').children()
+
+  for (const el of nodes) {
+    let image
+    // console.log(`${i}: ${el.tagName}`)
+    // console.log($.html(el))
+    switch (el.tagName) {
+      // case 'img':
+      case 'figure':
+        image = await createImage({
+          url: $(el).find('img').attr('data-src') ?? '',
+          alt: ''
+        })
+        blocks.push({
+          type: BlockType.Image,
+          imageID: image.id
+        })
+        break
+      case 'iframe':
+        src = $(el).attr('src')
+        if (src?.includes('youtube.com') || src?.includes('youtu.be')) {
           blocks.push({
-            type: BlockType.Image,
-            imageID: $(el).find('img').attr('data-src')
+            type: BlockType.YouTubeVideo,
+            videoID: src
           })
-          break
-        case 'iframe':
-          src = $(el).attr('src')
-          if (src?.includes('youtube.com') || src?.includes('youtu.be')) {
-            blocks.push({
-              type: BlockType.YouTubeVideo,
-              videoID: src
-            })
-          } else if (src?.includes('vimeo.com')) {
-            blocks.push({
-              type: BlockType.VimeoVideo,
-              videoID: src
-            })
-          } else {
-            blocks.push({
-              type: BlockType.Embed,
-              url: src
-            })
-          }
-          break
-        case 'hr':
-          break
-        case 'p':
-        case 'blockquote':
-        case 'h1':
-        case 'h2':
-        case 'h3':
-        case 'h4':
-        case 'h5':
-        case 'h6':
-        default:
-          htmlContent = htmlToSlate($.html(el).toString())[0] as unknown as RichTextNode
-          if (lastBlock?.type === BlockType.RichText) {
-            lastBlock.richText.push(htmlContent)
-          } else {
-            blocks.push({type: BlockType.RichText, richText: [htmlContent]})
-          }
-          break
-      }
-      lastBlock = blocks[blocks.length - 1]
-    })
+        } else if (src?.includes('vimeo.com')) {
+          blocks.push({
+            type: BlockType.VimeoVideo,
+            videoID: src
+          })
+        } else {
+          blocks.push({
+            type: BlockType.Embed,
+            url: src
+          })
+        }
+        break
+      case 'hr':
+        break
+      case 'p':
+      case 'blockquote':
+      case 'h1':
+      case 'h2':
+      case 'h3':
+      case 'h4':
+      case 'h5':
+      case 'h6':
+      default:
+        htmlContent = htmlToSlate($.html(el).toString())[0] as unknown as RichTextNode
+        if (lastBlock?.type === BlockType.RichText) {
+          lastBlock?.richText.push(htmlContent)
+        } else {
+          blocks.push({type: BlockType.RichText, richText: [htmlContent]})
+        }
+        break
+    }
+    lastBlock = blocks[blocks.length - 1]
+  }
 
   return {
     title: title.rendered,
@@ -137,8 +169,8 @@ const migratePosts = async () => {
     if (batch.length === 0) break
 
     for (const post of batch) {
-      const migratedPost = migratePost(post)
-      console.log(JSON.stringify(migratedPost, undefined, '  '))
+      const article = await migratePost(post)
+      console.log(JSON.stringify(article, undefined, '  '))
     }
 
     page += 1
