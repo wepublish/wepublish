5dd097b441af7e48655d19e164d793154ffce0ea
Elias Summermatter <elias.summermatter@seccom.ch>
2024-08-08 02:03:56 +0200

    Fixed guard

--- libs/media/api/src/lib/media-service/transform.guard.ts
+++ libs/media/api/src/lib/media-service/transform.guard.ts
@@ -2,21 +2,21 @@ import sharp from 'sharp'
 import {TransformationsDto} from './transformations.dto'
 import {BadRequestException} from '@nestjs/common'
 
-const M_PIXEL_LIMIT = 10
-const IMAGE_SIZE_LIMIT = 1
+const M_PIXEL_LIMIT = 25
+const IMAGE_SIZE_LIMIT = 10
 
 export class TransformGuard {
   checkDimensions(metadata: sharp.Metadata, transformations: TransformationsDto) {
+    let height = transformations.resize?.height ? transformations.resize?.height : metadata.height
+
     const totalHeight =
-      (metadata.height ?? 0) +
-      (transformations.extend?.top ?? 0) +
-      (transformations.extend?.bottom ?? 0) +
-      (transformations.resize?.height ?? 0)
+      (height ?? 0) + (transformations.extend?.top ?? 0) + (transformations.extend?.bottom ?? 0)
+
+    const width = transformations.resize?.width ? transformations.resize?.width : metadata.width
+
     const totalWidth =
-      (metadata.width ?? 0) +
-      (transformations.extend?.left ?? 0) +
-      (transformations.extend?.right ?? 0) +
-      (transformations.resize?.width ?? 0)
+      (width ?? 0) + (transformations.extend?.left ?? 0) + (transformations.extend?.right ?? 0)
+
     const mPixel = (totalHeight * totalWidth) / 1000 / 1000
     if (mPixel > M_PIXEL_LIMIT) {
       throw new BadRequestException(`Transformation exceeds pixel limit!`)
