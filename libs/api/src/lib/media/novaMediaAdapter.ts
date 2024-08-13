43e8eb14534f7e448ae691d195aba3dca7e27093
Elias Summermatter <elias.summermatter@seccom.ch>
2024-08-13 21:56:32 +0200

    Set-default-image-quality-to-65

--- libs/api/src/lib/media/novaMediaAdapter.ts
+++ libs/api/src/lib/media/novaMediaAdapter.ts
@@ -147,8 +147,7 @@ export class NovaMediaAdapter implements MediaAdapter {
     }
 
     // Max quality is 80 so 1 => 80
-    queryParameters.push(`quality=${transformations?.quality ? transformations.quality * 80 : 65}`)
-
+    queryParameters.push(`quality=${transformations?.quality ? transformations.quality * 65 : 65}`)
 
     return `${this.url}/${image.id}?${queryParameters.join('&')}`
   }
