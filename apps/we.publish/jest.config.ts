37f4a91ddfbc1a52f89ed0f4521f694631118a2c
Karl Merkli <karlmerkli@gmail.com>
2024-08-27 09:51:34 +0200

    refactor: wepublish -> we.publish

--- apps/we.publish/jest.config.ts
+++ apps/we.publish/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'wepublish',
+  displayName: 'we.publish',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', {presets: ['@nx/next/babel']}]
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/wepublish'
+  coverageDirectory: '../../coverage/apps/we.publish'
 }
