79f9ac0860e26387774026cca784723479da600c
Lukas_Skywalker <git@lukasdiener.ch>
2024-09-08 14:18:49 +0200

    WIP

--- apps/bajour/src/components/baslerin-des-tages/baslerin-des-tages.tsx
+++ apps/bajour/src/components/baslerin-des-tages/baslerin-des-tages.tsx
@@ -1,203 +1,31 @@
-import {styled} from '@mui/material'
-import {ApiV1, ArticleList, isRichTextBlock, RichTextBlock} from '@wepublish/website'
+import {ApiV1, isRichTextBlock, RichTextBlock} from '@wepublish/website'
 import {useWebsiteBuilder} from '@wepublish/website'
 import {format} from 'date-fns'
-import {BajourTeaserGrid} from '../website-builder-styled/blocks/teaser-grid-styled'
-import {
-  AuthorsAndDate,
-  ReadMoreButton,
-  TeaserContentStyled,
-  TeaserImgStyled,
-  TeaserLeadStyled,
-  TeaserPreTitleStyled,
-  TeaserTitlesStyled,
-  TitleLine
-} from '../website-builder-overwrites/blocks/teaser-overwrite.style'
-import {ColTeaser} from '../website-builder-overwrites/blocks/col-teaser'
 import {useState} from 'react'
 import {LikeButton} from './like-button'
 import {useLikeStatus} from './use-like-status'
+import {SearchBar} from './search-bar'
+import {
+  ArticleListDesktop,
+  ArticleListMobile,
+  BaslerinDesTagesWrapper,
+  Content,
+  DateDisplay,
+  DateWeekdayContainer,
+  DesktopGrid,
+  Heading,
+  HeadingLarge,
+  Headings,
+  HeadingsInner,
+  ImageWrapperMobile,
+  MobileGrid,
+  Title,
+  WeekdayDisplay
+} from './styles'
 
 interface BaslerinDesTagesProps {
   article: ApiV1.ArticleQuery
 }
-
-const Headings = styled('div')`
-  border-bottom: 1em solid #feddd2;
-  margin-left: 3em;
-  grid-column: 1/3;
-  ${({theme}) => theme.breakpoints.up('lg')} {
-    grid-column-start: 2;
-    padding-bottom: 3em;
-    margin-left: 1em;
-  }
-`
-
-const HeadingsInner = styled('div')`
-  ${({theme}) => theme.breakpoints.up('lg')} {
-    display: flex;
-    justify-content: space-between;
-    max-width: 500px;
-  }
-`
-
-const Heading = styled('h1')`
-  margin: 0;
-  display: block;
-  font-size: 1.6em;
-  line-height: 1.2em;
-  font-weight: 500;
-`
-
-const Content = styled('section')`
-  margin-left: 3em;
-  margin-right: 2em;
-  margin-top: 1em;
-  grid-column: 1/3;
-  ${({theme}) => theme.breakpoints.up('lg')} {
-    margin-left: 1em;
-    grid-column: 2/3;
-  }
-`
-
-const HeadingLarge = styled(Heading)`
-  font-size: 2em;
-  line-height: 1.3em;
-  text-transform: uppercase;
-`
-
-const BaslerinDesTagesWrapper = styled('article')`
-  overflow-x: hidden;
-`
-
-const MobileGrid = styled('div')`
-  display: grid;
-  grid-template-columns: 50% 50%;
-  grid-auto-rows: max-content;
-  gap: 1em;
-  ${({theme}) => theme.breakpoints.up('lg')} {
-    display: none;
-  }
-`
-
-const DesktopGrid = styled('div')`
-  display: none;
-  ${({theme}) => theme.breakpoints.up('lg')} {
-    display: grid;
-    grid-template-columns: 33% 67%;
-    grid-auto-rows: max-content;
-    gap: 1em;
-    padding-left: 3em;
-  }
-`
-
-const DateWeekdayContainer = styled('div')`
-  display: flex;
-  flex-direction: column;
-`
-
-const DateDisplay = styled('span')`
-  display: block;
-  border-bottom: 1px solid black;
-  font-size: 0.8em;
-  font-weight: 500;
-  ${({theme}) => theme.breakpoints.up('lg')} {
-    font-size: 1.3em;
-    text-align: right;
-  }
-`
-
-const WeekdayDisplay = styled(DateDisplay)`
-  border: none;
-`
-
-const Title = styled('span')`
-  font-size: 1.3em;
-  font-weight: 700;
-  margin-bottom: 1em;
-`
-
-const ImageWrapperMobile = styled('div')`
-  display: grid;
-  grid-template-columns: 45vw 55vw;
-  grid-template-rows: 1fr 1fr;
-  gap: 0.8em;
-  margin-top: 1em;
-  margin-left: 1em;
-  overflow-x: hidden;
-  width: 100vw;
-`
-
-const ArticleListDesktop = styled(ArticleList)`
-  display: none;
-  margin-top: 1em;
-  margin-left: 1em;
-  ${({theme}) => theme.breakpoints.up('lg')} {
-    display: block;
-    overflow-x: hidden;
-  }
-  ${BajourTeaserGrid} {
-    padding: 0;
-    grid-template-columns: repeat(6, 10vw);
-    height: 10vw;
-
-    & > * {
-      grid-column: span 1 !important;
-    }
-  }
-  ${ColTeaser} {
-    margin: 0;
-    height: 10vw;
-
-    ${TeaserImgStyled} {
-      height: 10vw;
-      border-radius: 15px;
-    }
-    ${TeaserContentStyled} {
-      background: transparent;
-      grid-column: 2/12;
-      padding: 0;
-
-      ${TeaserTitlesStyled} {
-        color: white;
-        text-shadow: 1px 1px 4px #000;
-        font-size: 0.8rem;
-        text-transform: uppercase;
-      }
-
-      ${TeaserPreTitleStyled}, ${AuthorsAndDate}, ${ReadMoreButton}, ${TeaserLeadStyled}, ${TitleLine} {
-        display: none;
-      }
-    }
-  }
-`
-
-const ArticleListMobile = styled(ArticleListDesktop)`
-  display: block;
-  overflow-x: hidden;
-  grid-template-columns: 1/7;
-  margin: 0;
-  ${BajourTeaserGrid} {
-    grid-template-columns: repeat(6, 20vw);
-    gap: 3vw;
-    height: 15vw;
-
-    ${ColTeaser} {
-      margin: 0;
-      height: 20vw;
-
-      ${TeaserImgStyled} {
-        height: 20vw;
-        border-radius: 15px;
-      }
-
-      ${TeaserContentStyled} {
-        background: transparent;
-        grid-column: 2/23;
-      }
-    }
-  }
-`
 
 function ContentBlock(props: {
   preTitle: string | null | undefined
@@ -225,6 +53,9 @@ export function BaslerinDesTages({article}: BaslerinDesTagesProps) {
     elements: {Image}
   } = useWebsiteBuilder()
 
+  const [currentArticle, setCurrentArticle] = useState(article.article)
+  const [searchQuery, setSearchQuery] = useState<string | undefined>()
+
   const {data: tagData, loading: tagLoading} = ApiV1.useTagQuery({
     variables: {
       tag: 'baslerin-des-tages',
@@ -235,7 +66,8 @@ export function BaslerinDesTages({article}: BaslerinDesTagesProps) {
   const {
     data: articleData,
     loading: articleLoading,
-    error: articleError
+    error: articleError,
+    refetch: refetchArticles
   } = ApiV1.useArticleListQuery({
     skip: !tagData?.tags?.nodes.length,
     variables: {
@@ -245,16 +77,36 @@ export function BaslerinDesTages({article}: BaslerinDesTagesProps) {
         tags: [tagData?.tags?.nodes.at(0)?.id ?? ''],
         publicationDateFrom: {
           comparison: ApiV1.DateFilterComparison.Lower,
-          date: article.article?.publishedAt
+          date: currentArticle?.publishedAt
         }
       }
     }
   })
 
+  /*const {data: phraseData, loading: phraseLoading, refetch: refetchPhrase} = ApiV1.usePhraseQuery({
+    skip: !searchQuery,
+    variables: {
+      query: searchQuery
+    }
+  })*/
+
+  /*const handleSearch = async (query: string) => {
+    setSearchQuery(query)
+    const { data } = await refetchPhrase({ query })
+    if (data?.phrase?.articles?.nodes && Array.isArray(data.phrase.articles.nodes) && data.phrase.articles.nodes.length > 0) {
+      setCurrentArticle(data.phrase.articles.nodes[0]);
+    }
+    await refetchArticles()
+  }*/
+
+  const handleSearch = async (query: string) => {
+    console.log(query)
+  }
+
   const [addLikeMutation] = ApiV1.useAddLikeMutation({
     variables: {
       input: {
-        articleId: article.article?.id ?? ''
+        articleId: currentArticle?.id ?? ''
       }
     }
   })
@@ -262,13 +114,13 @@ export function BaslerinDesTages({article}: BaslerinDesTagesProps) {
   const [removeLikeMutation] = ApiV1.useRemoveLikeMutation({
     variables: {
       input: {
-        articleId: article.article?.id ?? ''
+        articleId: currentArticle?.id ?? ''
       }
     }
   })
 
-  const [likes, setLikes] = useState(article.article?.likes || 0)
-  const {isLiked, updateLikeStatus} = useLikeStatus(article.article?.id ?? '')
+  const [likes, setLikes] = useState(currentArticle?.likes || 0)
+  const {isLiked, updateLikeStatus} = useLikeStatus(currentArticle?.id ?? '')
 
   const handleLike = async () => {
     if (isLiked) {
@@ -282,13 +134,13 @@ export function BaslerinDesTages({article}: BaslerinDesTagesProps) {
     }
   }
 
-  const textBlock = (article.article?.blocks as ApiV1.Block[]).find(isRichTextBlock)
+  const textBlock = (currentArticle?.blocks as ApiV1.Block[]).find(isRichTextBlock)
 
-  const publishedAt = article.article?.publishedAt
+  const publishedAt = currentArticle?.publishedAt
   const publicationDate = publishedAt ? format(new Date(publishedAt), 'd. MMM yyyy') : ''
   const publicationDay = publishedAt ? format(new Date(publishedAt), 'eeee') : ''
 
-  if (!article.article?.title || !article.article.image || !textBlock?.richText) {
+  if (!currentArticle?.title || !currentArticle.image || !textBlock?.richText) {
     return <></>
   }
 
@@ -297,7 +149,7 @@ export function BaslerinDesTages({article}: BaslerinDesTagesProps) {
       <DesktopGrid>
         <Image
           style={{gridRow: '1/4'}}
-          image={article.article.image}
+          image={currentArticle.image}
           square
           css={{borderRadius: '15%'}}
         />
@@ -309,6 +161,7 @@ export function BaslerinDesTages({article}: BaslerinDesTagesProps) {
               <Heading>des Tages</Heading>
             </div>
             <DateWeekdayContainer>
+              <SearchBar onSearch={handleSearch} />
               <DateDisplay>{publicationDate}</DateDisplay>
               <WeekdayDisplay>{publicationDay}</WeekdayDisplay>
             </DateWeekdayContainer>
@@ -323,8 +176,8 @@ export function BaslerinDesTages({article}: BaslerinDesTagesProps) {
 
         <div>
           <ContentBlock
-            preTitle={article.article?.preTitle}
-            title={article.article.title}
+            preTitle={currentArticle?.preTitle}
+            title={currentArticle.title}
             textBlock={textBlock}
           />
 
@@ -345,7 +198,7 @@ export function BaslerinDesTages({article}: BaslerinDesTagesProps) {
         </Headings>
 
         <ImageWrapperMobile>
-          <Image image={article.article.image} square css={{borderRadius: '15%', gridRow: '1/3'}} />
+          <Image image={currentArticle.image} square css={{borderRadius: '15%', gridRow: '1/3'}} />
           <ArticleListMobile data={articleData} loading={articleLoading} error={articleError} />
 
           <DateWeekdayContainer>
@@ -355,8 +208,8 @@ export function BaslerinDesTages({article}: BaslerinDesTagesProps) {
         </ImageWrapperMobile>
 
         <ContentBlock
-          preTitle={article.article?.preTitle}
-          title={article.article.title}
+          preTitle={currentArticle?.preTitle}
+          title={currentArticle.title}
           textBlock={textBlock}
         />
 
