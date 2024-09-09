88ac228a209543b93b5bee95f4d0c479fdc0bd6c
Karl Merkli <karlmerkli@gmail.com>
2024-09-09 23:21:41 +0200

    refactor(membership-website): fix wrong locale

--- libs/membership/website/src/lib/formatters/format-currency.spec.ts
+++ libs/membership/website/src/lib/formatters/format-currency.spec.ts
@@ -4,11 +4,11 @@ import {formatCurrency} from './format-currency'
 
 describe('formatCurrency', () => {
   it('should format CHF without decimals in the swiss german locale', () => {
-    expect(formatCurrency(50000, Currency.Chf, 'ch-DE')).toMatchInlineSnapshot(`"CHF 50'000.-"`)
+    expect(formatCurrency(50000, Currency.Chf, 'de-CH')).toMatchInlineSnapshot(`"CHF 50’000.-"`)
   })
 
   it('should format CHF with decimals in the swiss german locale', () => {
-    expect(formatCurrency(50000.25, Currency.Chf, 'ch-DE')).toMatchInlineSnapshot(`"CHF 50'000.25"`)
+    expect(formatCurrency(50000.25, Currency.Chf, 'de-CH')).toMatchInlineSnapshot(`"CHF 50’000.25"`)
   })
 
   it('should format CHF in another locale', () => {
@@ -16,7 +16,7 @@ describe('formatCurrency', () => {
   })
 
   it('should format a number without decimals in the swiss german locale but as euros', () => {
-    expect(formatCurrency(50000, Currency.Eur, 'ch-DE')).toMatchInlineSnapshot(`"€50'000.00"`)
+    expect(formatCurrency(50000, Currency.Eur, 'de-CH')).toMatchInlineSnapshot(`"EUR 50’000.00"`)
   })
 
   it('should format a number without decimals in the german locale as euros', () => {
--- libs/membership/website/src/lib/formatters/format-currency.ts
+++ libs/membership/website/src/lib/formatters/format-currency.ts
@@ -1,35 +1,12 @@
 import {Currency} from '@wepublish/website/api'
-import {fixCHNumberDelimiter} from './format-number'
 
-const formatChf = (value: number, locale = 'ch-DE') => {
-  const formatter = new Intl.NumberFormat(locale, {style: 'currency', currency: 'CHF'})
+export const formatCurrency = (value: number, currency: Currency, locale = 'de-CH') => {
+  const formatter = new Intl.NumberFormat(locale, {style: 'currency', currency})
   let result = formatter.format(value)
 
-  if (locale === 'ch-DE') {
-    result = fixCHNumberDelimiter(value, formatter)
-  }
-
-  if (result.endsWith('.00')) {
-    return result.replace('.00', '.-')
+  if (currency === Currency.Chf && result.endsWith('.00')) {
+    result = result.replace('.00', '.-')
   }
 
   return result
-}
-
-export const formatCurrency = (value: number, currency: Currency, locale = 'ch-DE') => {
-  let result: string
-
-  if (currency === Currency.Chf) {
-    result = formatChf(value, locale)
-  } else {
-    const formatter = new Intl.NumberFormat(locale, {style: 'currency', currency})
-    result = formatter.format(value)
-  }
-
-  return (
-    result
-      // Replacing those as CI & locally behave different
-      .replace(/EUR[\s]?/, '€')
-      .replace('’', "'")
-  )
 }
