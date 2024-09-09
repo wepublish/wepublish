3dd35db7694d65fd3f6f594ed4842f81a86ba2f4
Karl Merkli <karlmerkli@gmail.com>
2024-09-09 18:32:19 +0200

    test(membership-website): updated snapshots

--- libs/membership/website/src/lib/formatters/format-currency.spec.ts
+++ libs/membership/website/src/lib/formatters/format-currency.spec.ts
@@ -4,11 +4,11 @@ import {formatChf, formatCurrency} from './format-currency'
 
 describe('formatChf', () => {
   it('should format a number without decimals in the swiss german locale', () => {
-    expect(formatChf(50000, 'ch-DE')).toMatchInlineSnapshot(`"CHF 50'000.-"`)
+    expect(formatChf(50000, 'ch-DE')).toMatchInlineSnapshot(`"CHF 50’000.-"`)
   })
 
   it('should format a number with decimals in the swiss german locale', () => {
-    expect(formatChf(50000.25, 'ch-DE')).toMatchInlineSnapshot(`"CHF 50'000.25"`)
+    expect(formatChf(50000.25, 'ch-DE')).toMatchInlineSnapshot(`"CHF 50’000.25"`)
   })
 
   it('should format a number in another locale', () => {
@@ -18,7 +18,7 @@ describe('formatChf', () => {
 
 describe('formatCurrency', () => {
   it('should format a number without decimals in the swiss german locale but as euros', () => {
-    expect(formatCurrency(50000, Currency.Eur, 'ch-DE')).toMatchInlineSnapshot(`"EUR 50'000.00"`)
+    expect(formatCurrency(50000, Currency.Eur, 'ch-DE')).toMatchInlineSnapshot(`"EUR 50’000.00"`)
   })
 
   it('should format a number without decimals in the german locale as euros', () => {
