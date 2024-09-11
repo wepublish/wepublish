6e145af89d9c33dd9db4037dac7179e29f844855
Tomasz Durka <tomasz@durka.pl>
2024-09-11 13:35:54 +0200

    Adjust codegen to include file schemas

--- libs/api/__tests__/api/private/index.ts
+++ libs/api/__tests__/api/private/index.ts
@@ -13,13 +13,18 @@ export type Scalars = {
   Boolean: boolean;
   Int: number;
   Float: number;
+  /** A hexidecimal color value. */
   Color: string;
+  /** A date string, such as 2007-12-03, compliant with the `full-date` format outlined in section 5.6 of the RFC 3339 profile of the ISO 8601 standard for representation of dates and times using the Gregorian calendar. */
   Date: string;
+  /** A date-time string at UTC, such as 2007-12-03T10:15:30Z, compliant with the `date-time` format outlined in section 5.6 of the RFC 3339 profile of the ISO 8601 standard for representation of dates and times using the Gregorian calendar. */
   DateTime: string;
   GraphQLSettingValueType: any;
   RichText: Node[];
   Slug: string;
+  /** The `Upload` scalar type represents a file upload. */
   Upload: File;
+  /** A valid vote value */
   VoteValue: number;
 };
 
