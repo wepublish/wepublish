87993bd417668b29c911650ef7a0801492a0e38d
Elias Summermatter <elias.summermatter@seccom.ch>
2024-09-16 10:11:27 +0200

    Full rename

--- apps/we-publish/jest.config.ts
+++ apps/we-publish/jest.config.ts
@@ -7,5 +7,5 @@ export default {
     '^.+\\.[tj]sx?$': ['babel-jest', {presets: ['@nx/next/babel']}]
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/we.publish'
+  coverageDirectory: '../../coverage/apps/we-publish'
 }
