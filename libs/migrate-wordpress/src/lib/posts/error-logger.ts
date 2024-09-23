6fb0f5afc09c46f692da7b1c90a16a13e7ef64cc
Tomasz Durka <tomasz@durka.pl>
2024-09-23 13:35:08 +0200

    chore(migrate): Log errors to stderr if debug mode

--- libs/migrate-wordpress/src/lib/posts/error-logger.ts
+++ libs/migrate-wordpress/src/lib/posts/error-logger.ts
@@ -4,6 +4,9 @@ import * as path from 'path'
 let logsDirectory: string
 
 export const logError = async (id: string | number, content: string | any) => {
+  if (process.env['DEBUG'] === '1') {
+    console.error(content)
+  }
   if (!logsDirectory) {
     const mainLogsDirectory = 'libs/migrate-wordpress/logs'
     const latest = path.join(mainLogsDirectory, 'latest')
