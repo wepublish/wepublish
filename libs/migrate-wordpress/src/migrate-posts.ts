af5fdddb47990c765be0a74f8b4ff23e3fb8859c
Tomasz Durka <tomasz@durka.pl>
2024-09-25 16:58:57 +0200

    Migrate all posts

--- libs/migrate-wordpress/src/migrate-posts.ts
+++ libs/migrate-wordpress/src/migrate-posts.ts
@@ -1,29 +1,5 @@
 import {migrateAllPosts, migratePostById, migratePostsFromCategory} from './lib/posts'
 
 export async function migrate() {
-  await migratePostById(
-    206024,
-    205523, // paid, with header+paragraph
-    203921, // hr surrounded links
-    100273, // image gallery
-    182908, // article references
-    189813, // formatting
-    203921, // spacing before links
-    167171, // content-box + iframe
-    202384, // instagram aspect-ratio
-    197451,
-    182908,
-    190356,
-    189813,
-    203334,
-    200106,
-    203279,
-    202906,
-    201384,
-    202940,
-    202666
-  )
   await migrateAllPosts()
-  await migratePostsFromCategory(4371, 50)
-  await migratePostsFromCategory(4950, 50)
 }
