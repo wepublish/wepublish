4351c21550c8303db7c6ef13bb6e513c4a871dd5
Karl Merkli <karlmerkli@gmail.com>
2024-09-25 12:59:37 +0200

    refactor(mannschaft): use isContentBoxBlock

--- apps/mannschaft/src/mannschaft-blocks.tsx
+++ apps/mannschaft/src/mannschaft-blocks.tsx
@@ -1,6 +1,8 @@
-import {ApiV1, Blocks, BuilderBlocksProps, hasBlockStyle, isRichTextBlock} from '@wepublish/website'
+import {ApiV1, Blocks, BuilderBlocksProps, isRichTextBlock} from '@wepublish/website'
 import {allPass, complement, findIndex, insert} from 'ramda'
 import {useMemo} from 'react'
+
+import {isContentBoxBlock} from './mannschaft-content-box'
 
 export const createNewAdTeaser = (): ApiV1.TeaserGridBlock => ({
   __typename: 'TeaserGridBlock',
@@ -27,21 +29,27 @@ export const MannschaftBlocks = ({blocks, type}: BuilderBlocksProps) => {
 
     const richtextBlocks = blocks.filter(
       // We ignore content boxes here as we do not want to break them apart
-      allPass([isRichTextBlock, complement(hasBlockStyle('ContentBox'))])
+      allPass([isRichTextBlock, complement(isContentBoxBlock)])
     )
 
     let blocksWithAds = blocks
 
+    // We only want to show an ad if there's more than 3 richtext blocks,
+    // as there's already an ad at the end of the article
     if (richtextBlocks.length > 3) {
       blocksWithAds = insert(
+        // insert an ad **after** the 3rd richtext block
         findIndex(block => block === richtextBlocks[2])(blocks) + 1,
         createNewAdTeaser(),
         blocks
       )
     }
 
+    // We only want to show an ad if there's more than 6 richtext blocks,
+    // as there's already an ad at the end of the article
     if (richtextBlocks.length > 6) {
       blocksWithAds = insert(
+        // insert an ad **after** the 6th richtext block
         findIndex(block => block === richtextBlocks[5])(blocks) + 1,
         createNewAdTeaser(),
         blocks
