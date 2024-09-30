182d2977a4e0fb8937b4c381077b19241cf8dbfd
Elias Summermatter <elias.summermatter@seccom.ch>
2024-09-30 11:37:32 +0200

    Fix

--- apps/mannschaft/middleware.ts
+++ apps/mannschaft/middleware.ts
@@ -4,12 +4,17 @@ import redirectsJson from './redirects.json'
 // Convert redirects JSON to a Map for O(1) lookups
 const redirects = new Map(Object.entries(redirectsJson))
 
+type Redirect = {
+  destination: string
+  permanent?: boolean
+}
+
 export async function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname
-  const redirect = redirects.get(pathname)
+  const redirect: Redirect = redirects.get(pathname)
 
   if (redirect) {
-    const statusCode = redirect?.permanent ? 301 : 307
+    const statusCode = redirect.permanent ? 301 : 307
     const destination = request.nextUrl.clone()
     destination.pathname = redirect.destination
     return NextResponse.redirect(destination, statusCode)
