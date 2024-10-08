bc04b5d7c23c1a83cba1d2baff4366e67cf01724
Tomasz Durka <tomasz@durka.pl>
2024-10-08 13:29:05 +0200

    Add daily briefing component

--- apps/cultur/pages/_app.tsx
+++ apps/cultur/pages/_app.tsx
@@ -22,6 +22,7 @@ import translation from 'zod-i18n-map/locales/de/zod.json'
 
 import {ReactComponent as Logo} from '../src/logo.svg'
 import theme from '../src/theme'
+import {CulturTeaser} from '../src/components/cultur-teaser'
 
 setDefaultOptions({
   locale: de
@@ -99,6 +100,9 @@ function CustomApp({Component, pageProps}: CustomAppProps) {
           Head={Head}
           Script={Script}
           elements={{Link: NextWepublishLink}}
+          blocks={{
+            Teaser: CulturTeaser
+          }}
           date={{format: dateFormatter}}
           meta={{siteTitle}}>
           <ThemeProvider theme={theme}>
