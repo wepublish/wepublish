743b7f6ca23e1a4f2f7b524ba2f6f0a0666e72f6
Tomasz Durka <tomasz@durka.pl>
2024-10-22 11:00:50 +0200

    Add authenticated decorator and guard

--- libs/permissions/api/src/lib/permission.module.ts
+++ libs/permissions/api/src/lib/permission.module.ts
@@ -1,9 +1,10 @@
 import {Module} from '@nestjs/common'
 import {PermissionsGuard} from './permission.guard'
 import {PublicGuard} from './public.guard'
+import {AuthenticatedGuard} from './authenticated.guard'
 
 @Module({
-  providers: [PermissionsGuard, PublicGuard],
-  exports: [PermissionsGuard, PublicGuard]
+  providers: [PermissionsGuard, PublicGuard, AuthenticatedGuard],
+  exports: [PermissionsGuard, PublicGuard, AuthenticatedGuard]
 })
 export class PermissionModule {}
