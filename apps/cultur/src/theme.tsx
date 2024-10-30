1c3bf1b5fc13e988c93494fcb8e66bdc9114c9c6
Tomasz Durka <tomasz@durka.pl>
2024-10-30 16:10:55 +0100

    feat(cultur): Adjust fonts

--- apps/cultur/src/theme.tsx
+++ apps/cultur/src/theme.tsx
@@ -1,10 +1,10 @@
 import {createTheme, Theme, ThemeOptions} from '@mui/material'
 import {theme as WePTheme} from '@wepublish/ui'
-import {Hanken_Grotesk} from 'next/font/google'
+import {Open_Sans} from 'next/font/google'
 import {PartialDeep} from 'type-fest'
 
-const hankenGrotesk = Hanken_Grotesk({
-  weight: ['100', '300', '400', '500', '600', '700'],
+const openSans = Open_Sans({
+  weight: ['300', '400', '500', '600', '700'],
   style: ['italic', 'normal'],
   subsets: ['latin'],
   display: 'swap',
@@ -21,45 +21,45 @@ const theme = createTheme(WePTheme, {
   },
   typography: {
     h1: {
-      fontFamily: [hankenGrotesk.style.fontFamily, 'sans-serif'].join(',')
+      fontFamily: [openSans.style.fontFamily, 'sans-serif'].join(',')
     },
     h2: {
-      fontFamily: [hankenGrotesk.style.fontFamily, 'sans-serif'].join(',')
+      fontFamily: [openSans.style.fontFamily, 'sans-serif'].join(',')
     },
     h3: {
-      fontFamily: [hankenGrotesk.style.fontFamily, 'sans-serif'].join(',')
+      fontFamily: [openSans.style.fontFamily, 'sans-serif'].join(',')
     },
     h4: {
-      fontFamily: [hankenGrotesk.style.fontFamily, 'sans-serif'].join(',')
+      fontFamily: [openSans.style.fontFamily, 'sans-serif'].join(',')
     },
     h5: {
-      fontFamily: [hankenGrotesk.style.fontFamily, 'sans-serif'].join(',')
+      fontFamily: [openSans.style.fontFamily, 'sans-serif'].join(',')
     },
     h6: {
-      fontFamily: [hankenGrotesk.style.fontFamily, 'sans-serif'].join(',')
+      fontFamily: [openSans.style.fontFamily, 'sans-serif'].join(',')
     },
     body1: {
-      fontFamily: [hankenGrotesk.style.fontFamily, 'sans-serif'].join(',')
+      fontFamily: [openSans.style.fontFamily, 'sans-serif'].join(',')
     },
     body2: {
-      fontFamily: [hankenGrotesk.style.fontFamily, 'sans-serif'].join(',')
+      fontFamily: [openSans.style.fontFamily, 'sans-serif'].join(',')
     },
     button: {
-      fontFamily: [hankenGrotesk.style.fontFamily, 'sans-serif'].join(',')
+      fontFamily: [openSans.style.fontFamily, 'sans-serif'].join(',')
     },
     caption: {
-      fontFamily: [hankenGrotesk.style.fontFamily, 'sans-serif'].join(',')
+      fontFamily: [openSans.style.fontFamily, 'sans-serif'].join(',')
     },
     overline: {
-      fontFamily: [hankenGrotesk.style.fontFamily, 'sans-serif'].join(',')
+      fontFamily: [openSans.style.fontFamily, 'sans-serif'].join(',')
     },
     subtitle1: {
-      fontFamily: [hankenGrotesk.style.fontFamily, 'sans-serif'].join(',')
+      fontFamily: [openSans.style.fontFamily, 'sans-serif'].join(',')
     },
     subtitle2: {
-      fontFamily: [hankenGrotesk.style.fontFamily, 'sans-serif'].join(',')
+      fontFamily: [openSans.style.fontFamily, 'sans-serif'].join(',')
     },
-    fontFamily: [hankenGrotesk.style.fontFamily, 'sans-serif'].join(',')
+    fontFamily: [openSans.style.fontFamily, 'sans-serif'].join(',')
   },
   components: {
     MuiInputBase: {
