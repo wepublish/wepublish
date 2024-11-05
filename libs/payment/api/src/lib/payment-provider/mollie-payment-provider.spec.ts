8c770b148ea18ecff0dcc5dfbbcd1bb119597b09
Elias Summermatter <elias.summermatter@seccom.ch>
2024-11-05 13:46:53 +0100

    Fix

--- libs/payment/api/src/lib/payment-provider/mollie-payment-provider.spec.ts
+++ libs/payment/api/src/lib/payment-provider/mollie-payment-provider.spec.ts
@@ -318,7 +318,7 @@ describe('MolliePaymentProvider', () => {
       })
     })
     it('should invalid intent', async () => {
-      // Workaound to make linter happy :-)
+      // Workaround to make linter happy :-)
       const x = 'tt'
       ;(mollieOffSession.mollieClient.customerPayments.create as jest.Mock).mockImplementationOnce(
         () =>
