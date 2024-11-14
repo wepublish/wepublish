71a578119a7ad9ccc1a28bc80f7a3849d09f1f58
Elias Summermatter <elias.summermatter@seccom.ch>
2024-11-14 10:25:30 +0100

    Fix-slack-mailprovider

--- apps/api-example/src/app/slack-mail-provider.ts
+++ apps/api-example/src/app/slack-mail-provider.ts
@@ -25,7 +25,7 @@ export class SlackMailProvider extends BaseMailProvider {
           type: 'section',
           text: {
             type: 'mrkdwn',
-            text: `*From*: ${props.replyToAddress}\n*To*: ${props.recipient}\n*Subject*: ${props.subject}\n\`\`\`${props.message}\`\`\``
+            text: `*From*: ${props.replyToAddress}\n*To*: ${props.recipient}\n*Subject*: ${props.template}\n\`\`\`${props.templateData}\`\`\``
           }
         }
       ]
