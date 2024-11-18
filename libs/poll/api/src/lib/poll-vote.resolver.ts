1b9a5a1f394875d7f60bdbd0512a48f409f5a3cf
Tomasz Durka <tomasz@durka.pl>
2024-11-18 16:24:53 +0100

    lint

--- libs/poll/api/src/lib/poll-vote.resolver.ts
+++ libs/poll/api/src/lib/poll-vote.resolver.ts
@@ -1,4 +1,4 @@
-import {Args, Int, Mutation, Query, Resolver} from '@nestjs/graphql'
+import {Args, Mutation, Query, Resolver} from '@nestjs/graphql'
 import {
   DeletePollVotesResult,
   PaginatedPollVotes,
