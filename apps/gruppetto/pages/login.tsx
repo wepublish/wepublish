77d0d3442f7a0871b5a9f40ce844c29a97289785
Michael Scheurer <michael.scheurer@seccom.ch>
2024-11-19 16:21:29 +0100

    non-relevant change to trigger a test

--- apps/gruppetto/pages/login.tsx
+++ apps/gruppetto/pages/login.tsx
@@ -45,7 +45,6 @@ export default function Login({sessionToken}: LoginProps) {
   return (
     <LoginWrapper>
       <H3 component="h1">Login für Abonnent*innen</H3>
-
       <Typography variant="body1" paragraph>
         (Falls du noch keinen Account hast, <Link href={'/signup'}>klicke hier</Link> und falls du
         ein Abo lösen willst, <Link href={'/abo'}>klicke hier.</Link>)
