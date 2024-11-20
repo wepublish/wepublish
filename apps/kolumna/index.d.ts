2fa63fd9bbbf873e70084ff3c2d0d56c1c979b08
Karl Merkli <karlmerkli@gmail.com>
2024-11-20 11:54:07 +0100

    refactor(kolumna): pk stripe

--- apps/kolumna/index.d.ts
+++ apps/kolumna/index.d.ts
@@ -1,6 +1,12 @@
+import {Theme as MuiTheme} from '@mui/material'
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 declare module '*.svg' {
   const content: any
   export const ReactComponent: any
   export default content
+}
+
+declare module '@emotion/styled' {
+  export interface Theme extends MuiTheme {}
 }
