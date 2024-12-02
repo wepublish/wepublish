98d7dc9bf6ff169cfefedc831d663bbabe11ece7
Tomasz Durka <tomasz@durka.pl>
2024-12-02 02:56:49 +0100

    Add public.guard.spec.ts

--- libs/permissions/api/src/lib/public.guard.ts
+++ libs/permissions/api/src/lib/public.guard.ts
@@ -1,6 +1,5 @@
-import {Injectable, CanActivate, ExecutionContext} from '@nestjs/common'
+import {CanActivate, ExecutionContext, Injectable} from '@nestjs/common'
 import {Reflector} from '@nestjs/core'
-import {GqlExecutionContext} from '@nestjs/graphql'
 import {PUBLIC_METADATA_KEY} from './public.decorator'
 
 @Injectable()
@@ -8,8 +7,6 @@ export class PublicGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   public canActivate(context: ExecutionContext): boolean {
-    const ctx = GqlExecutionContext.create(context)
-
     const isPublic = this.reflector.getAllAndOverride<boolean>(PUBLIC_METADATA_KEY, [
       context.getHandler(),
       context.getClass()
