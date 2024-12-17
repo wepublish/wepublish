68b1fb06859ec15aea9597f04c17fe24c7a3cc3e
Tomasz Durka <tomasz@wepublish.ch>
2024-12-17 09:27:53 +0100

    feat(onlinereports): Add OnlineReports website (#1710)

---
+++ apps/onlinereports/pages/profile/subscription/[id].tsx
@@ -0,0 +1,115 @@
+import {css, styled} from '@mui/material'
+import {getSessionTokenProps, ssrAuthLink, withAuthGuard} from '@wepublish/utils/website'
+import {
+  ApiV1,
+  AuthTokenStorageKey,
+  ContentWrapper,
+  InvoiceListContainer,
+  SubscriptionListContainer,
+  useWebsiteBuilder
+} from '@wepublish/website'
+import {setCookie} from 'cookies-next'
+import {NextPageContext} from 'next'
+import getConfig from 'next/config'
+import {useRouter} from 'next/router'
+
+const SubscriptionsWrapper = styled(ContentWrapper)`
+  display: grid;
+  gap: ${({theme}) => theme.spacing(3)};
+
+  ${({theme}) => css`
+    ${theme.breakpoints.up('md')} {
+      grid-template-columns: 1fr 1fr;
+      gap: ${theme.spacing(10)};
+    }
+  `}
+`
+
+const SubscriptionListWrapper = styled('div')`
+  display: flex;
+  flex-flow: column;
+  gap: ${({theme}) => theme.spacing(2)};
+`
+
+function Subscription() {
+  const {
+    query: {id}
+  } = useRouter()
+  const {
+    elements: {H4}
+  } = useWebsiteBuilder()
+
+  return (
+    <SubscriptionsWrapper>
+      <SubscriptionListWrapper>
+        <H4 component={'h1'}>Abo</H4>
+
+        <SubscriptionListContainer
+          filter={subscriptions => subscriptions.filter(subscription => subscription.id === id)}
+        />
+      </SubscriptionListWrapper>
+
+      <SubscriptionListWrapper>
+        <H4 component={'h1'}>Rechnungen</H4>
+
+        <InvoiceListContainer
+          filter={invoices => invoices.filter(invoice => invoice.subscriptionID === id)}
+        />
+      </SubscriptionListWrapper>
+    </SubscriptionsWrapper>
+  )
+}
+
+const GuardedSubscription = withAuthGuard(Subscription)
+
+export {GuardedSubscription as default}
+;(GuardedSubscription as any).getInitialProps = async (ctx: NextPageContext) => {
+  if (typeof window !== 'undefined') {
+    return {}
+  }
+
+  const {publicRuntimeConfig} = getConfig()
+  const client = ApiV1.getV1ApiClient(publicRuntimeConfig.env.API_URL!, [
+    ssrAuthLink(() => getSessionTokenProps(ctx).sessionToken?.token)
+  ])
+
+  if (ctx.query.jwt) {
+    const data = await client.mutate({
+      mutation: ApiV1.LoginWithJwtDocument,
+      variables: {
+        jwt: ctx.query.jwt
+      }
+    })
+
+    setCookie(
+      AuthTokenStorageKey,
+      JSON.stringify(data.data.createSessionWithJWT as ApiV1.UserSession),
+      {
+        req: ctx.req,
+        res: ctx.res,
+        expires: new Date(data.data.createSessionWithJWT.expiresAt),
+        sameSite: 'strict'
+      }
+    )
+  }
+
+  const sessionProps = getSessionTokenProps(ctx)
+
+  if (sessionProps.sessionToken) {
+    await Promise.all([
+      client.query({
+        query: ApiV1.MeDocument
+      }),
+      client.query({
+        query: ApiV1.SubscriptionsDocument
+      }),
+      client.query({
+        query: ApiV1.InvoicesDocument
+      })
+    ])
+  }
+
+  const props = ApiV1.addClientCacheToV1Props(client, sessionProps)
+
+  return props
+}
