cd6dab1d1b9ed5e47750619981d1941c0fa359a9
Lukas_Skywalker <git@lukasdiener.ch>
2025-01-15 09:16:46 +0100

    Remove old index

--- libs/api/prisma/migrations/20241220132109_add_articles_and_pages_revisions_search_index/migration.sql
+++ libs/api/prisma/migrations/20241220132109_add_articles_and_pages_revisions_search_index/migration.sql
@@ -1,2 +1,5 @@
+DROP INDEX IF EXISTS idx_articles_revisions_tsvector;
+DROP INDEX IF EXISTS idx_pages_revisions_tsvector;
+
 CREATE INDEX idx_articles_revisions_text_search ON "articles.revisions" USING gin((to_tsvector('english', title) || jsonb_to_tsvector('english', jsonb_path_query_array(blocks, 'strict $.**."text"'::jsonpath, '{}'::jsonb, false), '["string"]'::jsonb)));
 CREATE INDEX idx_pages_revisions_text_search ON "pages.revisions" USING gin((to_tsvector('english', title) || jsonb_to_tsvector('english', jsonb_path_query_array(blocks, 'strict $.**."text"'::jsonpath, '{}'::jsonb, false), '["string"]'::jsonb)));
