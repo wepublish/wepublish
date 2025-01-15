d0911ac328deb320a684d0f3958382ea81bb3979
Lukas_Skywalker <git@lukasdiener.ch>
2025-01-15 07:57:16 +0100

    Revert "Impove for german search"
    This reverts commit 579da52809d66ed5c61da606abb825819c703842.

--- libs/api/prisma/migrations/20240925103400_full_text_search_inx/migration.sql
+++ libs/api/prisma/migrations/20240925103400_full_text_search_inx/migration.sql
@@ -1,21 +1,6 @@
 -- Recreate Idexes
 DROP INDEX "articles.revisions_jsonb_to_tsvector_idx";
-DROP INDEX "pages.revisions_jsonb_to_tsvector_idx";
-CREATE INDEX idx_articles_revisions_tsvector
-    ON public."articles.revisions" USING GIN (
-                                              (
-                                                  to_tsvector('german', title) ||
-                                                  to_tsvector('german', lead)
-                                                  )
-        );
-CREATE INDEX idx_pages_revisions_tsvector
-    ON public."pages.revisions" USING GIN (
-                                           (
-                                               to_tsvector('german', title) ||
-                                               jsonb_to_tsvector(
-                                                       'german',
-                                                       jsonb_path_query_array(blocks, 'strict $.**.text'),
-                                                       '["string"]'
-                                               )
-                                               )
+CREATE INDEX idx_article_revisions_tsvector
+    ON "articles.revisions" USING GIN (
+                                              (to_tsvector('english', title) || to_tsvector('english', lead))
         );
