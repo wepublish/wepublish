c691aa31c26b0ee0f0751d1a344378e1581f269f
Elias Summermatter <elias.summermatter@seccom.ch>
2025-01-20 15:46:23 +0100

    Added pixel function

--- libs/count-pixel/api/src/lib/count-pixel-provider/prolitteris/client-gateway.ts
+++ libs/count-pixel/api/src/lib/count-pixel-provider/prolitteris/client-gateway.ts
@@ -1,6 +1,35 @@
+import axios from 'axios'
+
+export type ReturnTrackingPixels = {
+  domain: string
+  pixelUid: string[]
+}
+
 export class GatewayClient {
   constructor(private memberNr: string, private userName: string, private password: string) {}
   getAuthorizationHeader() {
     return Buffer.from(`${this.memberNr}:${this.userName}:${this.password}`).toString('base64')
+  }
+
+  async httpPostRequest(url: string, body: any) {
+    return await axios.post(url, body, {
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+        Authorization: `OWEN ${this.getAuthorizationHeader()}`
+      }
+    })
+  }
+
+  async getTrackingPixels(amount: number): Promise<ReturnTrackingPixels> {
+    try {
+      const response = await this.httpPostRequest('https://owen.prolitteris.ch/rest/api/1/pixel', {
+        amount
+      })
+      console.log('Server responded with:', response.data)
+      return response.data
+    } catch (error) {
+      console.error('Request failed:', error.response?.data || error.message)
+      throw error
+    }
   }
 }
