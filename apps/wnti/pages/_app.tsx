6f1953bc33c0be6961be14c4419c2afb38c81a83
Lukas_Skywalker <git@lukasdiener.ch>
2025-02-12 13:12:24 +0100

    Remove referenced icons

--- apps/wnti/pages/_app.tsx
+++ apps/wnti/pages/_app.tsx
@@ -151,13 +151,12 @@ function CustomApp({Component, pageProps, emotionCache}: CustomAppProps) {
                 <link rel="sitemap" type="application/xml" title="Sitemap" href="/api/sitemap" />
 
                 {/* Favicon definitions, generated with https://realfavicongenerator.net/ */}
+                <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+                <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+                <link rel="shortcut icon" href="/favicon.ico" />
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <meta name="apple-mobile-web-app-title" content="Wnti" />
                 <link rel="manifest" href="/site.webmanifest" />
-                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
-                <meta name="msapplication-TileColor" content="#ffffff" />
-                <meta name="theme-color" content="#ffffff" />
               </Head>
 
               <Spacer>
