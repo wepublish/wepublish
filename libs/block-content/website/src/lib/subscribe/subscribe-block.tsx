f78da99547a68a756b7cfc9b58b3a044313dcd9e
Karl Merkli <karlmerkli@gmail.com>
2025-03-02 14:44:32 +0100

    feat(websites): use mitmachen page for subscribe block

--- libs/block-content/website/src/lib/subscribe/subscribe-block.tsx
+++ libs/block-content/website/src/lib/subscribe/subscribe-block.tsx
@@ -1,10 +1,4 @@
-import {BuilderSubscribeBlockProps} from '@wepublish/website/builder'
 import {Block, SubscribeBlock as SubscribeBlockType} from '@wepublish/website/api'
-import {SubscribeContainer} from '@wepublish/membership/website'
 
 export const isSubscribeBlock = (block: Block): block is SubscribeBlockType =>
   block.__typename === 'SubscribeBlock'
-
-export const SubscribeBlock = ({className}: BuilderSubscribeBlockProps) => (
-  <SubscribeContainer className={className} />
-)
