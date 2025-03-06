5b43eb7851eb731a56d7615caa4fb779b57ba6bf
michael-scheurer <70939054+michael-scheurer@users.noreply.github.com>
2025-03-06 12:06:02 +0100

    fix(wnti): index preview and og meta data

--- apps/wnti/pages/index.tsx
+++ apps/wnti/pages/index.tsx
@@ -1,67 +1,25 @@
-import mailchimp, {campaigns} from '@mailchimp/mailchimp_marketing'
 import {ApiV1, ContentWidthProvider, PageContainer} from '@wepublish/website'
 import {GetStaticProps} from 'next'
 import getConfig from 'next/config'
 
-import {DailyBriefingContext} from '../src/components/daily-briefing/daily-briefing-teaser'
-
-type IndexProps = {
-  campaigns: campaigns.Campaigns[]
-}
-
-export default function Index({campaigns}: IndexProps) {
+export default function Index() {
   return (
-    <DailyBriefingContext.Provider value={campaigns}>
-      <ContentWidthProvider fullWidth={false}>
-        <PageContainer slug={''} />
-      </ContentWidthProvider>
-    </DailyBriefingContext.Provider>
+    <ContentWidthProvider fullWidth={false}>
+      <PageContainer slug={''} />
+    </ContentWidthProvider>
   )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const {publicRuntimeConfig, serverRuntimeConfig} = getConfig()
+  const {publicRuntimeConfig} = getConfig()
 
-  if (
-    !publicRuntimeConfig.env.API_URL ||
-    !serverRuntimeConfig.env.MAILCHIMP_API_KEY ||
-    !serverRuntimeConfig.env.MAILCHIMP_SERVER_PREFIX
-  ) {
+  if (!publicRuntimeConfig.env.API_URL) {
     return {props: {}, revalidate: 1}
   }
 
-  mailchimp.setConfig({
-    apiKey: serverRuntimeConfig.env.MAILCHIMP_API_KEY,
-    server: serverRuntimeConfig.env.MAILCHIMP_SERVER_PREFIX
-  })
-
   const client = ApiV1.getV1ApiClient(publicRuntimeConfig.env.API_URL, [])
-  const [mailchimpResponse] = await Promise.all([
-    mailchimp.campaigns.list({
-      count: 4,
-      sortField: 'send_time',
-      status: 'sent',
-      sortDir: 'DESC',
-      folderId: '90c02813e1',
-      fields: ['campaigns.id', 'campaigns.long_archive_url', 'campaigns.settings.subject_line']
-    }),
-    client.query({
-      query: ApiV1.PageDocument,
-      variables: {
-        slug: ''
-      }
-    }),
-    client.query({
-      query: ApiV1.NavigationListDocument
-    }),
-    client.query({
-      query: ApiV1.PeerProfileDocument
-    })
-  ])
 
-  const {campaigns} = mailchimpResponse as campaigns.CampaignsSuccessResponse
-
-  const props = ApiV1.addClientCacheToV1Props(client, {campaigns})
+  const props = ApiV1.addClientCacheToV1Props(client, {})
 
   return {
     props,
