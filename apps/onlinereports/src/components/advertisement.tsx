27008af19267625d5052f7671575ac13e3693d1a
Tomasz Durka <tomasz@durka.pl>
2025-03-10 07:58:54 +0100

    Add Revive Ads

--- apps/onlinereports/src/components/advertisement.tsx
+++ apps/onlinereports/src/components/advertisement.tsx
@@ -1,40 +1,68 @@
-import {Box, styled} from '@mui/material'
+import {Box, styled, useMediaQuery, useTheme} from '@mui/material'
+import ReviveAd from './revive-ad'
 
 type AdvertisementProps = {
   type: 'whiteboard' | 'half-page' | 'small'
 }
 
 export const Advertisement = ({type}: AdvertisementProps) => {
+  const theme = useTheme()
+  const notLg = useMediaQuery(theme.breakpoints.down('lg'), {
+    ssrMatchMedia: () => ({matches: false})
+  })
+
+  if (type === 'whiteboard' && notLg) {
+    type = 'small'
+  }
+
   switch (type) {
     case 'whiteboard':
-      return <Whiteboard />
+      return (
+        <Whiteboard>
+          <ReviveAd reviveId={'727bec5e09208690b050ccfc6a45d384'} zoneId={'23506'} />
+        </Whiteboard>
+      )
     case 'half-page':
-      return <HalfPage />
+      return (
+        <HalfPage>
+          <ReviveAd reviveId={'727bec5e09208690b050ccfc6a45d384'} zoneId={'23507'} />
+        </HalfPage>
+      )
     case 'small':
-      return <Small />
+      return (
+        <Small>
+          <ReviveAd reviveId={'727bec5e09208690b050ccfc6a45d384'} zoneId={'23506'} />
+        </Small>
+      )
   }
 }
 
 const AdBox = styled(Box)`
   background: repeating-linear-gradient(-45deg, #dde8ee, #dde8ee 15px, #eee 15px, #eee 40px);
-  border: 5px solid #eee;
+  //border: 5px solid #eee;
   margin: 0 auto;
 `
 
 const Whiteboard = styled(AdBox)`
-  height: 250px;
-  width: 994px;
-  @media (max-width: 1050px) {
-    width: 300px;
+  &,
+  img {
+    height: 250px;
+    width: 994px;
   }
 `
 
 const HalfPage = styled(AdBox)`
-  height: 600px;
-  width: 300px;
+  &,
+  img {
+    height: 600px;
+    width: 300px;
+  }
 `
 
 const Small = styled(AdBox)`
-  height: 250px;
-  width: 300px;
+  &,
+  img {
+    height: 250px;
+    width: 300px;
+  }
 `
