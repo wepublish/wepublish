a40b3dcff6b0b9198bd22dae3ac06270a53d723e
Karl Merkli <karlmerkli@gmail.com>
2025-03-19 12:03:31 +0100

    fix(storybook): properly use i18n plugin

--- libs/storybook/src/lib/i18next.ts
+++ libs/storybook/src/lib/i18next.ts
@@ -1,5 +1,3 @@
-import {setDefaultOptions} from 'date-fns'
-import {de} from 'date-fns/locale'
 import i18next from 'i18next'
 import translation from 'zod-i18n-map/locales/de/zod.json'
 import LanguageDetector from 'i18next-browser-languagedetector'
@@ -7,22 +5,18 @@ import resourcesToBackend from 'i18next-resources-to-backend'
 import deTranlations from '@wepublish/website/translations/de.json'
 import {initReactI18next} from 'react-i18next'
 
-setDefaultOptions({
-  locale: de
-})
-
-i18next
+const i18 = i18next
   .use(LanguageDetector)
   .use(initReactI18next)
   .use(resourcesToBackend(() => deTranlations))
   .init({
     partialBundledLanguages: true,
-    lng: 'de',
-    fallbackLng: 'de',
-    supportedLngs: ['de'],
+    lng: 'en',
+    fallbackLng: 'en',
+    supportedLngs: ['en'],
     resources: {
       en: {zod: translation}
     }
   })
 
-export default i18next
+export default i18
