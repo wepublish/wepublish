bfe7d5121a894eea7774034eea2a6f8dca243de3
Michael Scheurer <michael.scheurer@seccom.ch>
2025-03-26 16:14:26 +0100

    stash select poll panel

--- libs/ui/editor/src/lib/blocks/CrowdfundingBlock.tsx
+++ libs/ui/editor/src/lib/blocks/CrowdfundingBlock.tsx
@@ -1,8 +1,36 @@
 import {useTranslation} from 'react-i18next'
-import {CrowdfundingBlockValue} from '.'
 import {BlockProps} from '../atoms/blockList'
 import {useEffect, useState} from 'react'
-import {Drawer, Panel} from 'rsuite'
+import {Drawer, IconButton, Panel as RPanel} from 'rsuite'
+import {PlaceholderInput} from '../atoms'
+import {MdEdit} from 'react-icons/md'
+import styled from '@emotion/styled'
+import {CrowdfundingBlockValue} from '.'
+import {SelectCrowdfundingPanel} from '../panel/selectCrowdfundingPanel'
+
+const IconWrapper = styled.div`
+  position: absolute;
+  z-index: 100;
+  height: 100%;
+  right: 0;
+`
+
+const Poll = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const Panel = styled(RPanel)`
+  display: grid;
+  height: 200px;
+  padding: 0;
+  overflow: hidden;
+  background-color: #f7f9fa;
+`
 
 export const CrowdfundingBlock = ({
   value: {crowdfunding},
@@ -21,11 +49,30 @@ export const CrowdfundingBlock = ({
   return (
     <>
       <Panel bodyFill bordered>
-        <div>Hier kommt was 1</div>
+        <PlaceholderInput onAddClick={() => setIsDialogOpen(true)}>
+          {crowdfunding && (
+            <Poll>
+              <IconWrapper>
+                <IconButton size="lg" icon={<MdEdit />} onClick={() => setIsDialogOpen(true)}>
+                  {t('blocks.poll.edit')}
+                </IconButton>
+              </IconWrapper>
+
+              {crowdfunding.name}
+            </Poll>
+          )}
+        </PlaceholderInput>
       </Panel>
 
       <Drawer size="lg" open={isDialogOpen} onClose={() => setIsDialogOpen(false)}>
-        <div>Hier kommt was 2</div>
+        <SelectCrowdfundingPanel
+          selectedCrowdfunding={crowdfunding}
+          onClose={() => setIsDialogOpen(false)}
+          onSelect={onNewCrowdfunding => {
+            setIsDialogOpen(false)
+            onChange({crowdfunding: onNewCrowdfunding})
+          }}
+        />
       </Drawer>
     </>
   )
