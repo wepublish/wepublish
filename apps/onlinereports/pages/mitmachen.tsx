b446b9b1a2cafd501e00f1e4035d90d26c216c4c
Tomasz Durka <tomasz@durka.pl>
2025-04-01 00:19:07 +0200

    Customize mitmachen

--- apps/onlinereports/pages/mitmachen.tsx
+++ apps/onlinereports/pages/mitmachen.tsx
@@ -1,3 +1,30 @@
 import {SubscribePage} from '@wepublish/utils/website'
+import styled from '@emotion/styled'
+import {SubscribeWrapper} from '@wepublish/membership/website'
 
-export default SubscribePage
+const OnlineReportsSubscribePageWrapper = styled('div')`
+  ${SubscribeWrapper} {
+    font-weight: bold;
+    grid-template-areas:
+      'returning'
+      'userForm'
+      'memberPlans'
+      'monthlyAmount'
+      'paymentPeriodicity'
+      'challenge'
+      'transactionFee'
+      'cta';
+  }
+`
+
+export const MitmachenInner = () => <SubscribePage fields={['firstName']} />
+
+export default function Mitmachen() {
+  return (
+    <OnlineReportsSubscribePageWrapper>
+      <MitmachenInner />
+    </OnlineReportsSubscribePageWrapper>
+  )
+}
+
+Mitmachen.getInitialProps = SubscribePage.getInitialProps
