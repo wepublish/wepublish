8d9fa6af7c847c8d1e5e60a1687bc8f49ef1974a
Michael Scheurer <michael.scheurer@seccom.ch>
2025-04-08 16:44:00 +0200

    closes https://github.com/wepublish/wepublish/pull/1803#discussion_r1986766558

---
+++ apps/flimmer/pages/mitmachen.tsx
@@ -0,0 +1,80 @@
+import {getSessionTokenProps, ssrAuthLink} from '@wepublish/utils/website'
+import {ApiV1, AuthTokenStorageKey, PageContainer} from '@wepublish/website'
+import {setCookie} from 'cookies-next'
+import {NextPageContext} from 'next'
+import getConfig from 'next/config'
+
+export default function Mitmachen() {
+  return <PageContainer slug={'mitmachen'} />
+}
+
+Mitmachen.getInitialProps = async (ctx: NextPageContext) => {
+  const {publicRuntimeConfig} = getConfig()
+  const client = ApiV1.getV1ApiClient(publicRuntimeConfig.env.API_URL!, [
+    ssrAuthLink(() => getSessionTokenProps(ctx).sessionToken?.token)
+  ])
+
+  if (ctx.query.jwt) {
+    const data = await client.mutate({
+      mutation: ApiV1.LoginWithJwtDocument,
+      variables: {
+        jwt: ctx.query.jwt
+      }
+    })
+
+    setCookie(
+      AuthTokenStorageKey,
+      JSON.stringify(data.data.createSessionWithJWT as ApiV1.UserSession),
+      {
+        req: ctx.req,
+        res: ctx.res,
+        expires: new Date(data.data.createSessionWithJWT.expiresAt),
+        sameSite: 'strict'
+      }
+    )
+  }
+
+  const sessionProps = getSessionTokenProps(ctx)
+
+  const dataPromises = [
+    client.query({
+      query: ApiV1.PageDocument,
+      variables: {
+        slug: 'mitmachen'
+      }
+    }),
+    client.query({
+      query: ApiV1.MemberPlanListDocument,
+      variables: {
+        take: 50
+      }
+    }),
+    client.query({
+      query: ApiV1.NavigationListDocument
+    }),
+    client.query({
+      query: ApiV1.PeerProfileDocument
+    })
+  ]
+
+  if (sessionProps.sessionToken) {
+    dataPromises.push(
+      ...[
+        client.query({
+          query: ApiV1.MeDocument
+        }),
+        client.query({
+          query: ApiV1.InvoicesDocument,
+          variables: {
+            take: 50
+          }
+        })
+      ]
+    )
+  }
+
+  await Promise.all(dataPromises)
+  const props = ApiV1.addClientCacheToV1Props(client, sessionProps)
+
+  return props
+}
