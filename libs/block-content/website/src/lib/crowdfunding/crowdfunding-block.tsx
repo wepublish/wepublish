e692dbf7faa59b2961460c5ccc529cc8818e8d4a
Michael Scheurer <michael.scheurer@seccom.ch>
2025-04-08 18:21:52 +0200

    add crowdfunding block to website builder

--- libs/block-content/website/src/lib/crowdfunding/crowdfunding-block.tsx
+++ libs/block-content/website/src/lib/crowdfunding/crowdfunding-block.tsx
@@ -1,5 +1,14 @@
 import {BlockContent, CrowdfundingBlock as CrowdfundingBlockType} from '@wepublish/website/api'
+import {BuilderCrowdfundingBlockProps} from '@wepublish/website/builder'
 
 export const isCrowdfundingBlock = (
   block: Pick<BlockContent, '__typename'>
 ): block is CrowdfundingBlockType => block.__typename === 'CrowdfundingBlock'
+
+export const CrowdfundingBlock = ({crowdfunding}: BuilderCrowdfundingBlockProps) => {
+  return (
+    <>
+      <h1>{crowdfunding?.name}</h1>
+    </>
+  )
+}
