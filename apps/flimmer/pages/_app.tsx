cc217005efe44104c3b5d8846f4bd5d3121c4a7c
Elias Summermatter <elias.summermatter@seccom.ch>
2025-05-14 11:50:36 +0200

    Add piwik

--- apps/flimmer/pages/_app.tsx
+++ apps/flimmer/pages/_app.tsx
@@ -190,6 +190,9 @@ function CustomApp({Component, pageProps, emotionCache}: CustomAppProps) {
                   </LogoLink>
                 </FooterContainer>
               </Spacer>
+              <Script>
+                {`(function(window, document, dataLayerName, id) { window[dataLayerName]=window[dataLayerName]||[],window[dataLayerName].push({start:(new Date).getTime(),event:"stg.start"});var scripts=document.getElementsByTagName('script')[0],tags=document.createElement('script'); var qP=[];dataLayerName!=="dataLayer"&&qP.push("data_layer_name="+dataLayerName);var qPString=qP.length>0?("?"+qP.join("&")):""; tags.async=!0,tags.src="https://flimmer.containers.piwik.pro/"+id+".js"+qPString,scripts.parentNode.insertBefore(tags,scripts); !function(a,n,i){a[n]=a[n]||{};for(var c=0;c<i.length;c++)!function(i){a[n][i]=a[n][i]||{},a[n][i].api=a[n][i].api||function(){var a=[].slice.call(arguments,0);"string"==typeof a[0]&&window[dataLayerName].push({event:n+"."+i+":"+a[0],parameters:[].slice.call(arguments,1)})}}(i[c])}(window,"ppms",["tm","cm"]); })(window, document, 'dataLayer', '12ae360b-6011-4db9-bf9f-f4ade84f8a6e');`}
+              </Script>
             </ThemeProvider>
           </WebsiteBuilderProvider>
         </WebsiteProvider>
