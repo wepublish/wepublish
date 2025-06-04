745a9457973439145f66cd2145b185036c8fee24
Tomasz Durka <tomasz@durka.pl>
2025-06-04 13:02:39 +0200

    Migrated public rateComment

--- libs/comments/api/src/lib/comment.service.ts
+++ libs/comments/api/src/lib/comment.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@nestjs/common'
-import {CommentAuthorType, PrismaClient} from '@prisma/client'
+import {Comment, CommentAuthorType, CommentsRevisions, PrismaClient} from '@prisma/client'
 import {SortOrder} from '@wepublish/utils/api'
 import {
   CalculatedRating,
@@ -11,12 +11,14 @@ import {
 import {RatingSystemService} from './rating-system'
 import {
   AnonymousCommentError,
+  AnonymousCommentRatingDisabledError,
   AnonymousCommentsDisabledError,
   ChallengeMissingCommentError,
   CommentAuthenticationError,
   CommentLengthError,
   countRichtextChars,
   hasPermission,
+  InvalidStarRatingValueError,
   NotAuthorisedError,
   NotFound,
   UserInputError,
@@ -26,6 +28,7 @@ import {CanCreateApprovedComment} from '@wepublish/permissions'
 import {ChallengeService} from '@wepublish/challenge/api'
 import {CommentInput, CommentUpdateInput} from './comment.input'
 import {SettingName, SettingsService} from '@wepublish/settings/api'
+import {CommentWithTags} from './comment.types'
 
 export interface CommentWithRevisions {
   title: string | null
@@ -43,17 +46,6 @@ export class CommentService {
     private readonly settingsService: SettingsService,
     private readonly challengeService: ChallengeService
   ) {}
-
-  mapCommentToPublicComment(comment: any): CommentWithRevisions {
-    const {revisions} = comment
-
-    return {
-      title: revisions.length ? revisions[revisions.length - 1].title : null,
-      lead: revisions.length ? revisions[revisions.length - 1].lead : null,
-      text: revisions.length ? revisions[revisions.length - 1].text : null,
-      ...comment
-    }
-  }
 
   async getPublicChildrenCommentsByParentId(parentId: string, userId: string | null) {
     const comments = await this.prisma.comment.findMany({
@@ -280,6 +272,89 @@ export class CommentService {
       text: updatedComment.revisions.at(-1)?.text,
       title: updatedComment.revisions.at(-1)?.title,
       lead: updatedComment.revisions.at(-1)?.lead
+    }
+  }
+
+  async rateComment(
+    commentId: string,
+    answerId: string,
+    value: number,
+    fingerprint: string | undefined,
+    session: Pick<UserSession, 'user'> | null
+  ) {
+    // check if anonymous rating is allowed
+    const guestRatingSetting = await this.settingsService.settingByName(
+      SettingName.ALLOW_GUEST_COMMENT_RATING
+    )
+
+    if (!session && guestRatingSetting?.value !== true) {
+      throw new AnonymousCommentRatingDisabledError()
+    }
+
+    const answer = await this.prisma.commentRatingSystemAnswer.findUnique({
+      where: {
+        id: answerId
+      }
+    })
+
+    if (!answer) {
+      throw new NotFound('CommentRatingSystemAnswer', answerId)
+    }
+
+    this.validateCommentRatingValue(answer.type as RatingSystemType, value)
+
+    await this.prisma.commentRating.upsert({
+      where: {
+        answerId_commentId_userId: {
+          answerId,
+          commentId,
+          userId: session?.user.id ?? ''
+        }
+      },
+      update: {
+        fingerprint,
+        value
+      },
+      create: {
+        answerId,
+        commentId,
+        userId: session?.user.id,
+        value,
+        fingerprint
+      }
+    })
+
+    const comment = await this.prisma.comment.findFirst({
+      where: {
+        id: commentId
+      },
+      include: {
+        revisions: {orderBy: {createdAt: 'asc'}},
+        overriddenRatings: true
+      }
+    })
+
+    return comment ? this.mapCommentToPublicComment(comment) : null
+  }
+
+  private mapCommentToPublicComment(comment: Comment & {revisions: CommentsRevisions[]}) {
+    const {revisions} = comment
+
+    return {
+      title: revisions.length ? revisions[revisions.length - 1].title : null,
+      lead: revisions.length ? revisions[revisions.length - 1].lead : null,
+      text: revisions.length ? revisions[revisions.length - 1].text : null,
+      ...comment
+    } as CommentWithTags
+  }
+
+  private validateCommentRatingValue(type: RatingSystemType, value: number) {
+    switch (type) {
+      case RatingSystemType.star: {
+        if (value <= 0 || value > 5) {
+          throw new InvalidStarRatingValueError()
+        }
+      }
     }
   }
 }
