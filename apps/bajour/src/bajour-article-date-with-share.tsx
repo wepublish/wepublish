68a9b3a29c49ce87a4a4b6bd4c2d66b5b0e880c5
Lukas_Skywalker <git@lukasdiener.ch>
2025-06-10 10:28:09 +0200

    BAJ-212: Use same share button as OnlineReports

--- apps/bajour/src/bajour-article-date-with-share.tsx
+++ apps/bajour/src/bajour-article-date-with-share.tsx
@@ -1,17 +1,7 @@
 import styled from '@emotion/styled';
-import { css, NoSsr } from '@mui/material';
 import { ArticleDate, ArticleDateWrapper } from '@wepublish/article/website';
-import { Article } from '@wepublish/website/api';
-import {
-  BuilderArticleDateProps,
-  useWebsiteBuilder,
-} from '@wepublish/website/builder';
-import { useState } from 'react';
-import { MdShare } from 'react-icons/md';
-
-const iconButtonStyles = css`
-  padding: 0;
-`;
+import { CommentListItemShare } from '@wepublish/comments/website';
+import { BuilderArticleDateProps } from '@wepublish/website/builder';
 
 const BajourArticleDateWithShareWrapper = styled(ArticleDateWrapper)`
   display: grid;
@@ -24,50 +14,9 @@ const ArticleDateStyled = styled(ArticleDate)`
   margin: 0;
 `;
 
-const ShareButtonWrapper = styled('div')`
-  position: relative;
-  display: inline-block;
-`;
-
-const SuccessMessage = styled('div')`
-  position: absolute;
-  bottom: calc(100% + 8px);
-  right: 0;
-  padding: 8px 12px;
-  border-radius: 4px;
-  text-wrap: nowrap;
-  background-color: ${({ theme }) => theme.palette.success.main};
-  color: ${({ theme }) => theme.palette.success.contrastText};
-`;
-
-export const BajourArticleDateWithShare = ({
-  article,
-}: BuilderArticleDateProps) => {
-  const {
-    elements: { IconButton },
-  } = useWebsiteBuilder();
-
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-
-  const share = async (article: Article) => {
-    // Native sharing dialog if supported, otherwise copy to clipboard
-    if (typeof window !== 'undefined' && 'share' in navigator) {
-      await navigator.share({
-        url: article.url,
-        title: article.latest.title ?? undefined,
-        text: article.latest.lead ?? undefined,
-      });
-    } else {
-      await navigator.clipboard.writeText(article.url);
-      setShowSuccessMessage(true);
-      setTimeout(() => {
-        setShowSuccessMessage(false);
-      }, 3000);
-    }
-  };
-
-  const showDate = !!article?.publishedAt;
-  const showShare = !!article;
+export const BajourArticleDateWithShare = ({ article }: BuilderArticleDateProps) => {
+  const showDate = !!article?.publishedAt
+  const showShare = !!article
 
   if (!showDate && !showShare) {
     return;
@@ -77,25 +26,7 @@ export const BajourArticleDateWithShare = ({
     <BajourArticleDateWithShareWrapper as={'div'}>
       {showDate && <ArticleDateStyled article={article} />}
 
-      {showShare && (
-        <NoSsr>
-          <ShareButtonWrapper>
-            {showSuccessMessage && (
-              <SuccessMessage>
-                Link wurde in die Zwischenablage kopiert.
-              </SuccessMessage>
-            )}
-            <IconButton
-              aria-label="Share"
-              color="primary"
-              onClick={() => share(article)}
-              css={iconButtonStyles}
-            >
-              <MdShare size={24} />
-            </IconButton>
-          </ShareButtonWrapper>
-        </NoSsr>
-      )}
+      <CommentListItemShare url={article.url} title="Share" />
     </BajourArticleDateWithShareWrapper>
   );
 };
