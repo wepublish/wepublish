851c8baa1d0cb6e59fc8a2fff07420b64283c291
Tomasz Durka <tomasz@wepublish.ch>
2025-07-17 09:13:33 +0200

    Fixes for v1->v2 (#1999)
    * Fix availablePaymentMethods
    * Fix deactivationDataloader

--- libs/membership/api/src/lib/subscription/subscription-deactivation.dataloader.ts
+++ libs/membership/api/src/lib/subscription/subscription-deactivation.dataloader.ts
@@ -1,26 +1,24 @@
-import {DataLoaderService} from '@wepublish/utils/api'
+import {createOptionalsArray, DataLoaderService} from '@wepublish/utils/api'
 import {PrismaClient, SubscriptionDeactivation} from '@prisma/client'
 import {Injectable, Scope} from '@nestjs/common'
 
 @Injectable({
   scope: Scope.REQUEST
 })
-export class SubscriptionDeactivationDataloader extends DataLoaderService<
-  SubscriptionDeactivation[]
-> {
+export class SubscriptionDeactivationDataloader extends DataLoaderService<SubscriptionDeactivation> {
   constructor(protected prisma: PrismaClient) {
     super()
   }
 
   protected async loadByKeys(subscriptionIds: string[]) {
-    const deactivations = await this.prisma.subscriptionDeactivation.findMany({
-      where: {
-        subscriptionID: {in: subscriptionIds}
-      }
-    })
-
-    return subscriptionIds.map(subscriptionId =>
-      deactivations.filter(deactivation => subscriptionId === deactivation.subscriptionID)
+    return createOptionalsArray(
+      subscriptionIds,
+      await this.prisma.subscriptionDeactivation.findMany({
+        where: {
+          subscriptionID: {in: subscriptionIds}
+        }
+      }),
+      'subscriptionID'
     )
   }
 }
