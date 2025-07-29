ef71eadfe2c30c41390c304f0f7647928ad25777
Karl Merkli <karlmerkli@gmail.com>
2025-07-30 01:32:47 +0200

    move pre title closer to title

--- apps/hauptstadt/src/components/hauptstadt-title-block.tsx
+++ apps/hauptstadt/src/components/hauptstadt-title-block.tsx
@@ -1,6 +1,10 @@
 import styled from '@emotion/styled'
-import {TitleBlock} from '@wepublish/block-content/website'
+import {TitleBlock, TitleBlockPreTitle} from '@wepublish/block-content/website'
 
 export const HauptstadtTitleBlock = styled(TitleBlock)`
   gap: ${({theme}) => theme.spacing(3.5)};
+
+  ${TitleBlockPreTitle} {
+    margin-bottom: -${({theme}) => theme.spacing(2)};
+  }
 `
