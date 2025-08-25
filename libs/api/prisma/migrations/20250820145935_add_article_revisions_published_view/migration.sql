2bbf9bf79585307265aea6b1f00b69c952a935bd
Elias Summermatter <elias.summermatter@seccom.ch>
2025-08-25 16:25:02 +0200

    Added more views and pages published

--- libs/api/prisma/migrations/20250820145935_add_article_revisions_published_view/migration.sql
+++ libs/api/prisma/migrations/20250820145935_add_article_revisions_published_view/migration.sql
@@ -1,11 +1,98 @@
+/** VIEWS for articles **/
+
 CREATE VIEW "article.revisions.published" AS
     SELECT DISTINCT ON ("articleId")
         id,
-        articleId
+        "articleId"
         FROM
         "articles.revisions"
         WHERE
         "publishedAt" IS NOT NULL AND "publishedAt" <= CURRENT_TIMESTAMP
     ORDER BY
     "articleId", "createdAt" DESC
+;
+
+CREATE VIEW "article.revisions.pending" AS
+    SELECT
+        id,
+        "articleId"
+    FROM
+        "articles.revisions"
+    WHERE
+        "publishedAt" IS NOT NULL AND "publishedAt" > CURRENT_TIMESTAMP
+    ORDER BY
+        "articleId", "createdAt" DESC
+;
+CREATE VIEW "article.revisions.archived" AS
+    SELECT
+        id,
+        "articleId"
+    FROM
+        "articles.revisions"
+    WHERE
+        "archivedAt" IS NOT NULL AND "archivedAt" <= CURRENT_TIMESTAMP
+    ORDER BY
+        "articleId", "createdAt" DESC
+;
+
+CREATE VIEW "article.revisions.draft" AS
+    SELECT
+        id,
+        "articleId"
+    FROM
+        "articles.revisions"
+    WHERE
+        "archivedAt" IS NULL AND "publishedAt" is NULL
+    ORDER BY
+        "articleId", "createdAt" DESC
+;
+
+
+/** VIEWS for Pages **/
+
+CREATE VIEW "pages.revisions.published" AS
+SELECT DISTINCT ON ("pageId")
+    id,
+    "pageId"
+FROM
+    "pages.revisions"
+WHERE
+    "publishedAt" IS NOT NULL AND "publishedAt" <= CURRENT_TIMESTAMP
+ORDER BY
+    "pageId", "createdAt" DESC
+;
+
+CREATE VIEW "pages.revisions.pending" AS
+SELECT
+    id,
+    "pageId"
+FROM
+    "pages.revisions"
+WHERE
+    "publishedAt" IS NOT NULL AND "publishedAt" > CURRENT_TIMESTAMP
+ORDER BY
+    "pageId", "createdAt" DESC
+;
+CREATE VIEW "pages.revisions.archived" AS
+SELECT
+    id,
+    "pageId"
+FROM
+    "pages.revisions"
+WHERE
+    "archivedAt" IS NOT NULL AND "archivedAt" <= CURRENT_TIMESTAMP
+ORDER BY
+    "pageId", "createdAt" DESC
+;
+
+CREATE VIEW "pages.revisions.draft" AS
+SELECT
+    id,
+    "pageId"
+FROM
+    "pages.revisions"
+WHERE
+    "archivedAt" IS NULL AND "publishedAt" is NULL
+ORDER BY
+    "pageId", "createdAt" DESC
 ;
