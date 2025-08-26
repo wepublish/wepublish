4b917d14705af9c14da6125d5e25ec31a89ac1e2
Elias Summermatter <elias.summermatter@seccom.ch>
2025-08-26 11:58:10 +0200

    Add indexes

--- libs/api/prisma/migrations/20250820145935_add_article_and_page_revisions_published_draft_pending_view/migration.sql
+++ libs/api/prisma/migrations/20250820145935_add_article_and_page_revisions_published_draft_pending_view/migration.sql
@@ -9,8 +9,15 @@ CREATE VIEW "articles.revisions.published" AS
         WHERE
         "publishedAt" IS NOT NULL AND "publishedAt" <= CURRENT_TIMESTAMP
     ORDER BY
-    "articleId", "createdAt" DESC
+     "articleId", "createdAt" DESC, id DESC
 ;
+CREATE INDEX article_revisions_published_by_article_created_id_desc
+    ON "articles.revisions" ("articleId", "createdAt" DESC, "id" DESC)
+    INCLUDE ("publishedAt")
+    WHERE "publishedAt" IS NOT NULL;
+CREATE INDEX article_revisions_published_pubat_article_created_id_desc
+    ON "articles.revisions" ("publishedAt", "articleId", "createdAt" DESC, "id" DESC)
+    WHERE "publishedAt" IS NOT NULL;
 
 CREATE VIEW "articles.revisions.pending" AS
     SELECT
@@ -23,6 +30,15 @@ CREATE VIEW "articles.revisions.pending" AS
     ORDER BY
         "articleId", "createdAt" DESC
 ;
+CREATE INDEX article_revisions_pending_pubat
+    ON "articles.revisions" ("publishedAt", "articleId", "createdAt" DESC, "id" DESC)
+    WHERE "publishedAt" IS NOT NULL;
+
+CREATE INDEX article_revisions_pending_by_article_created
+    ON "articles.revisions" ("articleId", "createdAt" DESC, "id" DESC)
+    INCLUDE ("publishedAt")
+    WHERE "publishedAt" IS NOT NULL;
+
 
 CREATE VIEW "articles.revisions.draft" AS
     SELECT
@@ -35,6 +51,15 @@ CREATE VIEW "articles.revisions.draft" AS
     ORDER BY
         "articleId", "createdAt" DESC
 ;
+
+CREATE INDEX article_revisions_draft_by_article_created
+    ON "articles.revisions" ("articleId", "createdAt" DESC, "id" DESC)
+    WHERE "archivedAt" IS NULL AND "publishedAt" IS NULL;
+
+CREATE INDEX article_revisions_draft_created_global
+    ON "articles.revisions" ("createdAt" DESC, "id" DESC)
+    INCLUDE ("articleId")
+    WHERE "archivedAt" IS NULL AND "publishedAt" IS NULL;
 
 
 /** VIEWS for Pages **/
@@ -51,6 +76,15 @@ ORDER BY
     "pageId", "createdAt" DESC
 ;
 
+CREATE INDEX page_revisions_published_by_page_created_id_desc
+    ON "pages.revisions" ("pageId", "createdAt" DESC, "id" DESC)
+    INCLUDE ("publishedAt")
+    WHERE "publishedAt" IS NOT NULL;
+CREATE INDEX page_revisions_published_pubat_page_created_id_desc
+    ON "pages.revisions" ("publishedAt", "pageId", "createdAt" DESC, "id" DESC)
+    WHERE "publishedAt" IS NOT NULL;
+
+
 CREATE VIEW "pages.revisions.pending" AS
 SELECT
     id,
@@ -62,6 +96,14 @@ WHERE
 ORDER BY
     "pageId", "createdAt" DESC
 ;
+CREATE INDEX page_revisions_pending_pubat
+    ON "pages.revisions" ("publishedAt", "pageId", "createdAt" DESC, "id" DESC)
+    WHERE "publishedAt" IS NOT NULL;
+
+CREATE INDEX page_revisions_pending_by_page_created
+    ON "pages.revisions" ("pageId", "createdAt" DESC, "id" DESC)
+    INCLUDE ("publishedAt")
+    WHERE "publishedAt" IS NOT NULL;
 
 CREATE VIEW "pages.revisions.draft" AS
 SELECT
@@ -74,3 +116,12 @@ WHERE
 ORDER BY
     "pageId", "createdAt" DESC
 ;
+
+CREATE INDEX page_revisions_draft_by_article_created
+    ON "pages.revisions" ("pageId", "createdAt" DESC, "id" DESC)
+    WHERE "archivedAt" IS NULL AND "publishedAt" IS NULL;
+
+CREATE INDEX page_revisions_draft_created_global
+    ON "pages.revisions" ("createdAt" DESC, "id" DESC)
+    INCLUDE ("pageId")
+    WHERE "archivedAt" IS NULL AND "publishedAt" IS NULL;
