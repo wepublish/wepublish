f4153aeddda30eeabe1aefe03ad6100b1ad1a1cb
dsheyp <dsheyp@gmail.com>
2025-09-04 15:54:50 +0200

    added fetch-404 to display 404-page from within getInitialProps. bajour: adjusted events-404-detection to the correct data-structure

--- apps/bajour/pages/event/[id].tsx
+++ apps/bajour/pages/event/[id].tsx
@@ -52,7 +52,7 @@ export const getStaticProps: GetStaticProps = async ({params}) => {
     })
   ])
 
-  const is404 = event.errors?.find(({extensions}) => extensions?.status === 404)
+  const is404 = event[0].errors?.find(({extensions}) => extensions?.status === 404)
 
   if (is404) {
     return {
