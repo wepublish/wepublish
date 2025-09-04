f64b57377f448aacb01969a514aa47a6dbe21eeb
Karl Merkli <karlmerkli@gmail.com>
2025-09-04 14:36:34 +0200

    fix auth link

--- libs/utils/website/src/lib/auth-link.tsx
+++ libs/utils/website/src/lib/auth-link.tsx
@@ -1,6 +1,7 @@
 import {ApolloLink, DefaultContext} from '@apollo/client'
 import {AuthTokenStorageKey} from '@wepublish/authentication/website'
 import {getCookie} from 'cookies-next'
+import {Observable} from 'zen-observable-ts'
 
 export const authLink = new ApolloLink((operation, forward) => {
   const {token: cookieToken} = JSON.parse(getCookie(AuthTokenStorageKey)?.toString() ?? '{}')
@@ -38,5 +39,54 @@ export const ssrAuthLink = (
       }
     })
 
-    return forward(operation)
+    const promise = Promise.resolve().then(async () => {
+      const tok = typeof token === 'function' ? await token() : token
+
+      operation.setContext((context: DefaultContext) => {
+        return {
+          ...context,
+          headers: {
+            ...context.headers,
+            authorization: tok ? `Bearer ${tok}` : ''
+          },
+          credentials: 'include'
+        }
+      })
+
+      return operation
+    })
+
+    return chainOperation(promise, forward)
   })
+
+// Some zen-observables helpers, apollo soon switches to rxjs which has those built in
+
+function fromPromise<T>(promise: Promise<T>): Observable<T> {
+  return new Observable<T>(observer => {
+    promise
+      .then(value => {
+        observer.next(value)
+        observer.complete()
+      })
+      .catch(err => observer.error(err))
+  })
+}
+
+// your function
+function chainOperation<T, R>(
+  promise: Promise<T>,
+  forward: (op: T) => Observable<R>
+): Observable<R> {
+  return new Observable<R>(observer => {
+    fromPromise(promise).subscribe({
+      next: op => {
+        forward(op).subscribe({
+          next: val => observer.next(val),
+          error: err => observer.error(err),
+          complete: () => observer.complete()
+        })
+      },
+      error: err => observer.error(err)
+    })
+  })
+}
