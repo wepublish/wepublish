aeb45b9b5941bd1b2491ed1e92881bd04ae4f289
Lukas_Skywalker <git@lukasdiener.ch>
2025-09-05 17:32:10 +0200

    fix(website): Separate comment average and user rating
    Since the average rating and the user rating are currently implemented in a
    single rating component, it is impossible to rate the current average value,
    since the component thinks the vote is already cast.
    This changes the component to display two distinct rating components. One
    displays the current average and is read only. The other appears as soon as the
    user hovers over the ratings and allows them to cast a new rating. They are
    visually distinct so it's easy to see if there already is a user rating or not.

--- libs/ui/src/lib/rating/rating.tsx
+++ libs/ui/src/lib/rating/rating.tsx
@@ -1,70 +1,115 @@
-import { css } from '@emotion/react';
 import styled from '@emotion/styled';
-import { Rating as MuiRating, useTheme } from '@mui/material';
-import { ComponentProps } from 'react';
+import {
+  Rating as MuiRating,
+  RatingProps as MuiRatingProps,
+} from '@mui/material';
+import { SyntheticEvent, useEffect, useState } from 'react';
 import { useHover } from 'react-aria';
 import { MdStar, MdStarBorder } from 'react-icons/md';
 
-export type RatingProps = ComponentProps<typeof MuiRating> & {
-  filledColor?: string;
-  hoverColor?: string;
-  emptyColor?: string;
-  showFilledIcon?: boolean;
+export type RatingProps = Omit<MuiRatingProps, 'value' | 'onChange'> & {
+  averageFilledColor?: string;
+  averageEmptyColor?: string;
+  userFilledColor?: string;
+  userEmptyColor?: string;
+  averageRating: number;
+  userRating: number;
+  onChange?: (event: SyntheticEvent<Element, Event>, newValue: number) => void;
 };
 
-export const RatingWrapper = styled('div')<RatingProps>`
-  ${({ filledColor, theme }) =>
-    filledColor &&
-    css`
-      & .MuiRating-iconFilled {
-        color: ${filledColor};
-      }
-    `}
+export const RatingWrapper = styled('div')<Partial<RatingProps>>`
+  position: relative;
+  display: inline-flex;
 
-  ${({ hoverColor }) =>
-    hoverColor &&
-    css`
-      & .MuiRating-iconHover {
-        color: ${hoverColor};
-      }
-    `}
+  .average-rating {
+    & .MuiRating-iconFilled {
+      color: ${({ theme, averageFilledColor }) =>
+        averageFilledColor || theme.palette.primary.main};
+    }
 
-    ${({ emptyColor }) =>
-    emptyColor &&
-    css`
-      & .MuiRating-iconEmpty {
-        color: ${emptyColor};
-      }
-    `}
+    & .MuiRating-iconEmpty {
+      color: ${({ theme, averageEmptyColor }) =>
+        averageEmptyColor || theme.palette.grey[400]};
+    }
+  }
+
+  .user-rating {
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    & .MuiRating-iconFilled {
+      color: ${({ theme, userFilledColor }) =>
+        userFilledColor || theme.palette.primary.main};
+    }
+
+    & .MuiRating-iconHover {
+      color: ${({ theme, userFilledColor }) =>
+        userFilledColor || theme.palette.primary.main};
+    }
+  }
 `;
 
 export function Rating({
-  filledColor,
-  hoverColor,
-  emptyColor,
-  showFilledIcon = true,
+  averageFilledColor,
+  averageEmptyColor,
+  userFilledColor,
+  userEmptyColor,
+  averageRating,
+  userRating,
+  onChange,
   ...props
 }: RatingProps) {
+  const [internalRating, setInternalRating] = useState(userRating);
   const { hoverProps, isHovered } = useHover({});
-  const theme = useTheme();
+
+  useEffect(() => {
+    setInternalRating(userRating);
+  }, [userRating]);
+
+  const handleRatingChange = (
+    event: SyntheticEvent<Element, Event>,
+    newValue: number | null
+  ) => {
+    if (newValue !== null) {
+      setInternalRating(newValue);
+      onChange?.(event, newValue);
+    }
+  };
+
+  const displayRating = internalRating ?? 0;
 
   const icon = props.icon ?? <MdStar />;
   const emptyIcon = props.emptyIcon ?? <MdStarBorder />;
 
+  const showUserRating = (displayRating > 0 || isHovered) && !props.readOnly;
+
   return (
     <RatingWrapper
       {...(hoverProps as object)}
-      emptyColor={emptyColor || theme.palette.grey[400]}
-      filledColor={filledColor || theme.palette.primary.main}
-      hoverColor={hoverColor || theme.palette.primary.main}
+      userEmptyColor={userEmptyColor}
+      userFilledColor={userFilledColor}
+      averageEmptyColor={averageEmptyColor}
+      averageFilledColor={averageFilledColor}
     >
-      <MuiRating
-        {...props}
-        icon={
-          (isHovered && !props.readOnly) || showFilledIcon ? icon : emptyIcon
-        }
-        emptyIcon={null}
-      />
+      {!showUserRating && (
+        <MuiRating
+          className="average-rating"
+          value={averageRating}
+          icon={emptyIcon}
+          emptyIcon={emptyIcon}
+          readOnly
+        />
+      )}
+      {showUserRating && (
+        <MuiRating
+          className="user-rating"
+          value={displayRating}
+          onChange={handleRatingChange}
+          icon={icon}
+          emptyIcon={emptyIcon}
+        />
+      )}
     </RatingWrapper>
   );
 }
