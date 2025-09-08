5970789bf8d3e9679834d161e645bc47dc57cd4e
Itrulia <karlmerkli@gmail.com>
2025-09-08 14:05:17 +0200

    Update apps/hauptstadt/pages/[slug].tsx

--- apps/hauptstadt/pages/[slug].tsx
+++ apps/hauptstadt/pages/[slug].tsx
@@ -49,6 +49,7 @@ export const getStaticProps: GetStaticProps = async ({params}) => {
     })
   ])
   const is404 = page.errors?.find(({extensions}) => extensions?.status === 404)
+
   if (is404) {
     return {
       notFound: true
