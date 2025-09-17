744e675fa5be399a983d92373ebbbc005e9e28a9
Elias Summermatter <41843343+elias-summermatter@users.noreply.github.com>
2025-09-17 12:14:48 +0200

    fix(payment): Make nightly check for openinvoices soft faild (#2100)

--- libs/membership/api/src/lib/subscription/subscription.service.ts
+++ libs/membership/api/src/lib/subscription/subscription.service.ts
@@ -18,7 +18,7 @@ import {
 import {PaymentProvider, PaymentsService} from '@wepublish/payment/api'
 import {add, endOfDay, startOfDay} from 'date-fns'
 import {Action} from '../subscription-event-dictionary/subscription-event-dictionary.type'
-import {mapPaymentPeriodToMonths} from '@wepublish/utils/api'
+import {logger, mapPaymentPeriodToMonths} from '@wepublish/utils/api'
 
 export type SubscriptionControllerConfig = {
   subscription: Subscription
@@ -424,13 +424,23 @@ export class SubscriptionService {
     const payments = await this.payments.findByInvoiceId(invoice.id)
     for (const payment of payments) {
       if (!payment || !payment.intentID) continue
-      const intentState = await paymentProvider.checkIntentStatus({
-        intentID: payment.intentID,
-        paymentID: payment.id
-      })
-      await paymentProvider.updatePaymentWithIntentState({
-        intentState
-      })
+      try {
+        const intentState = await paymentProvider.checkIntentStatus({
+          intentID: payment.intentID,
+          paymentID: payment.id
+        })
+        await paymentProvider.updatePaymentWithIntentState({
+          intentState
+        })
+      } catch (e) {
+        logger('checkInvoiceState').error(
+          'Checking payment <%s> with intent %s on payment provider %s soft failed with error: %s',
+          payment.id,
+          payment.intentID,
+          paymentProvider.name,
+          e
+        )
+      }
     }
   }
 
