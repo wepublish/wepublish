8df8b3f4bba8c9c08c401a0d9914546d73cef3fe
dsheyp <dsheyp@gmail.com>
2025-09-22 10:32:17 +0200

    style the block directly instead of the wrapper

--- apps/onlinereports/src/onlinereports-teaser-grid-block.tsx
+++ apps/onlinereports/src/onlinereports-teaser-grid-block.tsx
@@ -1,45 +1,10 @@
 import styled from '@emotion/styled'
-import {
-  alignmentForTeaserBlock,
-  isFilledTeaser,
-  isTeaserGridBlock,
-  TeaserGridBlockWrapper
-} from '@wepublish/block-content/website'
-import {BuilderTeaserGridBlockProps, useWebsiteBuilder} from '@wepublish/website/builder'
+import {TeaserGridBlock} from '@wepublish/block-content/website'
 
-export const OnlineReportsTeaserGridBlockWrapper = styled(TeaserGridBlockWrapper)`
+export const OnlineReportsTeaserGridBlock = styled(TeaserGridBlock)`
   row-gap: ${({theme}) => theme.spacing(3)};
 
   ${({theme}) => theme.breakpoints.up('sm')} {
     row-gap: ${({theme}) => theme.spacing(5)};
   }
 `
-
-export const OnlineReportsTeaserGridBlock = ({
-  numColumns,
-  teasers,
-  blockStyle,
-  className
-}: BuilderTeaserGridBlockProps) => {
-  const {
-    blocks: {Teaser}
-  } = useWebsiteBuilder()
-
-  const filledTeasers = teasers.filter(isFilledTeaser)
-
-  return (
-    <OnlineReportsTeaserGridBlockWrapper className={className} numColumns={numColumns}>
-      {filledTeasers.map((teaser, index) => (
-        <Teaser
-          key={index}
-          teaser={teaser}
-          numColumns={numColumns}
-          alignment={alignmentForTeaserBlock(index, numColumns)}
-          blockStyle={blockStyle}
-        />
-      ))}
-    </OnlineReportsTeaserGridBlockWrapper>
-  )
-}
-
-export {alignmentForTeaserBlock, isFilledTeaser, isTeaserGridBlock}
--- apps/onlinereports/src/onlinereports-teaser-grid-flex-blocks.tsx
+++ apps/onlinereports/src/onlinereports-teaser-grid-flex-blocks.tsx
@@ -1,42 +1,10 @@
 import styled from '@emotion/styled'
-import {
-  fixFlexTeasers,
-  isTeaserGridFlexBlock,
-  omitEmptyFlexTeasers,
-  TeaserGridFlexBlockWrapper
-} from '@wepublish/block-content/website'
-import {BuilderTeaserGridFlexBlockProps, useWebsiteBuilder} from '@wepublish/website/builder'
-import {useMemo} from 'react'
+import {TeaserGridFlexBlock} from '@wepublish/block-content/website'
 
-export const OnlineReportsTeaserGridFlexBlockWrapper = styled(TeaserGridFlexBlockWrapper)`
+export const OnlineReportsTeaserGridFlexBlock = styled(TeaserGridFlexBlock)`
   row-gap: ${({theme}) => theme.spacing(3)};
 
   ${({theme}) => theme.breakpoints.up('sm')} {
     row-gap: ${({theme}) => theme.spacing(5)};
   }
 `
-
-export const OnlineReportsTeaserGridFlexBlock = ({
-  flexTeasers,
-  blockStyle,
-  className
-}: BuilderTeaserGridFlexBlockProps) => {
-  const {
-    blocks: {Teaser}
-  } = useWebsiteBuilder()
-
-  const sortedTeasers = useMemo(
-    () => (flexTeasers ? fixFlexTeasers(flexTeasers) : []),
-    [flexTeasers]
-  )
-
-  return (
-    <OnlineReportsTeaserGridFlexBlockWrapper className={className}>
-      {sortedTeasers.map((teaser, index) => (
-        <Teaser key={index} {...teaser} blockStyle={blockStyle} />
-      ))}
-    </OnlineReportsTeaserGridFlexBlockWrapper>
-  )
-}
-
-export {fixFlexTeasers, isTeaserGridFlexBlock, omitEmptyFlexTeasers}
