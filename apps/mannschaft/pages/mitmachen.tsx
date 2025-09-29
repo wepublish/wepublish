55c1d4b1b70bced7ace99ab5215c762fd30e4e0c
Itrulia <karlmerkli@gmail.com>
2025-09-29 15:34:11 +0200

    Discard changes to apps/mannschaft/pages/mitmachen.tsx

--- apps/mannschaft/pages/mitmachen.tsx
+++ apps/mannschaft/pages/mitmachen.tsx
@@ -1,15 +1,11 @@
 import { SubscribePage, useUserCountry } from '@wepublish/utils/website';
 import { Currency } from '@wepublish/website/api';
-import { ComponentProps } from 'react';
 
-type MitmachenProps = ComponentProps<typeof SubscribePage>;
-
-export default function Mitmachen(props: MitmachenProps) {
+export default function Mitmachen() {
   const userCountry = useUserCountry() ?? 'CH';
 
   return (
     <SubscribePage
-      {...props}
       filter={memberPlans =>
         memberPlans.filter(memberPlan => {
           if (userCountry === 'CH') {
