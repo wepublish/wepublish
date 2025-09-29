9ec37fb9766f272b780291a95ff2fd0e4c81eaed
Itrulia <karlmerkli@gmail.com>
2025-09-29 15:33:54 +0200

    Discard changes to apps/bajour/pages/mitmachen.tsx

--- apps/bajour/pages/mitmachen.tsx
+++ apps/bajour/pages/mitmachen.tsx
@@ -1,17 +1,11 @@
 import { SubscribePage } from '@wepublish/utils/website';
-import { ComponentProps } from 'react';
 
 import { Container } from '../src/components/layout/container';
 
-type MitmachenProps = ComponentProps<typeof SubscribePage>;
-
-export default function Mitmachen(props: MitmachenProps) {
+export default function Mitmachen() {
   return (
     <Container>
-      <SubscribePage
-        {...props}
-        fields={['firstName']}
-      />
+      <SubscribePage fields={['firstName']} />
     </Container>
   );
 }
