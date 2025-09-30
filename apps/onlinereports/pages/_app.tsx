bb0d62a992493a21a77dede6cc24c1a9875706d3
dsheyp <dsheyp@gmail.com>
2025-09-30 15:45:46 +0200

    OR-102 share-flyout for comments should look the same as for articles forceNonSystemShare={true}

--- apps/onlinereports/pages/_app.tsx
+++ apps/onlinereports/pages/_app.tsx
@@ -52,6 +52,7 @@ import { OnlineReportsAuthorChip } from '../src/components/author-chip';
 import { OnlineReportsFooter } from '../src/components/footer';
 import { OnlineReportsArticleAuthors } from '../src/components/online-reports-article-authors';
 import { OnlineReportsArticleList } from '../src/components/online-reports-article-list';
+import { OnlineReportsCommentListItem } from '../src/components/online-reports-comment-list-item';
 import { OnlineReportsPage } from '../src/components/page';
 import { OnlineReportsPaymentAmount } from '../src/components/payment-amount';
 import { OnlineReportsQuoteBlock } from '../src/components/quote-block';
@@ -192,6 +193,7 @@ function CustomApp({ Component, pageProps, emotionCache }: CustomAppProps) {
             ArticleList={OnlineReportsArticleList}
             Navbar={OnlineReportsNavbar}
             Article={OnlineReportsArticle}
+            CommentListItem={OnlineReportsCommentListItem}
             Page={OnlineReportsPage}
             RegistrationForm={OnlineReportsRegistrationForm}
             PaymentAmount={OnlineReportsPaymentAmount}
