fb32cbc3f5ef461a1553da65fa962b1e57a8e9aa
dsheyp <dsheyp@gmail.com>
2025-10-13 10:49:50 +0200

    revert

--- libs/membership/website/src/lib/subscribe/subscribe.stories.tsx
+++ libs/membership/website/src/lib/subscribe/subscribe.stories.tsx
@@ -302,14 +302,9 @@ const clickPayTransactionFees: StoryObj['play'] = async ({
 const clickSubscribe: StoryObj['play'] = async ({ canvasElement, step }) => {
   const canvas = within(canvasElement);
 
-  let submitButton = canvas.getByText('Abonnieren', {
+  const submitButton = canvas.getByText('Abonnieren', {
     exact: false,
   });
-  if (!submitButton) {
-    submitButton = canvas.getByText('Spenden', {
-      exact: false,
-    });
-  }
 
   await step('Submit form', async () => {
     await userEvent.click(submitButton);
