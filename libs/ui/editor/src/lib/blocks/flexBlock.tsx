307f793a4a146935c5eeccda3ca7b6ab892df7ab
dsheyp <dsheyp@.....com>
2025-11-06 08:58:52 +0100

    WIP: add textarea to simulate entering all kinds of block-type content elements

--- libs/ui/editor/src/lib/blocks/flexBlock.tsx
+++ libs/ui/editor/src/lib/blocks/flexBlock.tsx
@@ -11,6 +11,19 @@ export const FlexBlock = ({
   return (
     <div>
       <h2>{'Der FlexBlock kommt hier hin.'}</h2>
+      <textarea
+        autoFocus={autofocus}
+        value={JSON.stringify(value, null, 2)}
+        onChange={e => {
+          try {
+            const parsedValue = JSON.parse(e.target.value);
+            onChange(parsedValue);
+          } catch (error) {
+            console.error('Invalid JSON:', error);
+          }
+        }}
+        style={{ width: '100%', height: '600px' }}
+      />
     </div>
   );
 };
