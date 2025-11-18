184c313da58d83478aafa0e65a6e6f1e5272c617
dsheyp <dsheyp@gmail.com>
2025-11-18 20:48:41 +0100

    Revert "rename and relocate pageTypeBasedProps"
    This reverts commit 2ce587654f62ad05eb701f00eaad162f2c32d700.

--- apps/tsri/pages/_app.tsx
+++ apps/tsri/pages/_app.tsx
@@ -121,7 +121,7 @@ function CustomApp({ Component, pageProps, emotionCache }: CustomAppProps) {
   const cache = emotionCache ?? createEmotionCache();
   cache.compat = true;
 
-  const pageTypeBasedContent = getPageTypeBasedContent(pageProps);
+  const pageTypeRelatedContent = getPageTypeBasedContent(pageProps);
 
   return (
     <AppCacheProvider emotionCache={cache}>
@@ -229,7 +229,7 @@ function CustomApp({ Component, pageProps, emotionCache }: CustomAppProps) {
                 slug="main"
                 headerSlug="header"
                 iconSlug="icons"
-                pageTypeBasedProps={pageTypeBasedContent}
+                pageTypeBasedContent={pageTypeBasedContent}
               />
 
               <main>
