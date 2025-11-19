75d9a060c1b9007d53cc36439485e9ab26549840
Itrulia <karlmerkli@gmail.com>
2025-11-19 13:22:50 +0100

    revert(zwoelf): restore login page (#2256)

--- apps/zwoelf/pages/_app.tsx
+++ apps/zwoelf/pages/_app.tsx
@@ -207,7 +207,6 @@ function CustomApp({ Component, pageProps, emotionCache }: CustomAppProps) {
                   href: 'https://shop.zwoelf.ch/produkt-kategorie/abos/',
                   target: '_blank',
                 }}
-                loginBtn={null}
               />
 
               <main>
