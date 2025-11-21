90f97e9c843026e0c2b3c94a0ebc93dc22d49f93
dsheyp <dsheyp@gmail.com>
2025-11-21 12:20:44 +0100

    comment getArticlePathsBasedOnPage in reflekt-app to avoid failing checks

--- apps/reflekt/pages/a/[slug].tsx
+++ apps/reflekt/pages/a/[slug].tsx
@@ -4,7 +4,7 @@ import {
   ArticleWrapper,
 } from '@wepublish/article/website';
 import { CommentListContainer } from '@wepublish/comments/website';
-import { getArticlePathsBasedOnPage } from '@wepublish/utils/website';
+//import { getArticlePathsBasedOnPage } from '@wepublish/utils/website';
 import {
   addClientCacheToV1Props,
   ArticleDocument,
@@ -79,7 +79,7 @@ export default function ArticleBySlugOrId() {
   );
 }
 
-export const getStaticPaths = getArticlePathsBasedOnPage('');
+//export const getStaticPaths = getArticlePathsBasedOnPage('');
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { id, slug } = params || {};
