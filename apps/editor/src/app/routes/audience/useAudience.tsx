29cd7ffc1a9e08c8026aa9b1f7c34a13734e0bd4
dsheyp <dsheyp@gmail.com>
2025-11-24 14:39:39 +0100

    remove unused console.log

--- apps/editor/src/app/routes/audience/useAudience.tsx
+++ apps/editor/src/app/routes/audience/useAudience.tsx
@@ -285,7 +285,6 @@ export function useAudience({
       'predictedSubscriptionRenewalUsersHighProbability',
       'predictedSubscriptionRenewalUsersLowProbability',
     ];
-    console.log('audienceStatsComputed', audienceStatsComputed);
     userKeys.forEach(key => {
       const users = audienceStatsComputed
         .flatMap(stat => stat[key] as DailySubscriptionStatsUser[])
