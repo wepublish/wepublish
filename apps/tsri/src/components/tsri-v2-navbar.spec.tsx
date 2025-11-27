6da81527ddb0ef2d669f2794dfa047c5ff66452a
dsheyp <dsheyp@gmail.com>
2025-11-27 13:56:52 +0100

    move everything test/storybook related about tsri to the respective app. add mock google font. fix tsri tests.

--- apps/tsri/src/components/tsri-v2-navbar.spec.tsx
+++ apps/tsri/src/components/tsri-v2-navbar.spec.tsx
@@ -1,14 +1,30 @@
+import { MockedProvider } from '@apollo/client/testing';
 import { composeStories } from '@storybook/react';
 import { render } from '@testing-library/react';
 
+import { WithWebsiteBuilderProvider } from '../with-website-builder-provider';
 import * as stories from './tsri-v2-navbar.stories';
+
+jest.mock('next/font/google', () => ({
+  Hanken_Grotesk: () => ({
+    style: {
+      fontFamily: 'mocked',
+    },
+  }),
+}));
 
 const storiesCmp = composeStories(stories);
 
 describe('Navbar', () => {
   Object.entries(storiesCmp).forEach(([story, Component]) => {
     it(`should render ${story}`, () => {
-      render(<Component />);
+      render(
+        <MockedProvider {...Component.parameters?.apolloClient}>
+          <WithWebsiteBuilderProvider>
+            <Component />
+          </WithWebsiteBuilderProvider>
+        </MockedProvider>
+      );
     });
   });
 });
--- libs/storybook/src/lib/tsridecorators.tsx
+++
@@ -1,7 +0,0 @@
-import { Preview } from '@storybook/react';
-
-import { WithWebsiteProviderDecorator } from './decorators/tsri-website.decorator';
-
-export const tsridecorators = [
-  WithWebsiteProviderDecorator,
-] as Preview['decorators'];
