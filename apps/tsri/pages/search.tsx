0c26c2672ab8f9c8fc4e3a3540b6b483a23354ed
dsheyp <dsheyp@gmail.com>
2026-01-15 22:25:17 +0100

    adjust to new tsri design

--- apps/tsri/pages/search.tsx
+++ apps/tsri/pages/search.tsx
@@ -1,4 +1,12 @@
-export {
-  SearchPage as default,
-  SearchPageGetServerSideProps as getServerSideProps,
+import styled from '@emotion/styled';
+import {
+  SearchPage,
+  SearchPageGetServerSideProps,
 } from '@wepublish/utils/website';
+
+export default styled(SearchPage)`
+  & > h1.MuiTypography-root {
+    display: none;
+  }
+`;
+export { SearchPageGetServerSideProps as getServerSideProps };
