2845a961d73cf466b6fe3af3053afd86ce4b893d
dsheyp <dsheyp@gmail.com>
2026-01-20 19:40:38 +0100

    change naming of import

--- apps/tsri/pages/_app.tsx
+++ apps/tsri/pages/_app.tsx
@@ -44,7 +44,7 @@ import { TsriArticleMeta } from '../src/components/tsri-article-meta';
 import { TsriBanner } from '../src/components/tsri-banner';
 import { TsriBreakBlock } from '../src/components/tsri-break-block';
 import { TsriContextBox } from '../src/components/tsri-context-box';
-import { TSRIFooter } from '../src/components/tsri-footer';
+import { TsriFooter } from '../src/components/tsri-footer';
 import { TsriNavbar } from '../src/components/tsri-navbar';
 import { TsriQuoteBlock } from '../src/components/tsri-quote-block';
 import { TsriRichText } from '../src/components/tsri-richtext';
@@ -121,7 +121,7 @@ function CustomApp({ Component, pageProps, emotionCache }: CustomAppProps) {
       <WebsiteProvider>
         <WebsiteBuilderProvider
           Head={Head}
-          Footer={TSRIFooter}
+          Footer={TsriFooter}
           Script={Script}
           Navbar={TsriNavbar}
           ArticleDate={TsriArticleDate}
