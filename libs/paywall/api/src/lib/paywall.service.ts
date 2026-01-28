b8eb851d87144a4d61bc3e37f33165a5ee54a093
Itrulia <karlmerkli@gmail.com>
2026-01-28 10:42:03 +0100

    fix(api): make paywall bypass tokens unique with paywallId (#2360)

--- libs/paywall/api/src/lib/paywall.service.ts
+++ libs/paywall/api/src/lib/paywall.service.ts
@@ -63,7 +63,7 @@ export class PaywallService {
   }
 
   @PrimeDataLoader(PaywallDataloaderService)
-  public async updatePaywall({
+  public updatePaywall({
     id,
     memberPlanIds,
     bypassTokens,
@@ -73,7 +73,7 @@ export class PaywallService {
       throw new BadRequestException('hideContentAfter can not be lower than 0');
     }
 
-    const result = await this.prisma.paywall.update({
+    return this.prisma.paywall.update({
       where: {
         id,
       },
@@ -104,6 +104,11 @@ export class PaywallService {
         bypasses:
           bypassTokens ?
             {
+              deleteMany: {
+                token: {
+                  notIn: bypassTokens,
+                },
+              },
               createMany: {
                 skipDuplicates: true,
                 data: bypassTokens.map(token => ({
@@ -114,26 +119,6 @@ export class PaywallService {
           : undefined,
       },
     });
-
-    await this.prisma.paywall.update({
-      where: {
-        id,
-      },
-      data: {
-        bypasses:
-          bypassTokens ?
-            {
-              deleteMany: {
-                token: {
-                  notIn: bypassTokens,
-                },
-              },
-            }
-          : undefined,
-      },
-    });
-
-    return result;
   }
 
   public deletePaywall(id: string) {
