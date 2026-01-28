c4109c350a686ff85806be5fc293ecc09f0e9fb9
Itrulia <karlmerkli@gmail.com>
2026-01-28 10:46:15 +0100

    Discard changes to libs/comments/website/src/lib/comment-ratings/comment-ratings.tsx

--- libs/comments/website/src/lib/comment-ratings/comment-ratings.tsx
+++ libs/comments/website/src/lib/comment-ratings/comment-ratings.tsx
@@ -68,10 +68,8 @@ export const CommentRatings = ({
   const callAction = useAsyncAction(setLoading, setError);
 
   const canVote = hasUser || canRateAnonymously;
-
   const allUserRatings =
-    hasUser ? userRatings
-    : ratingSystem.answers ?
+    hasUser ? userRatings : (
       ratingSystem.answers.flatMap(answer => {
         const value = getAnonymousRate(commentId, answer.id);
 
@@ -84,7 +82,7 @@ export const CommentRatings = ({
           value,
         };
       })
-    : [];
+    );
 
   const rateComment = useCallback(
     async ({ answerId, commentId, value }: RateCommentMutationVariables) =>
@@ -100,36 +98,35 @@ export const CommentRatings = ({
     [callAction, rate]
   );
 
-  const showRatingNames = ratingSystem.answers?.length > 1;
+  const showRatingNames = ratingSystem.answers.length > 1;
 
   return (
     <CommentRatingsWrapper>
       <NoSsr>
-        {ratingSystem.answers &&
-          ratingSystem.answers.map(answer => (
-            <Fragment key={answer.id}>
-              {answer.type === RatingSystemType.Star && (
-                <StarRating
-                  name={showRatingNames ? answer.answer : null}
-                  hasRated={hasUserRated(answer.id, allUserRatings)}
-                  rating={getCommentRating(
-                    answer.id,
-                    allUserRatings,
-                    calculatedRatings,
-                    overriddenRatings
-                  )}
-                  onChange={rating =>
-                    rateComment({
-                      answerId: answer.id,
-                      commentId,
-                      value: rating,
-                    })
-                  }
-                  readOnly={!canVote}
-                />
-              )}
-            </Fragment>
-          ))}
+        {ratingSystem.answers.map(answer => (
+          <Fragment key={answer.id}>
+            {answer.type === RatingSystemType.Star && (
+              <StarRating
+                name={showRatingNames ? answer.answer : null}
+                hasRated={hasUserRated(answer.id, allUserRatings)}
+                rating={getCommentRating(
+                  answer.id,
+                  allUserRatings,
+                  calculatedRatings,
+                  overriddenRatings
+                )}
+                onChange={rating =>
+                  rateComment({
+                    answerId: answer.id,
+                    commentId,
+                    value: rating,
+                  })
+                }
+                readOnly={!canVote}
+              />
+            )}
+          </Fragment>
+        ))}
       </NoSsr>
 
       {error && <Alert severity="error">{error.message}</Alert>}
