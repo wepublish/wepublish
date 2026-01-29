0571b3070a46b1087ea02e8350814cd75a678c1f
dsheyp <dsheyp@gmail.com>
2026-01-29 10:16:58 +0100

    add missing imports (#2363)
    Co-authored-by: dsheyp <dsheyp@.....com>

--- apps/tsri/pages/search.tsx
+++ apps/tsri/pages/search.tsx
@@ -1,4 +1,8 @@
-import { SearchPage } from '@wepublish/utils/website';
+import styled from '@emotion/styled';
+import {
+  SearchPage,
+  SearchPageGetServerSideProps,
+} from '@wepublish/utils/website';
 
 const TsriSearchPage = styled(SearchPage)`
   padding-top: calc(var(--navbar-height) / 2);
