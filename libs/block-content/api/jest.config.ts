fe01f841a28c7a947e9205f49576894645f10753
dsheyp <dsheyp@.....com>
2026-02-04 15:21:38 +0100

    mov slot-teaser-loader.spec to article-api project. revert unsused configs & modifications.

--- libs/block-content/api/jest.config.ts
+++ libs/block-content/api/jest.config.ts
@@ -4,30 +4,8 @@ export default {
   preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': [
-      'ts-jest',
-      {
-        tsconfig: '<rootDir>/tsconfig.spec.json',
-      },
-    ],
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
-  moduleNameMapper: {
-    '^@faker-js/faker$': '<rootDir>/../../../__mocks__/faker.ts',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../../coverage/libs/block-content/api',
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/**/*.model.ts',
-    '!src/**/*.resolver.ts',
-    '!src/**/index.ts',
-  ],
-  coverageThreshold: {
-    global: {
-      branches: 50,
-      functions: 50,
-      lines: 50,
-      statements: 50,
-    },
-  },
 };
