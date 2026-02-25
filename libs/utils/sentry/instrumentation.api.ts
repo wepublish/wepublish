5db11e588144177e8a8e85588e8239b4b0247a19
Elias Summermatter <elias.summermatter@seccom.ch>
2026-02-25 22:37:21 +0100

    Add

--- libs/utils/sentry/instrumentation.api.ts
+++ libs/utils/sentry/instrumentation.api.ts
@@ -8,13 +8,18 @@
  * // ... rest of imports
  */
 import * as Sentry from '@sentry/nestjs';
+import { graphqlIntegration, prismaIntegration } from '@sentry/nestjs';
 import { nodeProfilingIntegration } from '@sentry/profiling-node';
 
 import { getBaseConfig, setCommonTags } from './config';
 
 Sentry.init({
   ...getBaseConfig(),
-  integrations: [nodeProfilingIntegration()],
+  integrations: [
+    nodeProfilingIntegration(),
+    prismaIntegration(),
+    graphqlIntegration(),
+  ],
   profilesSampleRate: 1.0,
 });
 
