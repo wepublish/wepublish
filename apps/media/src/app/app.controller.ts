94d6504b69411dc28826f30b4bb824715923c921
Elias Summermatter <elias.summermatter@seccom.ch>
2026-03-09 17:15:39 +0100

    Fix

--- apps/media/src/app/app.controller.ts
+++ apps/media/src/app/app.controller.ts
@@ -65,7 +65,7 @@ export class AppController {
     @UploadedFile(
       new ParseFilePipe({
         fileIsRequired: true,
-        validators: [],
+        validators: [new SupportedImagesValidator()],
       })
     )
     uploadedFile: Express.Multer.File
@@ -122,13 +122,6 @@ export class AppController {
         httpCode,
         `${process.env['S3_PUBLIC_HOST']}/${uriFromCache.uri}`
       );
-      return;
-    }
-
-    const fallbackUrl = process.env['MEDIA_FALLBACK_URL'];
-    if (fallbackUrl && !(await this.media.hasImage(imageId))) {
-      res.setHeader('Cache-Control', `public, max-age=600`);
-      res.redirect(HTTP_CODE_FOUND, `${fallbackUrl}${(req as any).url}`);
       return;
     }
 
