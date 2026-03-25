8953634b91e58e5757847239aebf87b64c7035ef
LukasSkywalker <git@lukasdiener.ch>
2026-03-25 14:40:46 +0100

    Remove setup-database

--- libs/system-info/api/jest.config.ts
+++ libs/system-info/api/jest.config.ts
@@ -14,5 +14,4 @@ export default {
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../../coverage/libs/system-info/api',
-  globalSetup: '<rootDir>/setup-database.js',
 };
