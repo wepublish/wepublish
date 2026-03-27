ba0f437bcb86bd77a450ba837b933ac390c7ac28
dsheyp <dsheyp@gmail.com>
2026-03-27 08:36:33 +0100

    limit styles for bildwurf-ad to the definition of sizing/position but otherwise avoid affecting the inner styling-logic of the ads

--- apps/tsri/src/components/tsri-bildwurf-ad-block.tsx
+++ apps/tsri/src/components/tsri-bildwurf-ad-block.tsx
@@ -2,10 +2,6 @@ import styled from '@emotion/styled';
 import { BildwurfAdBlock as BildwurfAdBlockDefault } from '@wepublish/block-content/website';
 
 export const TsriBildwurfAdBlock = styled(BildwurfAdBlockDefault)`
-  & #bildwurf-ad-in-content {
-    overflow: hidden;
-  }
-
   ${({ theme }) => theme.breakpoints.up('md')} {
     margin: 0 auto;
     width: 83%;
