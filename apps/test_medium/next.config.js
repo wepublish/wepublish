6727677cd87c65388f527756c8de184c6563bd2f
wepublish-service <it@wepublish.ch>
2026-03-27 13:48:26 +0100

    feat: add website app for "test_medium"

---
+++ apps/test_medium/next.config.js
@@ -0,0 +1,29 @@
+//@ts-check
+
+const { composePlugins, withNx } = require('@nx/next');
+const wepNextConfig = require('../../libs/utils/website/src/lib/next.config');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled:
+    process.env.NODE_ENV === 'production' && !!process.env.ANALYZE_BUNDLE,
+  openAnalyzer: false,
+});
+
+/**
+ * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
+ **/
+const nextConfig = {
+  ...wepNextConfig,
+  publicRuntimeConfig: {
+    env: {
+      API_URL: process.env.API_URL || '',
+    },
+  },
+};
+
+const plugins = [
+  // Add more Next.js plugins to this list if needed.
+  withNx,
+  withBundleAnalyzer,
+];
+
+module.exports = composePlugins(...plugins)(nextConfig);
