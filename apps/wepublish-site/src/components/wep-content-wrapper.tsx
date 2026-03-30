eca47c7c5fcefe91f07bc03caa4f7196ed87b27e
dsheyp <dsheyp@gmail.com>
2026-03-30 08:42:18 +0200

    WIP

--- apps/wepublish-site/src/components/wep-content-wrapper.tsx
+++ apps/wepublish-site/src/components/wep-content-wrapper.tsx
@@ -3,7 +3,8 @@ import { ContentWrapperStyled } from '@wepublish/content/website';
 
 export const WepContentWrapper = styled(ContentWrapperStyled)`
   row-gap: ${({ theme }) => theme.spacing(5)};
+
   ${({ theme }) => theme.breakpoints.up('md')} {
-    column-gap: ${({ theme }) => theme.spacing(15)};
+    row-gap: ${({ theme }) => theme.spacing(15)};
   }
 `;
