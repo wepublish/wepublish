0382986272ac677e0c564fbaf6656edb0ee5e16f
Lukas Diener <LukasSkywalker@users.noreply.github.com>
2026-04-01 18:07:08 +0200

    Add Sentry to all components (#2542)
    Co-authored-by: Elias Summermatter <elias.summermatter@seccom.ch>

--- apps/bajour/instrumentation.ts
+++ apps/bajour/instrumentation.ts
@@ -1,13 +1,1 @@
-import * as Sentry from '@sentry/nextjs';
-
-export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await import('@wepublish/utils/website/sentry/server');
-  }
-
-  if (process.env.NEXT_RUNTIME === 'edge') {
-    await import('@wepublish/utils/website/sentry/edge');
-  }
-}
-
-export const onRequestError = Sentry.captureRequestError;
+export { onRequestError, register } from '@wepublish/utils/sentry/nextjs';
