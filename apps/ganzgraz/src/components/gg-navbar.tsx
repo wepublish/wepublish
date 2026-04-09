602bd372dc6f5622874b9ae94ddec7d751f9f733
dsheyp <dsheyp@gmail.com>
2026-04-09 10:56:45 +0200

    ganzgraz: set basic frontend styles

---
+++ apps/ganzgraz/src/components/gg-navbar.tsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+import { Navbar, NavbarActions } from '@wepublish/navigation/website';
+
+export const GGNavbar = styled(Navbar)`
+  ${NavbarActions} {
+    display: none;
+  }
+`;
