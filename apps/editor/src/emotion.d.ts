4184f186a794377519e9443a2fc890e1fc2faafd
dsheyp <dsheyp@gmail.com>
2026-04-09 17:31:24 +0200

    add linter exception and allow empty interface

--- apps/editor/src/emotion.d.ts
+++ apps/editor/src/emotion.d.ts
@@ -2,6 +2,6 @@
 import type { Theme as MuiTheme } from '@mui/material/styles';
 
 declare module '@emotion/react' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   interface Theme extends MuiTheme {}
 }
