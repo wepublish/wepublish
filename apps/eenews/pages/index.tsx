5ccea109b1372f0010768b38f2d93d89702d1952
wepublish-service <it@wepublish.ch>
2026-04-21 11:02:37 +0200

    feat: add website app for "eenews"

---
+++ apps/eenews/pages/index.tsx
@@ -0,0 +1,50 @@
+import { ContentWidthProvider } from '@wepublish/content/website';
+import { PageContainer } from '@wepublish/page/website';
+import {
+  addClientCacheToV1Props,
+  getV1ApiClient,
+  NavigationListDocument,
+  PageDocument,
+  PeerProfileDocument,
+} from '@wepublish/website/api';
+import { GetStaticProps } from 'next';
+import getConfig from 'next/config';
+
+export default function Index() {
+  return (
+    <ContentWidthProvider fullWidth>
+      <PageContainer slug={''} />
+    </ContentWidthProvider>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { publicRuntimeConfig } = getConfig();
+
+  if (!publicRuntimeConfig.env.API_URL) {
+    return { props: {}, revalidate: 1 };
+  }
+
+  const client = getV1ApiClient(publicRuntimeConfig.env.API_URL, []);
+  await Promise.all([
+    client.query({
+      query: PageDocument,
+      variables: {
+        slug: '',
+      },
+    }),
+    client.query({
+      query: NavigationListDocument,
+    }),
+    client.query({
+      query: PeerProfileDocument,
+    }),
+  ]);
+
+  const props = addClientCacheToV1Props(client, {});
+
+  return {
+    props,
+    revalidate: 60, // every 60 seconds
+  };
+};
