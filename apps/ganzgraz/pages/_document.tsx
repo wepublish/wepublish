daf58bfc524471aae663e9bc1c807127421622f8
Karl Merkli <karlmerkli@gmail.com>
2026-05-05 23:25:31 +0200

    feat(ganzgraz): consentmanager

--- apps/ganzgraz/pages/_document.tsx
+++ apps/ganzgraz/pages/_document.tsx
@@ -4,12 +4,23 @@ import {
   DocumentHeadTagsProps,
 } from '@mui/material-nextjs/v15-pagesRouter';
 import { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function MuiDocument(props: DocumentHeadTagsProps) {
   return (
     <Html lang="de">
       <Head>
         <DocumentHeadTags {...props} />
+
+        <Script
+          type="text/javascript"
+          data-cmp-ab="1"
+          src="https://cdn.consentmanager.net/delivery/autoblocking/ee45badb726a6.js"
+          data-cmp-host="b.delivery.consentmanager.net"
+          data-cmp-cdn="cdn.consentmanager.net"
+          data-cmp-codesrc="0"
+          strategy="beforeInteractive"
+        ></Script>
       </Head>
 
       <body>
