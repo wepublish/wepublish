4a14b931f0f2517b40c07fed15bba9589c1a5d33
dsheyp <dsheyp@gmail.com>
2026-05-13 08:39:33 +0200

    WIP

--- apps/ganzgraz/pages/index.tsx
+++ apps/ganzgraz/pages/index.tsx
@@ -1,4 +1,5 @@
 import { PageContainer } from '@wepublish/page/website';
+import { getApiUrl } from '@wepublish/utils/website';
 import {
   addClientCacheToV1Props,
   getV1ApiClient,
@@ -6,11 +7,18 @@ import {
   PageDocument,
   PeerProfileDocument,
 } from '@wepublish/website/api';
+import { LinkContext } from '@wepublish/website/builder';
 import { GetStaticProps } from 'next';
 import getConfig from 'next/config';
 
 export default function Index() {
-  return <PageContainer slug={''} />;
+  return (
+    <LinkContext.Provider value={{ prefetch: true }}>
+      <ContentWidthProvider fullWidth>
+        <PageContainer slug={''} />
+      </ContentWidthProvider>
+    </LinkContext.Provider>
+  );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
@@ -20,7 +28,7 @@ export const getStaticProps: GetStaticProps = async () => {
     return { props: {}, revalidate: 1 };
   }
 
-  const client = getV1ApiClient(publicRuntimeConfig.env.API_URL, []);
+  const client = getV1ApiClient(getApiUrl(), []);
   await Promise.all([
     client.query({
       query: PageDocument,
