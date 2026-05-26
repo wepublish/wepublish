cb6186561f57cedd89ecc04c51636a2994b8e8b3
dsheyp <dsheyp@gmail.com>
2026-05-26 16:41:33 +0200

    remove seeding related code

--- apps/editor/src/app/app.tsx
+++ apps/editor/src/app/app.tsx
@@ -83,7 +83,6 @@ import { PeerList } from './routes/peers/peerList';
 import { PollEditView } from './routes/polls/pollEditView';
 import { PollList } from './routes/polls/pollList';
 import { PollVoteListContainer } from './routes/polls/PollVotesListContainer';
-import { Seed } from './routes/seed/seed';
 import { SubscriptionEditView } from './routes/subscriptions/subscriptionEditView';
 import { SubscriptionList } from './routes/subscriptions/subscriptionList';
 import { TagCreateView } from './routes/tags/tagCreateView';
@@ -263,10 +262,6 @@ export function App() {
             <Route
               path="login"
               element={<Login />}
-            />
-            <Route
-              path="seed"
-              element={<Seed />}
             />
             <Route
               path="login/jwt/:jwt"
