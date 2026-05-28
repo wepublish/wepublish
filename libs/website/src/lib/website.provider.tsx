99e847282654765c950a87ddfe5b67bf198a5a06
dsheyp <dsheyp@gmail.com>
2026-05-28 21:00:13 +0200

    WIP

--- libs/website/src/lib/website.provider.tsx
+++ libs/website/src/lib/website.provider.tsx
@@ -7,7 +7,11 @@ import {
   ArticleSEO,
   ArticleTags,
 } from '@wepublish/article/website';
-import { LoginForm, RegistrationForm } from '@wepublish/authentication/website';
+import {
+  LoginForm,
+  RegistrationForm,
+  UserForm,
+} from '@wepublish/authentication/website';
 import {
   ArticleAuthor,
   Author,
@@ -213,6 +217,7 @@ export const WebsiteProvider = memo<WebsiteProps>(({ children }) => (
           LoginForm={LoginForm}
           RegistrationForm={RegistrationForm}
           PersonalDataForm={PersonalDataForm}
+          UserForm={UserForm}
           SubscriptionList={SubscriptionList}
           SubscriptionListItem={SubscriptionListItem}
           InvoiceList={InvoiceList}
