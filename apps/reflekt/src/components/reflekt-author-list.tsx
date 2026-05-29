ad21a6eaded582faad10a81e69714fd4b0aee6d9
dsheyp <dsheyp@gmail.com>
2026-05-29 16:50:52 +0200

    improved responsiveness of team page

--- apps/reflekt/src/components/reflekt-author-list.tsx
+++ apps/reflekt/src/components/reflekt-author-list.tsx
@@ -3,9 +3,18 @@ import { AuthorList as AuthorListDefault } from '@wepublish/author/website';
 
 export const ReflektAuthorList = styled(AuthorListDefault)`
   column-gap: ${({ theme }) => theme.spacing(2)};
+  grid-template-columns: 1fr;
+
+  ${({ theme }) => theme.breakpoints.up('sm')} {
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: ${({ theme }) => theme.spacing(1)};
+  }
 
   ${({ theme }) => theme.breakpoints.up('md')} {
-    grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
     column-gap: ${({ theme }) => theme.spacing(4)};
+  }
+
+  ${({ theme }) => theme.breakpoints.up('lg')} {
+    grid-template-columns: repeat(3, 1fr);
   }
 `;
