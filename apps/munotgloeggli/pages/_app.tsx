7a2e66ba9153985eacbb44877de4074531a5fe01
Lukas Diener <LukasSkywalker@users.noreply.github.com>
2026-06-09 15:22:21 +0200

    feat(munotgloeggli): Hide page publication date in teasers (SHAZ-18) (#2744)

--- apps/munotgloeggli/pages/_app.tsx
+++ apps/munotgloeggli/pages/_app.tsx
@@ -39,6 +39,7 @@ import { z } from 'zod';
 import { zodI18nMap } from 'zod-i18n-map';
 
 import deOverriden from '../locales/deOverriden.json';
+import { MunotgloeggliBaseTeaser } from '../src/components/website-builder-overwrites/blocks/teaser';
 import theme from '../src/theme';
 
 setDefaultOptions({
@@ -102,6 +103,7 @@ function CustomApp({ Component, pageProps, emotionCache }: CustomAppProps) {
             elements={{ Link: NextWepublishLink }}
             date={{ format: dateFormatter }}
             meta={{ siteTitle }}
+            blocks={{ BaseTeaser: MunotgloeggliBaseTeaser }}
           >
             <ThemeProvider theme={theme}>
               <CssBaseline />
