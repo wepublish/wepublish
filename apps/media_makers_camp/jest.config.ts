cd209049628e1bcdd3df4e31214302e5255f3d50
wepublish-service <it@wepublish.ch>
2026-06-10 10:40:44 +0200

    feat: add website app for "media_makers_camp"

---
+++ apps/media_makers_camp/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'media_makers_camp',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/media_makers_camp',
+};
