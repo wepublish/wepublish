879fee15fe39b4a2334cd06ff6179f0de400841d
Itrulia <karlmerkli@gmail.com>
2026-06-18 09:14:42 +0200

    feat(website): inteded routing via query (#2819)

--- apps/babanews/pages/login.tsx
+++ apps/babanews/pages/login.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 import { Typography } from '@mui/material';
 import {
+  IntendedRouteExpiryInSeconds,
   IntendedRouteStorageKey,
   LoginFormContainer,
   useUser,
@@ -12,7 +13,8 @@ import {
   SessionWithTokenWithoutUser,
 } from '@wepublish/website/api';
 import { useWebsiteBuilder } from '@wepublish/website/builder';
-import { deleteCookie, getCookie } from 'cookies-next';
+import { deleteCookie, getCookie, setCookie } from 'cookies-next';
+import { add } from 'date-fns';
 import { NextPageContext } from 'next';
 import { useRouter } from 'next/router';
 import { useEffect, useRef } from 'react';
@@ -36,6 +38,17 @@ export default function Login({ sessionToken }: LoginProps) {
       setToken(sessionToken);
     }
   }, [sessionToken, setToken]);
+
+  if (
+    router.query.intended &&
+    (router.query.intended as string).startsWith('/')
+  ) {
+    setCookie(IntendedRouteStorageKey, router.query.intended, {
+      expires: add(new Date(), {
+        seconds: IntendedRouteExpiryInSeconds,
+      }),
+    });
+  }
 
   if (hasUser && typeof window !== 'undefined') {
     const intendedRoute = getCookie(IntendedRouteStorageKey)?.toString();
