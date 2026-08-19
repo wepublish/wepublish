44e79d65fb42c0fa9b32d6aa52020cce4e4fdb78
dsheyp <dsheyp@gmail.com>
2026-08-19 12:11:10 +0200

    move periodicity prices to a separate table instead of using a json-field

--- libs/user-subscription/api/src/lib/user-subscription.service.spec.ts
+++ libs/user-subscription/api/src/lib/user-subscription.service.spec.ts
@@ -81,6 +81,7 @@ describe('UserSubscriptionService', () => {
         active: true,
         extendable: true,
         amountPerMonthMin: 0,
+        periodicityPricing: [],
       }),
     };
 
@@ -133,6 +134,7 @@ describe('UserSubscriptionService', () => {
       active: true,
       extendable: true,
       amountPerMonthMin: 0,
+      periodicityPricing: [],
     });
     prismaMock.paymentMethod.findFirst.mockResolvedValue({
       id: 'paymentMethodId',
