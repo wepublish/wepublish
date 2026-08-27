8295e4f1edb6ba7e5b87b2663aa4862d45c8efe4
dsheyp <dsheyp@gmail.com>
2026-08-27 03:36:16 +0200

    a checkbox in the editor to select whether goodie-minimum-payment-amount applies to upgrades too: yes or no?

--- libs/peering/api/src/lib/remote/graphql.ts
+++ libs/peering/api/src/lib/remote/graphql.ts
@@ -6338,6 +6338,7 @@ export type SubscribeBlock = BaseBlock & {
   disabled?: Maybe<Scalars['Boolean']>;
   fields: Array<SubscribeBlockField>;
   goodieMinValue?: Maybe<Scalars['Int']>;
+  goodieMinValueAppliesToUpgrade: Scalars['Boolean'];
   hideRepeatGoodieOnUpgrade: Scalars['Boolean'];
   memberPlanIds: Array<Scalars['String']>;
   memberPlanRenderSettings: Array<SubscribeBlockMemberPlanRenderSetting>;
@@ -6363,6 +6364,7 @@ export type SubscribeBlockInput = {
   disabled?: InputMaybe<Scalars['Boolean']>;
   fields?: Array<SubscribeBlockField>;
   goodieMinValue?: InputMaybe<Scalars['Int']>;
+  goodieMinValueAppliesToUpgrade?: Scalars['Boolean'];
   hideRepeatGoodieOnUpgrade?: Scalars['Boolean'];
   memberPlanIds?: Array<Scalars['String']>;
   memberPlanRenderSettings: Array<SubscribeBlockMemberPlanRenderSettingInput>;
--- libs/testing/src/graphql/graphql-public.ts
+++ libs/testing/src/graphql/graphql-public.ts
@@ -6130,6 +6130,7 @@ export type SubscribeBlock = BaseBlock & {
   disabled?: Maybe<Scalars['Boolean']>;
   fields: Array<SubscribeBlockField>;
   goodieMinValue?: Maybe<Scalars['Int']>;
+  goodieMinValueAppliesToUpgrade: Scalars['Boolean'];
   hideRepeatGoodieOnUpgrade: Scalars['Boolean'];
   memberPlanIds: Array<Scalars['String']>;
   memberPlanRenderSettings: Array<SubscribeBlockMemberPlanRenderSetting>;
@@ -6155,6 +6156,7 @@ export type SubscribeBlockInput = {
   disabled?: InputMaybe<Scalars['Boolean']>;
   fields?: Array<SubscribeBlockField>;
   goodieMinValue?: InputMaybe<Scalars['Int']>;
+  goodieMinValueAppliesToUpgrade?: Scalars['Boolean'];
   hideRepeatGoodieOnUpgrade?: Scalars['Boolean'];
   memberPlanIds?: Array<Scalars['String']>;
   memberPlanRenderSettings: Array<SubscribeBlockMemberPlanRenderSettingInput>;
