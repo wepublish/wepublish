1cd3069226ddb9e0884688b3a9a60c998d82a662
LukasSkywalker <git@lukasdiener.ch>
2026-09-02 17:04:02 +0200

    fix(hauptstadt): Add spacing before charcount in metadata panel

--- libs/ui/editor/src/lib/atoms/deferredTextField.tsx
+++ libs/ui/editor/src/lib/atoms/deferredTextField.tsx
@@ -13,6 +13,8 @@ const InputGroup = styled(RInputGroup)`
 
 const CharCount = styled.label`
   float: right;
+  margin-left: 8px;
+  color: var(--rs-text-secondary, #8e8e93);
 `;
 
 const CharCountWarning = styled.label`
