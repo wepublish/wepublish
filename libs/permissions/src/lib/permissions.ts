5d39e97c7c7244dd4d5dde80387042cf7a2b10c0
Ahmed Bajra <ahmed.bajra@gmail.com>
2026-09-08 10:15:09 +0200

    add permissions

--- libs/permissions/src/lib/permissions.ts
+++ libs/permissions/src/lib/permissions.ts
@@ -900,6 +900,28 @@ export const CanGetAudienceStats: Permission = {
 };
 
 /**
+ * Block Templates
+ */
+
+export const CanCreateBlockTemplate: Permission = {
+  id: 'CAN_CREATE_BLOCK_TEMPLATE',
+  description: 'Allows to create a block template',
+  deprecated: false,
+};
+
+export const CanUpdateBlockTemplate: Permission = {
+  id: 'CAN_UPDATE_BLOCK_TEMPLATE',
+  description: 'Allows to update a block template',
+  deprecated: false,
+};
+
+export const CanDeleteBlockTemplate: Permission = {
+  id: 'CAN_DELETE_BLOCK_TEMPLATE',
+  description: 'Allows to delete a block template',
+  deprecated: false,
+};
+
+/**
  * Paywall
  */
 export const CanCreatePaywall: Permission = {
@@ -1292,6 +1314,9 @@ export const AllPermissions: Permission[] = [
   CanCreateBlockStyle,
   CanUpdateBlockStyle,
   CanDeleteBlockStyle,
+  CanCreateBlockTemplate,
+  CanUpdateBlockTemplate,
+  CanDeleteBlockTemplate,
   CanCreatePaywall,
   CanUpdatePaywall,
   CanDeletePaywall,
