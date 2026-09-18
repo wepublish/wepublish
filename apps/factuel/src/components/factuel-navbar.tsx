e9c467ebdd91115eb5de4dc219af7253b60147bc
Ahmed Bajra <ahmed.bajra@gmail.com>
2026-09-18 15:41:11 +0200

    hide factuel menu

--- apps/factuel/src/components/factuel-navbar.tsx
+++ apps/factuel/src/components/factuel-navbar.tsx
@@ -3,6 +3,6 @@ import { ExtendedNavbarProps, Navbar } from '@wepublish/navigation/website';
 export const FactuelNavbar = (props: ExtendedNavbarProps) => (
   <Navbar
     {...props}
-    // hideMenu={true}
+    hideMenu={true}
   />
 );
