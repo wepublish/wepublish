43c435c863d57ef685b998cc56924f36b00d2f54
LukasSkywalker <git@lukasdiener.ch>
2026-09-23 21:56:24 +0200

    fix(hauptstadt): Reduce pgpool connect delay by increasing connection lifetime and therefore reuse

--- libs/nest-modules/src/lib/database/prisma.service.ts
+++ libs/nest-modules/src/lib/database/prisma.service.ts
@@ -15,7 +15,7 @@ export class PrismaService
       options: '-c timezone=UTC',
       max: parseInt(process.env['DATABASE_POOL_SIZE'] ?? '20'),
       connectionTimeoutMillis: 5_000,
-      idleTimeoutMillis: 10_000,
+      idleTimeoutMillis: 300_000,
     });
 
     super({ adapter });
