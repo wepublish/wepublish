1a706f08d1d0ad39db6d5bbd530d12a8e470a59e
Elias Summermatter <elias.summermatter@seccom.ch>
2026-09-24 16:09:59 +0200

    Add(demo): Add demo instance to wepublish

---
+++ apps/demo/index.d.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+declare module '*.svg' {
+  const content: any;
+  export const ReactComponent: any;
+  export default content;
+}
+
+declare module '*.css';
